d05631c4756154b1efa3e1273c40efec0306e371
Roger Ferrer <roger.ferrer@bsc.es>
2006-04-19 15:44:48 +0000

    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@61 39d7fdcd-100c-0410-a620-8454655e6f6e

--- src/cxx-buildsymtab.c
+++ src/cxx-buildsymtab.c
@@ -486,6 +486,7 @@ void gather_type_spec_from_enum_specifier(AST a, symtab_t* st, simple_type_t* si
 			AST enumeration_name = ASTSon0(enumeration);
 			AST enumeration_expr = ASTSon1(enumeration);
 
+			// Note that enums do not define an additional scope
 			symtab_entry_t* enumeration_item = new_symbol(st, ASTText(enumeration_name));
 
 			enumeration_item->kind = SK_ENUMERATOR;
