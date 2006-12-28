4c1e8f491dfe5b08b8ebbe0145bfd2d5d389a6ec
Xavier Teruel <xavier.teruel@bsc.es>
2006-12-28 13:39:27 +0000

    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@473 39d7fdcd-100c-0410-a620-8454655e6f6e

--- regenerate.sh
+++ regenerate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aclocal-1.9 && autoconf2.50 && autoheader2.50 && automake-1.9 -a -c --foreign && libtoolize --force
+aclocal-1.9 && autoconf && autoheader && libtoolize --force && automake-1.9 -a -c --foreign 
