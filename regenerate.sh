11f561ff5c92b0f733d044c9112498f1721b78bd
Roger Ferrer <roger.ferrer@bsc.es>
2007-04-26 09:57:15 +0000

    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@804 39d7fdcd-100c-0410-a620-8454655e6f6e

--- regenerate.sh
+++ regenerate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aclocal-1.9 && autoconf && autoheader && libtoolize --force && automake-1.9 -a -c --foreign 
+aclocal-1.9 && autoconf && autoheader && libtoolize --force && automake-1.9 -a -c 
