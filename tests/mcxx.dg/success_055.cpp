bb3d8f446ba745aa8a2a853fd51a76b3d22d662e
Roger Ferrer <roger.ferrer@bsc.es>
2007-06-21 13:11:14 +0000

    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@1581 39d7fdcd-100c-0410-a620-8454655e6f6e

---
+++ tests/mcxx.dg/success_055.cpp
@@ -0,0 +1,38 @@
+/*
+    Mercurium C/C++ Compiler
+    Copyright (C) 2006-2007 - Roger Ferrer Ibanez <roger.ferrer@bsc.es>
+    Barcelona Supercomputing Center - Centro Nacional de Supercomputacion
+    Universitat Politecnica de Catalunya
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+template <int _N, int _M>
+struct A
+{
+    typedef int T1[_N][_M];
+};
+
+// template <int _N>
+// struct A<_N, _N>
+// {
+//     typedef float T2[_N][_N];
+// };
+
+// A<20, 21>::T1 k1;
+// A<20, 21>::T1 k2;
+// A<20, 20>::T2 k3;
+
+typedef A<20, 21>::T1 P;
+typedef A<20, 21>::T1 P;
