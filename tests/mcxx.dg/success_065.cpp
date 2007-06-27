a5ac8e8c054b02064cfaa1b794a5f4830da190e9
Roger Ferrer <roger.ferrer@bsc.es>
2007-06-27 14:46:39 +0000

    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@1590 39d7fdcd-100c-0410-a620-8454655e6f6e

---
+++ tests/mcxx.dg/success_065.cpp
@@ -0,0 +1,75 @@
+/*
+    Mercurium C/C++ Compiler
+    Copyright (C) 2006-2007 - Roger Ferrer Ibanez <roger.ferrer@bsc.es>
+    Barcelona Supercomputing Center - Centro Nacional de Supercomputacion
+    Universitat Politecnica de Catalunya
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+namespace NS2
+{
+    template <typename _T, typename _Q>
+    struct D
+    {
+    };
+}
+
+namespace NS1
+{
+    template <typename _T, int _N>
+    struct C
+    {
+    };
+
+    template <typename _T, typename _Q>
+    struct F
+    {
+    };
+    
+    template <>
+    struct F<int, float>
+    {
+        template <typename _T, typename _Q>
+        struct B : C<
+                     typename NS2::D<
+                                typename _T::T, 
+                                typename _Q::T
+                               >::T,
+                     (_T::V + _Q::V)
+                    >
+        {
+        };
+    };
+    
+    template <typename _T, typename _Q>
+    struct G
+    {
+    };
+    
+    template <>
+    struct G<int, float>
+    {
+        template <typename _T, typename _Q>
+        struct B : C<
+                     typename NS2::D<
+                                typename _T::T, 
+                                typename _Q::T
+                               >::T,
+                     (_T::V - _Q::V)
+                    >
+        {
+        };
+    };
+}
