04851fbf66fd2b879574479c1adaf4db5f417bc4
Josep Maria Perez <josep.m.perez@bsc.es>
2008-03-10 14:52:07 +0000

    Added tests to check for inconsistyencies in superscalar parameter translation when determining sizes.
    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@2229 39d7fdcd-100c-0410-a620-8454655e6f6e

---
+++ tests/mcc.superscalar.dg/success_smpss-035.c
@@ -0,0 +1,9 @@
+#pragma css task input(a, b[a])
+void f(int a, float *b);
+
+
+void g(int c, float *d) {
+	f(c, d);
+}
+
+
---
+++ tests/mcc.superscalar.dg/success_smpss-036.c
@@ -0,0 +1,9 @@
+#pragma css task input(b[a], a)
+void f(float *b, int a);
+
+
+void g(float *d, int c) {
+	f(d, c);
+}
+
+
