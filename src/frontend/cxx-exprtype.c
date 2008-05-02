5d8f2eb82bd6ffd466394aee0c19981f64d05912
Roger Ferrer <roger.ferrer@bsc.es>
2008-05-02 13:21:39 +0000

    - More pseudo-destructor breakage
    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@2488 39d7fdcd-100c-0410-a620-8454655e6f6e

--- src/frontend/cxx-exprtype.c
+++ src/frontend/cxx-exprtype.c
@@ -7740,8 +7740,20 @@ static char check_for_pseudo_destructor_call(AST expression, decl_context_t decl
         return 0;
     }
 
-    if (!is_dependent_expr_type(ASTExprType(postfix_expression))
-            && !is_scalar_type(ASTExprType(postfix_expression)))
+    type_t* considered_type = ASTExprType(postfix_expression);
+
+    if (is_arrow)
+    {
+        if (!is_pointer_type(no_ref(ASTExprType(postfix_expression))))
+        {
+            return 0;
+        }
+
+        considered_type = pointer_type_get_pointee_type(no_ref(ASTExprType(postfix_expression)));
+    }
+
+    if (!is_dependent_expr_type(no_ref(considered_type))
+            && !is_scalar_type(no_ref(considered_type)))
     {
         return 0;
     }
