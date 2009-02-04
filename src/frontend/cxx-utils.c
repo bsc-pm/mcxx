c05f2ec0c263767516bab4a5b506938aa630e43c
Roger Ferrer <roger.ferrer@bsc.es>
2009-02-04 16:17:50 +0000

    - Remove unnecessary headers
    git-svn-id: svn+ssh://mercurium.pc.ac.upc.edu/var/lib/svn/mcxx/trunk@2997 39d7fdcd-100c-0410-a620-8454655e6f6e

--- src/frontend/cxx-utils.c
+++ src/frontend/cxx-utils.c
@@ -18,9 +18,6 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
