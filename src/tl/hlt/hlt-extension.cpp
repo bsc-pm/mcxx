d74e3c5dc912f98208fe4a78191e364057784798
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2009-05-25 15:29:02 +0200

    More fixes

--- src/tl/hlt/hlt-extension.cpp
+++ src/tl/hlt/hlt-extension.cpp
@@ -48,9 +48,17 @@ void FunctionExtension::do_extension()
     }
     else
     {
-        if (!_extension_amount.is_constant())
+        bool is_const_extension = false;
+        int const_value = 0;
+        is_const_extension = _extension_amount.is_constant();
+        if (!is_const_extension)
         {
             extended_args << _extension_amount.get_type().get_declaration(_function_symbol.get_scope(), "_N");
+        }
+        else
+        {
+            bool valid = false;
+            const_value = _extension_amount.evaluate_constant_int_expression(valid);
         }
 
         DeclaredEntity declared_entity = _funct_def.get_declared_entity();
@@ -60,6 +68,8 @@ void FunctionExtension::do_extension()
         Statement function_body = _funct_def.get_function_body();
         Expression *fake_dim_expr = NULL;
         Scope fake_dim_expr_sc;
+
+        if (!is_const_extension)
         {
             // I hate having to do this
             AST_t placeholder;
@@ -74,6 +84,16 @@ void FunctionExtension::do_extension()
 
             fake_dim_expr_sc = function_body.get_scope_link().get_scope(placeholder);
         }
+        else
+        {
+            Source src;
+            src << const_value;
+            fake_dim_expr = new Expression(
+                    src.parse_expression(function_body.get_ast(), 
+                        function_body.get_scope_link()), 
+                    function_body.get_scope_link());
+            fake_dim_expr_sc = function_body.get_scope_link().get_scope(function_body.get_ast());
+        }
 
         ReplaceSrcIdExpression replacements(_funct_def.get_scope_link());
         for (ObjectList<ParameterDeclaration>::iterator it = param_decl.begin();
@@ -84,7 +104,8 @@ void FunctionExtension::do_extension()
             Symbol sym = id_expr.get_symbol();
             Type param_type = it->get_type();
 
-            Type array_type = param_type.get_array_to(fake_dim_expr->get_ast(), fake_dim_expr_sc);
+            Type array_type(NULL);
+            array_type = param_type.get_array_to(fake_dim_expr->get_ast(), fake_dim_expr_sc);
 
             // This expression is not valid
 
@@ -95,11 +116,21 @@ void FunctionExtension::do_extension()
             replacements.add_replacement(sym, "(" + sym.get_name() + "[_i])");
         }
 
+        Source upper_ext;
+        if (is_const_extension)
+        {
+            upper_ext << const_value;
+        }
+        else
+        {
+            upper_ext << "_N";
+        }
+
         Source replaced_body;
         extended_body
             << "{"
             <<   "int _i;"
-            <<   "for (_i = 0; _i < " << _extension_amount << "; _i++)"
+            <<   "for (_i = 0; _i < " << upper_ext << "; _i++)"
             <<   "{"
             <<   replacements.replace(function_body)
             <<   "}"
