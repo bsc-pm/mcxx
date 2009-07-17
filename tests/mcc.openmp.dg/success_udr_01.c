02c2b052f52474a75e9b92dc8ef5795fc1544516
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2009-07-17 11:17:49 +0200

    Tests for UDR

---
+++ tests/mcc.openmp.dg/success_udr_01.c
@@ -0,0 +1,26 @@
+typedef struct {
+  double real;
+  double imaginary;
+} my_complex_t;
+
+my_complex_t complex_add (my_complex_t a, my_complex_t b);
+my_complex_t complex_sub (my_complex_t a, my_complex_t b);
+my_complex_t complex_mul (my_complex_t a, my_complex_t b);
+
+#pragma omp declare reduction type(my_complex_t) operator(complex_add, complex_sub) identity({0,0})
+
+#pragma omp declare reduction type(my_complex_t) operator(complex_mul) identity({1,0})
+
+#define N 100
+my_complex_t vector[N];
+
+void f(my_complex_t x, my_complex_t y)
+{
+    int i;
+#pragma omp parallel for reduction(complex_add:x) reduction(complex_mul:y)
+    for ( i = 0; i < N ; i++ ) 
+    {
+        x = complex_add(x,vector[i]);
+        y = complex_mul(y,vector[i]);
+    }
+}
