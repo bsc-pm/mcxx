70d25b5ac97d2917e6eda60b712cc580aea4d1f6
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2009-08-12 15:27:56 +0200

    More fixes to make it more api compliant

--- src/tl/omp_nanox/tl-task.cpp
+++ src/tl/omp_nanox/tl-task.cpp
@@ -45,6 +45,8 @@ void OMPTransform::task_postorder(PragmaCustomConstruct ctr)
             replaced_body,
             initial_replace_code);
 
+    Source device_description, device_descriptor;
+
     Source newly_generated_code;
     newly_generated_code
         << struct_arg_type_decl_src
@@ -53,21 +55,39 @@ void OMPTransform::task_postorder(PragmaCustomConstruct ctr)
         <<     initial_replace_code
         <<     replaced_body
         << "}"
+        // Devices related this task
+        << device_description
         ;
 
+    // Device descriptor
+    // FIXME - Currently only SMP is supported
+    device_descriptor << outline_name << "_devices";
+    device_description
+        << "nanos_device_t " << device_descriptor << "="
+        << "{"
+        // SMP
+        // FIXME: g++ will not allow C99 initialization syntax!!!
+        // (technically: only C90 intialization is valid in C++03)
+        // (sad note aside: even C++0x is not going to support C99 initializers)
+        // So, args of a device should be a void* and we could statically make
+        // it point a previously device-dependent structure 
+        << "{nanos_smp_factory, { .smp = {"  << outline_name << " }}},"
+        // There must be some kind of sentinel, otherwise nobody will know the length of this array
+        // and does not seem to be a size argument for the device
+        << "{ NULL }"
+        << "};"
+        ;
 
     AST_t outline_code 
         = newly_generated_code.parse_declaration(funct_def.get_ast(), ctr.get_scope_link());
     ctr.get_ast().prepend_sibling_function(outline_code);
     
 // FIXME - No dependences (yet)
-    Source spawn_code, device_description;
+    Source spawn_code;
     Source fill_outline_arguments, fill_immediate_arguments;
 
     fill_data_args("ol_args->", data_environ_info, fill_outline_arguments);
     fill_data_args("imm_args.", data_environ_info, fill_immediate_arguments);
-
-    device_description << "(nanos_device_description_t*)0";
 
     spawn_code
         << "{"
@@ -75,20 +95,20 @@ void OMPTransform::task_postorder(PragmaCustomConstruct ctr)
         <<     "nanos_wd_t* wd = (nanos_wd_t*)0;"
         // <<     "nanos_wd_props_t props;"
         <<     "nanos_err_t err;"
-        <<     "err = nanos_create_wd(&wd, " << device_description << ", sizeof(" << struct_arg_type_name << "),"
-        <<                "&ol_args, nanos_current_wd(), (nanos_wd_props_t*)0);" // props are 0 here
+        <<     "err = nanos_create_wd(&wd, " << device_descriptor << ", sizeof(" << struct_arg_type_name << "),"
+        <<                "(void**)&ol_args, nanos_current_wd(), (nanos_wd_props_t*)0);" // props are 0 here
         //     FIXME - Do something useful with err
         <<     "if (wd != (nanos_wd_t*)0)"
         <<     "{"
         <<        fill_outline_arguments
-        <<        "err = nanos_submit(wd, (nanos_dependence_t*)0, 0);" // What is the last 0 ?
+        <<        "err = nanos_submit(wd, (nanos_dependence_t*)0, (nanos_team_t*)0);"
         //     FIXME - Do something useful with err 
         <<     "}"
         <<     "else"
         <<     "{"
         <<        struct_arg_type_name << " imm_args;"
         <<        fill_immediate_arguments
-        <<        "err = nanos_create_wd_and_run(" << device_description << ", " 
+        <<        "err = nanos_create_wd_and_run(" << device_descriptor << ", " 
         <<        "       &imm_args, (nanos_dependence_t*)0, (nanos_wd_props_t*)0);"
         //     FIXME - Do something useful with err 
         <<     "}"
