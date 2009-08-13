89935285196e41a23db6dfeea3e7f27787a0c076
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2009-08-13 18:07:03 +0200

    Changes

--- src/tl/omp_nanox/tl-parallel.cpp
+++ src/tl/omp_nanox/tl-parallel.cpp
@@ -6,6 +6,30 @@ namespace TL
     {
         void OMPTransform::parallel_postorder(PragmaCustomConstruct ctr)
         {
+
+        }
+
+        Source common_parallel_spawn_code()
+        {
+            Source result;
+
+            result
+                << "{"
+                // FIXME - How to get the default number of threads?
+                << "unsigned int _nanos_num_threads = 4;"
+                << "nanos_team_t _nanos_team;"
+                << "nanos_thread_t _nanos_threads[_nanos_num_threads];"
+                << "err = nth_create_team(&team, (nanos_sched_t)0, &_nanos_num_threads,"
+                <<            "(nanos_constraint_t*)0, /* reuse */ false, _nanos_threads);"
+                << "if (err != NANOS_OK) nanos_error_handler(err);"
+
+                << "int _i;"
+                << "for (_i = 0; _i < _nanos_num_threads; _i++)"
+                << "{"
+                << "}"
+                ;
+
+            return result;
         }
     }
 }
