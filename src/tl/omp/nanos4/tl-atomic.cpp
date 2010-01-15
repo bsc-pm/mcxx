7adaaa21bc5781e2055179c2de94ba1ef625e851
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-01-15 16:50:18 +0100

    Implement atomic for other types and operations using an atomic update
    This commit fixes #206 for Nanos 4

--- src/tl/omp/nanos4/tl-atomic.cpp
+++ src/tl/omp/nanos4/tl-atomic.cpp
@@ -48,7 +48,7 @@ namespace TL
                     atomic_construct.get_scope_link());
         }
 
-        static bool allowed_expressions_critical(Expression expr)
+        static bool allowed_expressions_critical(Expression expr, bool &using_builtin)
         {
             Expression::OperationKind op_kind = expr.get_operation_kind();
             if (op_kind == Expression::PREINCREMENT  // ++x
@@ -68,21 +68,27 @@ namespace TL
                 }
 
                 if (!is_lvalue
-                        || !t.is_integral_type())
+                        || !(t.is_integral_type()
+                            || t.is_floating_type()))
                     return false;
+
+                // Only integer types can use a gcc builtin
+                using_builtin = t.is_integral_type();
 
                 // No objections
                 return true;
             }
-            if (op_kind == Expression::ADDITION // x += y
-                    || op_kind == Expression::SUBSTRACTION  // x -= y
-                    // || op_kind == Expression::MULTIPLICATION  // x *= y
-                    // || op_kind == Expression::DIVISION // x /= y
-                    || op_kind == Expression::BITWISE_AND  // x &= y
-                    || op_kind == Expression::BITWISE_OR  // x |= y
-                    || op_kind == Expression::BITWISE_XOR // x ^= y
-                    // || op_kind == Expression::SHIFT_LEFT // x <<= y
-                    // || op_kind == Expression::SHIFT_RIGHT // x >>= y
+            if (expr.is_operation_assignment()
+                    && (op_kind == Expression::ADDITION // x += y
+                        || op_kind == Expression::SUBSTRACTION  // x -= y
+                        || op_kind == Expression::MULTIPLICATION  // x *= y
+                        || op_kind == Expression::DIVISION // x /= y
+                        || op_kind == Expression::BITWISE_AND  // x &= y
+                        || op_kind == Expression::BITWISE_OR  // x |= y
+                        || op_kind == Expression::BITWISE_XOR // x ^= y
+                        || op_kind == Expression::SHIFT_LEFT // x <<= y
+                        || op_kind == Expression::SHIFT_RIGHT // x >>= y
+                       )
                )
             {
                 Expression lhs = expr.get_first_operand();
@@ -98,15 +104,28 @@ namespace TL
                         t = t.references_to();
                 }
 
+                bool lhs_is_integral = t.is_integral_type();
                 if (!is_lvalue
-                        || !t.is_integral_type())
+                        || !(lhs_is_integral
+                            || t.is_floating_type()))
                     return false;
+
                 
                 // Likewise for rhs
                 t = rhs.get_type(is_lvalue);
                 if (!is_lvalue
-                        || !t.is_integral_type())
+                        || !(t.is_integral_type()
+                            || t.is_floating_type()))
                     return false;
+
+                using_builtin =
+                    lhs_is_integral && 
+                    (op_kind == Expression::ADDITION // x += y
+                     || op_kind == Expression::SUBSTRACTION  // x -= y
+                     || op_kind == Expression::BITWISE_AND  // x &= y
+                     || op_kind == Expression::BITWISE_OR  // x |= y
+                     || op_kind == Expression::BITWISE_XOR // x ^= y
+                    );
 
                 return true;
             }
@@ -114,7 +133,101 @@ namespace TL
             return false;
         }
 
-        static AST_t efficient_atomic(PragmaCustomConstruct atomic_construct, Expression expr)
+        static AST_t compare_and_exchange(PragmaCustomConstruct atomic_construct, Expression expr)
+        {
+            Expression::OperationKind op_kind = expr.get_operation_kind();
+            Source critical_source;
+            Source type, lhs, rhs, op, bytes, proper_int_type;
+
+            Type expr_type = expr.get_type();
+
+            if (expr_type.is_reference())
+            {
+                expr_type = expr_type.references_to();
+            }
+
+            critical_source 
+                << "{"
+                <<   type << " __oldval;"
+                <<   type << " __newval;"
+
+                <<   "do {"
+                <<      "__oldval = (" << lhs << ");"
+                <<      "__newval = __oldval " << op << " (" << rhs << ");"
+                <<      "__sync_synchronize();"
+                <<   "} while (!__sync_bool_compare_and_swap_" << bytes << "( &(" << lhs << ") ,"
+                <<                 "*(" << proper_int_type << "*)&__oldval,"
+                <<                 "*(" << proper_int_type << "*)&__newval ));"
+                << "}"
+            ;
+
+            if (op_kind == Expression::PREINCREMENT  // ++x
+                    || op_kind == Expression::POSTINCREMENT // x++
+                    || op_kind == Expression::PREDECREMENT // --x
+                    || op_kind == Expression::POSTDECREMENT) // x--
+            {
+                lhs << expr.get_unary_operand().prettyprint();
+                rhs << "1";
+
+                if (op_kind == Expression::PREDECREMENT
+                        || op_kind == Expression::POSTDECREMENT)
+                {
+                    op << "-";
+                }
+                else
+                {
+                    op << "+";
+                }
+
+            }
+            else
+            {
+                op << expr.get_operator_str();
+                lhs << expr.get_first_operand().prettyprint();
+                rhs << expr.get_second_operand().prettyprint();
+            }
+
+            type = expr_type.get_declaration(expr.get_scope(), "");
+            bytes << expr_type.get_size();
+
+            // FIXME - We should be choosing the proper size using the environment
+            if (expr_type.get_size() == 4)
+            {
+                Type int_type(::get_unsigned_int_type());
+                if (int_type.get_size() == 4)
+                {
+                    proper_int_type << "unsigned int";
+                }
+                else
+                {
+                    internal_error("Code unreachable", 0);
+                }
+            }
+            else if (expr_type.get_size() == 8)
+            {
+                // FIXME - Why are we using a low level op?
+                Type long_type(::get_unsigned_long_int_type());
+                Type long_long_type(::get_unsigned_long_long_int_type());
+
+                if (long_type.get_size() == 8)
+                {
+                    proper_int_type << "unsigned long";
+                }
+                else if (long_long_type.get_size() == 8)
+                {
+                    proper_int_type << "unsigned long long";
+                }
+                else
+                {
+                    internal_error("Code unreachable", 0);
+                }
+            }
+
+            return critical_source.parse_statement(atomic_construct.get_ast(),
+                    atomic_construct.get_scope_link());
+        }
+
+        static AST_t builtin_atomic_int_op(PragmaCustomConstruct atomic_construct, Expression expr)
         {
             Expression::OperationKind op_kind = expr.get_operation_kind();
             Source critical_source;
@@ -235,14 +348,22 @@ namespace TL
             else
             {
                 Expression expr = critical_body.get_expression();
-                if (!allowed_expressions_critical(expr))
+                bool using_builtin = false;
+                if (!allowed_expressions_critical(expr, using_builtin))
                 {
                     std::cerr << atomic_construct.get_ast().get_locus() << ": warning: 'atomic' expression cannot be implemented efficiently" << std::endl;
                     atomic_tree = inefficient_atomic(atomic_construct);
                 }
                 else
                 {
-                    atomic_tree = efficient_atomic(atomic_construct, expr);
+                    if (using_builtin)
+                    {
+                        atomic_tree = builtin_atomic_int_op(atomic_construct, expr);
+                    }
+                    else
+                    {
+                        atomic_tree = compare_and_exchange(atomic_construct, expr);
+                    }
                 }
             
             }
