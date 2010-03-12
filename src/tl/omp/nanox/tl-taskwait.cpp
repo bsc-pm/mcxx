ec67904ca0f5aedb5334e6c8ec76d70a9c4dda66
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-03-12 17:14:29 +0100

    Remove "completation"

--- src/tl/omp/nanox/tl-taskwait.cpp
+++ src/tl/omp/nanox/tl-taskwait.cpp
@@ -112,7 +112,7 @@ namespace TL
             }
             else
             {
-                src << "nanos_wg_wait_completation(nanos_current_wd());"
+                src << "nanos_wg_wait_completion(nanos_current_wd());"
                     ;
             }
 
