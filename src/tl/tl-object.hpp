ae9d8101603ff1e5884ebed74f1d7617b782270d
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-03-22 10:34:16 +0100

    Add a main page in doxygen
    This commit fixes #136

--- src/tl/tl-object.hpp
+++ src/tl/tl-object.hpp
@@ -24,6 +24,53 @@
 #ifndef TL_OBJECT_HPP
 #define TL_OBJECT_HPP
 
+/*!
+  \mainpage Mercurium C/C++ TL API Documentation
+
+  Welcome to the Mercurium C/C++ TL API Documentation. TL is the component of Mercurium
+  meant for extending the compiler. TL stands, unofficially, for Transformation Language
+  and actually is a set of C++ APIs which interact with the internals of the compiler. 
+  
+  Although not always achieved (compilers are tough pieces of software), TL's goal is
+  reducing the complexity by providing tools for most everyday uses of phases. There is
+  support for handling the internal representation of the compiler and to create new
+  code. In Mercurium new code is created using plain source, there is no need to mess
+  with abstract syntax trees. Analysis of code, though, is less easily achieved because
+  of the high level nature of the source-to-source approach.
+
+  \section navigating Navigating this documentation
+
+  In order to navigate this code, we recommend using the Class Index (there is a link on top of this page).
+
+  \section overview Quick overview of TL
+
+  There are several concepts which you should be familiar with. Phases in Mercurium
+  are dynamically loaded plugins which inherit of class TL::CompilerPhase. There are some
+  specialized phases already pre-made: TL::PragmaCustomCompilerPhase (to implement your
+  own #pragma constructs) and TL::OpenMP::OpenMPPhase (to implement OpenMP transformations). 
+  
+  Most of the time there is no need to work directly with TL::AST_t (abstract syntax trees). 
+  Instead wrappers about several kinds of AST_t are provided: these are called LangConstruct.
+  Probably the most useful ones are Expression and Statement (there are some other kinds of
+  subclasses).
+
+  Walking in the AST_t to find things can be performed using predicates, which
+  assert some boolean property on the tree nodes. Many TL::LangConstructs provide
+  predicates (for instance FunctionDefinition::predicate) in order to make
+  easier to walk only specific nodes.
+
+  Entities of the program are represented by objects of the class TL::Symbol. Most Symbol objects
+  also have a TL::Type, which wraps the C/C++ type-system. Type is useful for creating new
+  declarations in the newly created source.
+
+  TL::Source, which reminds a C++ stringstream, is the class used to create new
+  code. Once a Source has been filled with the desired string (representing
+  some valid code) it can be parsed. A new AST_t is returned (which if needed
+  can be wrapped into some specific LangConstruct).  This tree can then be used
+  for replacing (or adding to) an existing AST_t, thus changing the source that
+  will be finally compiled by the native (backend) compiler.
+*/
+
 #include "tl-common.hpp"
 #include <iostream>
 #include <string>
