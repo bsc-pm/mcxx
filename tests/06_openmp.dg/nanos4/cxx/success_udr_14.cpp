0f742389d39b8b16ff350a7a4a620aa9854d16eb
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-04-08 18:13:02 +0200

    Restructure OpenMP tests
    Separate common tests from {nanos4,nanox} specific codes
    
    This commit ref #272

---
+++ tests/06_openmp.dg/nanos4/cxx/success_udr_14.cpp
@@ -0,0 +1,29 @@
+/*
+<testinfo>
+test_generator=config/mercurium-nanos4
+</testinfo>
+*/
+#include <list>
+#include <assert.h>
+
+template <typename _T>
+void f()
+{
+    std::list<_T> a;
+}
+
+#pragma omp declare reduction(template<typename _T> std::list<_T>::merge : std::list<_T>) 
+
+const int N = 100;
+
+int main (int argc, char *argv[])
+{
+	std::list<int> l;
+
+	#pragma omp parallel for reduction(.merge : l)
+	for ( unsigned i = 0; i < N; i++ ) {
+		l.push_back(i);
+	}
+
+	assert(l.size() == N);
+}
