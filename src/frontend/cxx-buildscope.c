bba296fba35e4cc056efa6ca53cce058e2e7b96e
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-05-07 16:11:14 +0200

    Improve message when a template explicit specialization does not match any template declaration

--- src/frontend/cxx-buildscope.c
+++ src/frontend/cxx-buildscope.c
@@ -5181,6 +5181,14 @@ static scope_entry_t* register_function(AST declarator_id, type_t* declarator_ty
                 return NULL;
             }
 
+            if (decl_context.template_parameters == NULL)
+            {
+                running_error("%s: error: explicit specialization '%s' does not match any template of '%s'\n",
+                        ast_location(declarator_id),
+                        print_decl_type_str(declarator_type, decl_context, function_name),
+                        function_name);
+            }
+
             ERROR_CONDITION(decl_context.template_parameters == NULL,
                     "Error, there must be template parameters", 0);
 
