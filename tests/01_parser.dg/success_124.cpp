707b0fff6ac4fbcad6c4bd938024376657824049
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2010-07-23 09:14:06 +0200

    Mark test as faulty
    See #337

--- tests/01_parser.dg/success_124.cpp
+++ tests/01_parser.dg/success_124.cpp
@@ -1,6 +1,14 @@
 /*
 <testinfo>
 test_generator=config/mercurium
+
+# This test is faulty because the implicit type
+# of member functions of the bases pulled in by
+# using declarations do not have their implicit
+# member assumed to be of the derived class
+# See ticket #337
+test_compile_faulty=yes
+test_compile_fail=yes
 </testinfo>
 */
 struct A
