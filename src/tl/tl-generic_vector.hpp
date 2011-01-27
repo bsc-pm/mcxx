f9675d982267b208f01af4bc48ed0d7347b658ae
Diego Caballero <diego.caballero@bsc.es>
2011-01-27 17:04:00 +0100

    Add a test case for vector typechecking

---
+++ src/tl/tl-generic_vector.hpp
@@ -0,0 +1,9 @@
+#ifndef TL_GENERIC_VECTOR_HPP
+#define TL_GENERIC_VECTOR_HPP
+
+#define BUILTIN_VL_NAME "__builtin_vector_loop"
+#define BUILTIN_VE_NAME "__builtin_vector_expansion"
+#define ATTR_GEN_VEC_NAME "generic_vector"
+
+
+#endif
