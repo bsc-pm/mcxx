53021f11caa629b452fb69b46442d5223192cf44
Sara Royuela <sara.royuela@bsc.es>
2011-03-24 12:45:02 +0100

    Fixes for Fortran

--- src/tl/tl-fortran.cpp
+++ src/tl/tl-fortran.cpp
@@ -25,20 +25,20 @@
 --------------------------------------------------------------------*/
 
 
-#ifdef FORTRAN_SUPPORT
-
 #include "tl-fortran.hpp"
+
+#ifdef FORTRAN_SUPPORT
 
 namespace TL { namespace Fortran {
 
-    PredicateAttr ProgramUnit::predicate(LANG_IS_FORTRAN_PROGRAM_UNIT);
+    const PredicateAttr ProgramUnit::predicate(LANG_IS_FORTRAN_PROGRAM_UNIT);
 
-    PredicateAttr Function::predicate(LANG_IS_FORTRAN_FUNCTION);
-    PredicateAttr Subroutine::predicate(LANG_IS_FORTRAN_SUBROUTINE);
-    PredicateAttr BlockData::predicate(LANG_IS_FORTRAN_BLOCKDATA);
-    PredicateAttr Module::predicate(LANG_IS_FORTRAN_MODULE);
+    const PredicateAttr Function::predicate(LANG_IS_FORTRAN_FUNCTION);
+    const PredicateAttr Subroutine::predicate(LANG_IS_FORTRAN_SUBROUTINE);
+    const PredicateAttr BlockData::predicate(LANG_IS_FORTRAN_BLOCK_DATA);
+    const PredicateAttr Module::predicate(LANG_IS_FORTRAN_MODULE);
 
-    PredicateAttr SpecificationStatement::predicate(LANG_IS_FORTRAN_SPECIFICATION_STATEMENT);
+    const PredicateAttr SpecificationStatement::predicate(LANG_IS_FORTRAN_SPECIFICATION_STATEMENT);
 
     ObjectList<Statement> ProgramUnit::get_statements()
     {
@@ -55,14 +55,14 @@ namespace TL { namespace Fortran {
 
         if (!statements.is_list())
         {
-            std::cerr << "Warning: expecting a list here -- " << statement.get_locus() << std::endl;
+            std::cerr << "Warning: expecting a list here -- " << statements.get_locus() << std::endl;
             return result;
         }
 
         ASTIterator it = statements.get_list_iterator();
         while (!it.end())
         {
-            result.push_back(Statement(it.item(), _sl));
+            result.push_back(Statement(it.item(), _scope_link));
             it.next();
         }
 
@@ -73,14 +73,16 @@ namespace TL { namespace Fortran {
     {
         ObjectList<ProgramUnit> result;
 
-        AST_t internal = program_unit_body.get_attribute(LANG_FORTRAN_PROGRAM_UNIT_INTERNAL_SUBPROGRAMS);
+        AST_t internal = _ref.get_attribute(LANG_FORTRAN_PROGRAM_UNIT_INTERNAL_SUBPROGRAMS);
 
         ASTIterator it = internal.get_list_iterator();
         while (!it.end())
         {
-            result.push_back(ProgramUnit(it.item(), _sl));
+            result.push_back(ProgramUnit(it.item(), _scope_link));
             it.next();
         }
+
+        return result;
     }
 
 } }
