3da6d1d6484bf2e71a34b3f71429f2490c33cbf3
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2011-04-08 19:17:34 +0200

    More progress for extra attributes

--- src/frontend/fortran/fortran03-modules.c
+++ src/frontend/fortran/fortran03-modules.c
@@ -16,9 +16,8 @@ typedef sqlite3* storage_handle_t;
 static void create_storage(storage_handle_t*, const char*);
 static void init_storage(storage_handle_t, const char*);
 static void dispose_storage(storage_handle_t);
-static void dump_symbol(storage_handle_t, scope_entry_t* module, scope_entry_t* symbol);
 
-static int insert_symbol(storage_handle_t handle, scope_entry_t* symbol);
+static sqlite3_int64 insert_symbol(storage_handle_t handle, scope_entry_t* symbol);
 
 void dump_module_info(scope_entry_t* module)
 {
@@ -62,7 +61,7 @@ static void create_storage(storage_handle_t* handle, const char* name)
         running_error("Error while removing old module '%s' (%s)\n", filename, strerror(errno));
     }
 
-    int result = sqlite3_open(filename, handle);
+    sqlite3_int64 result = sqlite3_open(filename, handle);
 
     if (result != SQLITE_OK)
     {
@@ -125,29 +124,63 @@ static void init_storage(storage_handle_t handle, const char* name)
     }
 }
 
-static int insert_type_simple(storage_handle_t handle, const char* name)
+static int count_rows(void* datum, 
+        int nrows, 
+        char** values UNUSED_PARAMETER,
+        char** colnames UNUSED_PARAMETER)
 {
-    // FIXME - Do not insert twice the same type
-    char * insert_type = sqlite3_mprintf("INSERT INTO type(kind) VALUES (%Q);", name);
+    int *num = (int*)datum;
+    *num = nrows;
+    return 0;
+}
+
+static sqlite3_int64 oid_already_inserted(storage_handle_t handle, const char *table, void *ptr)
+{
+    char * select_oid = sqlite3_mprintf("SELECT oid FROM %s WHERE oid = %p;", table, ptr);
+    char* errmsg = NULL;
+    int num_rows = 0;
+    if (sqlite3_exec(handle, select_oid, count_rows, &num_rows, &errmsg) != SQLITE_OK)
+    {
+        running_error("Error during query: %s\nQuery was: %s\n", errmsg, select_oid);
+    }
+    sqlite3_free(errmsg);
+
+    return (num_rows != 0);
+}
+
+static sqlite3_int64 insert_type_simple(storage_handle_t handle, type_t* t, const char* name)
+{
+    if (oid_already_inserted(handle, "type", t))
+        return (sqlite3_int64)t;
+
+    char * insert_type = sqlite3_mprintf("INSERT INTO type(oid, kind) VALUES (%p, %Q);", t, name);
     run_query(handle, insert_type);
-    int result = sqlite3_last_insert_rowid(handle);
+    sqlite3_int64 result = sqlite3_last_insert_rowid(handle);
     sqlite3_free(insert_type);
     return result;
 }
 
-static int insert_type_ref_to(storage_handle_t handle, const char* name, int ref_type)
+static sqlite3_int64 insert_type_ref_to(storage_handle_t handle, type_t* t, const char* name, sqlite3_int64 ref_type)
 {
-    char * insert_type = sqlite3_mprintf("INSERT INTO type(kind, ref_type) VALUES(%Q, '%d');", name, ref_type);
-    int result = sqlite3_last_insert_rowid(handle);
+    if (oid_already_inserted(handle, "type", t))
+        return (sqlite3_int64)t;
+
+    char * insert_type = sqlite3_mprintf("INSERT INTO type(oid, kind, ref_type) VALUES(%p, %Q, '%d');", t, name, ref_type);
+    sqlite3_int64 result = sqlite3_last_insert_rowid(handle);
     sqlite3_free(insert_type);
     return result;
 }
 
-static int insert_type_ref_to_list_types(storage_handle_t handle, 
+static sqlite3_int64 insert_type_ref_to_list_types(storage_handle_t handle, 
+        type_t* t,
         const char *name, 
-        int ref_type, 
-        int num_parameters, int *parameter_types)
+        sqlite3_int64 ref_type, 
+        sqlite3_int64 num_parameters, 
+        sqlite3_int64 *parameter_types)
 {
+    if (oid_already_inserted(handle, "type", t))
+        return (sqlite3_int64)t;
+
     char *list = sqlite3_mprintf("");
     int i;
     for (i = 0; i < num_parameters; i++)
@@ -164,33 +197,40 @@ static int insert_type_ref_to_list_types(storage_handle_t handle,
         }
     }
 
-    char * insert_type = sqlite3_mprintf("INSERT INTO type(kind, ref_type, types) VALUES(%Q, '%d', %Q);", 
-            name, ref_type, list);
-    int result = sqlite3_last_insert_rowid(handle);
+    char * insert_type = sqlite3_mprintf("INSERT INTO type(oid, kind, ref_type, types) VALUES(%p, %Q, '%d', %Q);", 
+            t, name, ref_type, list);
+    sqlite3_int64 result = sqlite3_last_insert_rowid(handle);
     sqlite3_free(insert_type);
     sqlite3_free(list);
     return result;
 }
 
-static int insert_type_ref_to_ast(storage_handle_t handle, 
+static sqlite3_int64 insert_type_ref_to_ast(storage_handle_t handle, 
+        type_t* t,
         const char* name, 
-        int ref_type, 
-        int ast0, 
-        int ast1)
+        sqlite3_int64 ref_type, 
+        sqlite3_int64 ast0, 
+        sqlite3_int64 ast1)
 {
-    char *insert_type = sqlite3_mprintf("INSERT INTO type(kind, ref_type, ast0, ast1) VALUES (%Q, %d, %d, %d);",
-            name, ref_type, ast0, ast1);
+    if (oid_already_inserted(handle, "type", t))
+        return (sqlite3_int64)t;
+
+    char *insert_type = sqlite3_mprintf("INSERT INTO type(oid, kind, ref_type, ast0, ast1) VALUES (%p, %Q, %d, %d, %d);",
+            t, name, ref_type, ast0, ast1);
     int result = sqlite3_last_insert_rowid(handle);
     sqlite3_free(insert_type);
     return result;
 }
 
-static int insert_ast(storage_handle_t handle, AST a)
+static sqlite3_int64 insert_ast(storage_handle_t handle, AST a)
 {
     if (a == NULL)
         return 0;
 
-    int children[MAX_AST_CHILDREN];
+    if (oid_already_inserted(handle, "type", a))
+        return (sqlite3_int64)a;
+
+    sqlite3_int64 children[MAX_AST_CHILDREN];
     memset(children, 0, sizeof(children));
 
     int i;
@@ -213,8 +253,9 @@ static int insert_ast(storage_handle_t handle, AST a)
         text = sqlite3_mprintf("%Q", ast_get_text(a));
     }
 
-    char *insert_node = sqlite3_mprintf("INSERT INTO ast (kind, file, line, text, ast0, ast1, ast2, ast3) "
-            "VALUES (%d, %q, %d, %s, %d, %d, %d, %d);",
+    char *insert_node = sqlite3_mprintf("INSERT INTO ast (oid, kind, file, line, text, ast0, ast1, ast2, ast3) "
+            "VALUES (%p, %d, %q, %d, %s, %d, %d, %d, %d);",
+            a,
             ast_get_type(a),
             ast_get_filename(a),
             ast_get_line(a),
@@ -225,52 +266,54 @@ static int insert_ast(storage_handle_t handle, AST a)
             children[3]);
 
     run_query(handle, insert_node);
-    int result = sqlite3_last_insert_rowid(handle);
+    sqlite3_int64 result = sqlite3_last_insert_rowid(handle);
     sqlite3_free(insert_node);
     sqlite3_free(text);
     return result;
 }
 
-static int insert_type(storage_handle_t handle, type_t* t)
+static sqlite3_int64 insert_type(storage_handle_t handle, type_t* t)
 {
-    int result = -1;
+    sqlite3_int64 result = 0;
+    if (t == NULL)
+        return result;
 
     if (is_any_int_type(t))
     {
         char * name = sqlite3_mprintf("INTEGER(%d)", type_get_size(t));
-        result = insert_type_simple(handle, name);
+        result = insert_type_simple(handle, t, name);
         sqlite3_free(name);
     }
     else if (is_bool_type(t))
     {
         char * name = sqlite3_mprintf("LOGICAL(%d)", type_get_size(t));
-        result = insert_type_simple(handle, name);
+        result = insert_type_simple(handle, t, name);
         sqlite3_free(name);
     }
     else if (is_floating_type(t))
     {
         char * name = sqlite3_mprintf("REAL(%d)", type_get_size(t));
-        result = insert_type_simple(handle, name);
+        result = insert_type_simple(handle, t, name);
         sqlite3_free(name);
     }
     else if (is_complex_type(t))
     {
         char * name = sqlite3_mprintf("COMPLEX(%d)", type_get_size(complex_type_get_base_type(t)));
-        result = insert_type_simple(handle, name);
+        result = insert_type_simple(handle, t, name);
         sqlite3_free(name);
     }
     else if (is_pointer_type(t))
     {
         result = insert_type(handle, pointer_type_get_pointee_type(t));
         const char *name = "POINTER";
-        result = insert_type_ref_to(handle, name, result);
+        result = insert_type_ref_to(handle, t, name, result);
     }
     else if (is_function_type(t))
     {
         const char *name = "FUNCTION";
         // +1 if the num parameters is zero
         int num_parameters = function_type_get_num_parameters(t);
-        int parameter_types[num_parameters + 1];
+        sqlite3_int64 parameter_types[num_parameters + 1];
 
         int i;
         for (i = 0; i < num_parameters; i++)
@@ -284,14 +327,14 @@ static int insert_type(storage_handle_t handle, type_t* t)
             result = insert_type(handle, function_type_get_return_type(t));
         }
 
-        result = insert_type_ref_to_list_types(handle, name, result, num_parameters, parameter_types);
+        result = insert_type_ref_to_list_types(handle, t, name, result, num_parameters, parameter_types);
     }
     else if (is_array_type(t))
     {
         const char *name = "ARRAY";
 
-        int lower_tree = 0;
-        int upper_tree = 0;
+        sqlite3_int64 lower_tree = 0;
+        sqlite3_int64 upper_tree = 0;
         if (!array_type_is_unknown_size(t))
         {
             lower_tree = insert_ast(handle, array_type_get_array_lower_bound(t));
@@ -300,7 +343,7 @@ static int insert_type(storage_handle_t handle, type_t* t)
 
         result = insert_type(handle, array_type_get_element_type(t));
 
-        result = insert_type_ref_to_ast(handle, name, result, lower_tree, upper_tree);
+        result = insert_type_ref_to_ast(handle, t, name, result, lower_tree, upper_tree);
     }
     else if (is_class_type(t))
     {
@@ -309,7 +352,7 @@ static int insert_type(storage_handle_t handle, type_t* t)
         int i = 0;
         int num_fields = class_type_get_num_nonstatic_data_members(t);
 
-        int field_list[num_fields+1];
+        sqlite3_int64 field_list[num_fields+1];
         memset(field_list, 0, sizeof(field_list));
 
         for (i = 0; i < num_fields; i++)
@@ -319,7 +362,7 @@ static int insert_type(storage_handle_t handle, type_t* t)
             field_list[i] = insert_symbol(handle, field);
         }
 
-        result = insert_type_ref_to_list_types(handle, name, 0, num_fields, field_list);
+        result = insert_type_ref_to_list_types(handle, t, name, 0, num_fields, field_list);
     }
     else
     {
@@ -329,18 +372,69 @@ static int insert_type(storage_handle_t handle, type_t* t)
     return result;
 }
 
-static int insert_symbol(storage_handle_t handle, scope_entry_t* symbol)
+static void insert_extra_attr_int(storage_handle_t handle, scope_entry_t* symbol, const char* name, sqlite3_int64 value)
 {
-    // char * attribute_values = symbol_get_attribute_values(symbol);
-    // char * insert_symbol = sqlite3_mprintf("INSERT INTO symbol(name, kind, type, file, line, %s) "
-    //         "VALUES (%Q, %Q, %d, %Q, %D, %s);",
-    //         symbol->symbol_name,
-    //         symbol->kind,
-    //         type_id,
-    //         attribute_values);
+    char *insert_extra_attr = sqlite3_mprintf("INSERT INTO attributes(symbol, name, value) VALUES(%p, %Q, %d);",
+           symbol, name, value); 
+    run_query(handle, insert_extra_attr);
+    sqlite3_free(insert_extra_attr);
+}
 
-    // sqlite3_free(insert_symbol);
-    // sqlite3_free(attribute_values);
+static void insert_extra_attr_data(storage_handle_t handle, scope_entry_t* symbol, const char* name, void* data,
+        sqlite3_int64 (*fun)(storage_handle_t handle, void* data))
+{
+    sqlite3_int64 m = fun(handle, data);
+    char *insert_extra_attr = sqlite3_mprintf("INSERT INTO attributes(symbol, name, value) VALUES(%p, %Q, %d);",
+           symbol, name, m); 
+    run_query(handle, insert_extra_attr);
+    sqlite3_free(insert_extra_attr);
+}
+
+static void insert_extra_attr_symbol(storage_handle_t handle, scope_entry_t* symbol, const char* name,
+        scope_entry_t* ref)
+{
+    insert_extra_attr_data(handle, symbol, name, ref, 
+            (sqlite3_int64(*)(storage_handle_t, void*))(insert_symbol));
+}
+
+static void insert_extra_attr_type(storage_handle_t handle, scope_entry_t* symbol, const char* name,
+        type_t* ref)
+{
+    insert_extra_attr_data(handle, symbol, name, ref, 
+            (sqlite3_int64(*)(storage_handle_t, void*))(insert_type));
+}
+
+UNUSED_PARAMETER static void insert_extra_attr_ast(storage_handle_t handle, scope_entry_t* symbol, const char* name,
+        AST ref)
+{
+    insert_extra_attr_data(handle, symbol, name, ref, 
+            (sqlite3_int64(*)(storage_handle_t, void*))(insert_ast));
+}
+
+#include "fortran03-modules-bits.h"
+
+static sqlite3_int64 insert_symbol(storage_handle_t handle, scope_entry_t* symbol)
+{
+    if (oid_already_inserted(handle, "symbol", symbol))
+        return (sqlite3_int64)symbol;
+
+    char * attribute_values = symbol_get_attribute_values(handle, symbol);
+
+    sqlite3_int64 type_id = insert_type(handle, symbol->type_information);
+
+    char * insert_symbol_query = sqlite3_mprintf("INSERT INTO symbol(oid, name, kind, type, file, line, %s) "
+            "VALUES (%p, %Q, %Q, %d, %Q, %d, %s);",
+            attr_field_names,
+            symbol,
+            symbol->symbol_name,
+            symbol->kind,
+            type_id,
+            attribute_values);
+
+    insert_extended_attributes(handle, symbol);
+
+    sqlite3_free(insert_symbol_query);
+    sqlite3_free(attribute_values);
 
     return 0;
 }
--- src/frontend/gen-symbol-attrs.py
+++ src/frontend/gen-symbol-attrs.py
@@ -130,9 +130,55 @@ typedef struct entity_specifiers_tag\n{"""
     print ""
     print "#endif"
 
+def insert_extra_attr_code(_type, name, suffix):
+    _insert_code = []
+    if (_type == "integer") :
+        _insert_code.append("insert_extra_attr_int(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type == "bool"):
+        _insert_code.append("insert_extra_attr_int(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type == "string"):
+        _insert_code.append("insert_extra_attr_string(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type == "AST"):
+        _insert_code.append("insert_extra_attr_ast(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type == "type"):
+        _insert_code.append("insert_extra_attr_type(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type == "symbol"):
+        _insert_code.append("insert_extra_attr_symbol(handle, sym, \"" + name + "\", sym->entity_specs." + name + suffix + ");")
+    elif (_type.startswith("array") or _type.startswith("static_array")):
+        if _type.startswith("array"):
+            type_name = get_up_to_matching_paren(_type[len("array"):])
+        elif _type.startswith("static_array"):
+            type_name = get_up_to_matching_paren(_type[len("static_array"):])
+        field_names = name.split(",")
+        if (len(field_names) == 1):
+           num_name = "num_" + name
+           list_name = name
+        elif (len(field_names) == 2):
+            num_name = field_names[0]
+            list_name = field_names[1]
+        else:
+            raise Exception("Invalid number of fields in array name. Only 1 or 2 comma-separated are allowed")
+        _insert_code.append(insert_extra_attr_code("integer", num_name, "")[0])
+        _insert_code.append("{ int i; for (i = 0; i < sym->entity_specs." + num_name + "; i++) {");
+        _insert_code = _insert_code + insert_extra_attr_code(type_name, list_name, "[i]");
+        _insert_code.append("} }");
+    elif (_type.startswith("typeof")):
+        type_name = get_up_to_matching_paren(_type[len("typeof"):])
+        if type_name == "gather_gcc_attribute_t" :
+            pass
+        elif type_name == "default_argument_info_t*" :
+            pass
+        else:
+            sys.stderr.write("%s: warning: unknown typeof '%s'\n" % (sys.argv[0], type_name))
+    else:
+        pass
+    return _insert_code
+
+
 def print_fortran_modules_functions(lines):
     attr_names = []
     _format = []
+    _insertion_code = [];
     for l in lines:
       fields = l.split("|");
       (_type,language,name,description) = fields
@@ -144,26 +190,55 @@ def print_fortran_modules_functions(lines):
           _format.append("%d")
       elif (_type == "AST"):
           attr_names.append(name)
-          _format.append("%d")
+          _format.append("%p")
+          _insertion_code.append("    insert_ast(handle, sym->entity_specs." + name + ");");
       elif (_type == "type"):
           attr_names.append(name)
-          _format.append("%d")
+          _format.append("%p")
+          _insertion_code.append("    insert_type(handle, sym->entity_specs." + name + ");");
       elif (_type == "string"):
           attr_names.append(name)
           _format.append("%Q")
+      elif (_type.startswith("typeof")):
+            type_name = get_up_to_matching_paren(_type[len("typeof"):])
+            if type_name == "intent_kind_t" or type_name == "access_specifier_t":
+                attr_names.append(name)
+                _format.append("%d")
+            elif type_name == "_size_t":
+                attr_names.append(name)
+                _format.append("%lld")
+            else:
+                sys.stderr.write("%s: warning: unknown typeof '%s'\n" % (sys.argv[0], type_name))
       else:
           pass
     print "#ifndef FORTRAN03_MODULES_BITS_H"
     print "#define FORTRAN03_MODULES_BITS_H"
     print ""
     print "static const char * attr_field_names = \"" + string.join(attr_names, ", ") + "\";";
-    print "static char * symbol_get_attribute_values(scope_entry_t* sym)"
+    print "static char * symbol_get_attribute_values(storage_handle_t handle, scope_entry_t* sym)"
     print "{"
     print "    const char *format = \"" + string.join(_format, ", ") + "\";"
+    print ""
+    print string.join(_insertion_code, "\n");
+    print ""
     print "    char * result = sqlite3_mprintf(format, " + string.join(map(lambda x : "sym->entity_specs." + x, attr_names), ", ") + ");"
     print "    return result;"
     print "}"
+    _extra_attr_code = []
+    for l in lines:
+      fields = l.split("|");
+      (_type,language,name,description) = fields
+      if (_type.startswith("array")):
+          _extra_attr_code = _extra_attr_code + insert_extra_attr_code(_type, name, "")
+      else:
+           pass
+    print "static void insert_extended_attributes(storage_handle_t handle, scope_entry_t* sym)"
+    print "{"
+    print string.join(_extra_attr_code, "\n");
+    print "}"
+    print ""
     print "#endif // FORTRAN03_MODULES_BITS_H"
+
 
 
 lines = loadlines(f)
