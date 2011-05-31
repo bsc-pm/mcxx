e700f1f94155ec8a19b83aa1311c9d4907ebcecb
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2011-05-31 17:35:01 +0200

    Fix grammar for a convoluted typename case
    Also rewrite nested_name_specifier in a more correct way

---
+++ tests/01_parser.dg/success_132.cpp
@@ -0,0 +1,59 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2011 Barcelona Supercomputing Center 
+                          Centro Nacional de Supercomputacion
+  
+  This file is part of Mercurium C/C++ source-to-source compiler.
+  
+  See AUTHORS file in the top level directory for information 
+  regarding developers and contributors.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+
+/*
+<testinfo>
+test_generator=config/mercurium
+</testinfo>
+*/
+
+template <typename S>
+struct A
+{
+    template <typename T>
+        struct _nx_data_env_0_t
+        {
+            T t;
+        };
+
+    template <typename T> 
+        static void _smp__ol_foo_0(typename ::A< S>::template _nx_data_env_0_t< T> * const __restrict__ args)
+        {
+            args->t = 3.5f;
+        }
+};
+
+void g()
+{
+    A<int> a;
+
+    A<int>::_nx_data_env_0_t<float> b;
+    b.t = 3.4f;
+
+    a._smp__ol_foo_0(&b);
+}
+
