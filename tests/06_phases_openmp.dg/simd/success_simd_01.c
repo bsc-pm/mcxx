d7e132bc959f270b7606030d64c474e2d499e452
Diego Caballero <diego.caballero@bsc.es>
2011-07-26 12:21:22 +0200

    Add SIMD test
    See #636

---
+++ tests/06_phases_openmp.dg/simd/success_simd_01.c
@@ -0,0 +1,76 @@
+/*
+<testinfo>
+test_generator=config/mercurium-simd
+</testinfo>
+*/
+
+int test(void)
+{
+    int i;
+    unsigned char a[102];
+    float b[102];
+
+
+#pragma omp task shared(i, a, b)
+    {
+    
+#pragma hlt simd (a)
+        for (i=0; i<101; i++)
+        {
+            a[i] = (unsigned char)2;
+        }
+
+#pragma hlt simd (b)
+        for (i=0; i<101; i++)
+        {
+            b[i] = 10.0f;
+        }
+
+        a[101] = 8;
+        b[101] = 7.0f;
+
+#pragma hlt simd (a, b)
+        for (i=0; i<101; i++)
+        {
+            b[i] += 6.0f;
+            a[i] = b[i];
+
+        }
+    }
+#pragma omp taskwait
+
+    for (i=0; i<101; i++)
+    {
+        if (a[i] != 16)
+        {
+            printf("ERROR: a[%d] == %d\n", i, a[i]);
+            return 1;
+        }
+
+        if (b[i] != 16)
+        {
+            printf("ERROR: b[%d] == %d\n", i, a[i]);
+            return 1;
+        }
+
+    }
+
+    if (a[101] != 8)
+    {
+        printf("ERROR: a[%d] == %d\n", i, a[101]);
+        return 1;
+    }
+
+    if (b[101] != 7.0f)
+    {
+        printf("ERROR: b[%d] == %d\n", i, a[101]);
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    return test();
+}
