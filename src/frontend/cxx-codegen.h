1a51e9583ad51f8724113a8a4b547fa7a06b9f50
sroyuela <sara.royuela@bsc.es>
2011-07-27 17:48:21 +0200

    Fix some bugs in nodecl

--- src/frontend/cxx-codegen.h
+++ src/frontend/cxx-codegen.h
@@ -3,10 +3,15 @@
 
 #include <stdio.h>
 #include "cxx-ast.h"
+#include "cxx-macros.h"
 #include "cxx-scopelink.h"
 #include "libmcxx-common.h"
 
-void c_cxx_codegen_translation_unit(FILE* f, nodecl_t a, scope_link_t* sl);
-char* c_cxx_codegen_to_str(nodecl_t node);
+MCXX_BEGIN_DECLS
+
+LIBMCXX_EXTERN void c_cxx_codegen_translation_unit(FILE* f, nodecl_t a, scope_link_t* sl);
+LIBMCXX_EXTERN char* c_cxx_codegen_to_str(nodecl_t node);
+
+MCXX_END_DECLS
 
 #endif // CXX_CODEGEN_H
