6eef1a3b0e7b25f0ccd2b7c312f5ce65c8c051f5
sroyuela <sara.royuela@bsc.es>
2011-07-27 17:59:35 +0200

    Fix some bugs in nodecl
    Conflicts:
    
    	src/frontend/nodecl-generator.py

--- src/frontend/fortran/fortran03-codegen.h
+++ src/frontend/fortran/fortran03-codegen.h
@@ -6,8 +6,12 @@
 #include "cxx-scopelink.h"
 #include "libmf03-common.h"
 
+MCXX_BEGIN_DECLS
+
 LIBMCXX_EXTERN void fortran_codegen_translation_unit(FILE* f, nodecl_t a, scope_link_t* sl);
 LIBMCXX_EXTERN char* fortran_codegen_to_str(nodecl_t node);
+
+MCXX_END_DECLS
 
 #endif // FORTRAN03_CODEGEN_H
 
