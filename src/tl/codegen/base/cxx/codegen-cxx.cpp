179e5921a7d9a4c9692e89fc52f9961e7f2a1702
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2011-11-03 09:52:29 +0100

    Be more string when error expressions slip in codegen of a file

--- src/tl/codegen/base/cxx/codegen-cxx.cpp
+++ src/tl/codegen/base/cxx/codegen-cxx.cpp
@@ -617,7 +617,8 @@ CxxBase::Ret CxxBase::visit(const Nodecl::ErrExpr& node)
     }
     else
     {
-        internal_error("Error expression found when the output is a file", 0);
+        internal_error("%s: error: error expression found when the output is a file", 
+                node.get_locus().c_str());
     }
 }
 
