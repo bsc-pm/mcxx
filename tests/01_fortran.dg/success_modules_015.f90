f0d39f0fffd2fac6c88cb965419e0dbb8c82d568
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2012-01-02 12:25:57 +0100

    Module tests are compiled with and without cache

--- tests/01_fortran.dg/success_modules_015.f90
+++ tests/01_fortran.dg/success_modules_015.f90
@@ -24,10 +24,14 @@
 !   Cambridge, MA 02139, USA.
 ! --------------------------------------------------------------------
 
-
 ! <testinfo>
 ! test_generator=config/mercurium-fortran
+! compile_versions="cache nocache"
+! test_FFLAGS_cache=""
+! test_FFLAGS_nocache="--debug-flags=disable_module_cache"
 ! </testinfo>
+
+
 MODULE M
 IMPLICIT NONE
 PUBLIC :: X
