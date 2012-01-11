115244738b199d490e14f5b6379c9bbc87f549a0
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-01-11 12:38:18 +0100

    No need to insert the symbol in the enclosing scope
    This already happens now

--- src/frontend/fortran/fortran03-buildscope.c
+++ src/frontend/fortran/fortran03-buildscope.c
@@ -1351,21 +1351,14 @@ static scope_entry_t* new_procedure_symbol(
 
         if (enclosing_symbol != NULL)
         {
-            if (enclosing_symbol->kind == SK_FUNCTION
-                    || enclosing_symbol->kind == SK_PROGRAM
-                    || enclosing_symbol->kind == SK_MODULE)
+            if (enclosing_symbol->kind == SK_MODULE)
             {
-                insert_entry(enclosing_symbol->decl_context.current_scope, entry);
+                // If we are enclosed by a module, we are a module procedure
+                entry->entity_specs.in_module = enclosing_symbol;
 
-                if (enclosing_symbol->kind == SK_MODULE)
-                {
-                    // If we are enclosed by a module, we are a module procedure
-                    entry->entity_specs.in_module = enclosing_symbol;
-
-                    P_LIST_ADD_ONCE(enclosing_symbol->entity_specs.related_symbols,
-                            enclosing_symbol->entity_specs.num_related_symbols,
-                            entry);
-                }
+                P_LIST_ADD_ONCE(enclosing_symbol->entity_specs.related_symbols,
+                        enclosing_symbol->entity_specs.num_related_symbols,
+                        entry);
             }
         }
     }
