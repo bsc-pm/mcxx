9e47b20dc3251c8225ddc5e6d08e329b938d9d31
Xavier Teruel <xavier.teruel@bsc.es>
2012-01-27 15:58:44 +0100

    Fixes for tests

--- tests/06_phases_openmp.dg/common/cxx/success_new_udr_17.cpp
+++ tests/06_phases_openmp.dg/common/cxx/success_new_udr_17.cpp
@@ -29,13 +29,15 @@
 <testinfo>
 test_generator=config/mercurium-omp
 
-# test_compile_fail_nanox_plain=yes
-# test_compile_faulty_nanox_plain=yes
-# 
-# test_compile_fail_nanox_instrument=yes
-# test_compile_faulty_nanox_instrument=yes
+test_compile_fail_nanox_plain=yes
+test_compile_faulty_nanox_plain=yes
+
+test_compile_fail_nanox_instrument=yes
+test_compile_faulty_nanox_instrument=yes
 </testinfo>
 */
+
+#include "omp.h"
 
 enum A
 {
