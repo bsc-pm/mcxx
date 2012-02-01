cc37c994b91046a5a8086c3ab8b8f7f94a8fc219
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-02-01 10:15:16 +0100

    Remove old code

--- src/tl/omp/nanox/tl-parallel.cpp
+++ src/tl/omp/nanox/tl-parallel.cpp
@@ -37,21 +37,6 @@ using namespace TL::Nanox;
 
 void OMPTransform::parallel_postorder(PragmaCustomConstruct ctr)
 {
-#ifdef OMPSS
-    std::cerr << ctr.get_ast().get_locus() << ": warning: '#pragma omp parallel' is not fully implemented" << std::endl;
-
-    Source code;
-
-    code 
-        << "{"
-        <<    ctr.get_statement()
-        <<    get_barrier_code(ctr.get_ast())
-        << "}"
-        ;
-
-    AST_t parallel_code = code.parse_statement(ctr.get_ast(), ctr.get_scope_link());
-    ctr.get_ast().replace(parallel_code);
-#endif
     OpenMP::DataSharingEnvironment& data_sharing = openmp_info->get_data_sharing(ctr.get_ast());
 
     DataEnvironInfo data_environ_info;
