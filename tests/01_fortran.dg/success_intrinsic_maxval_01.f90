9a1788d1a2a3e349c567a70ccd9d57d59acba1ac
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-04-04 10:45:41 +0200

    New tests for {MAX,MIN}VAL

---
+++ tests/01_fortran.dg/success_intrinsic_maxval_01.f90
@@ -0,0 +1,36 @@
+! <testinfo>
+! test_generator="config/mercurium-fortran run"
+! </testinfo>
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER :: I
+
+    INTEGER, PARAMETER :: A(3, 3, 3) = RESHAPE( (/ (I, I=1,27) /), &
+         SHAPE = (/ 3, 3, 3 /) )
+     INTEGER :: B (3, 3), B1(3, 3)
+     INTEGER :: C
+
+     C = 27
+     IF (MAXVAL(A) /= C) STOP "INVALID MAXVAL SCALAR"
+     C = 1
+     IF (MINVAL(A) /= C) STOP "INVALID MINVAL SCALAR"
+
+     B1 = MAXVAL(A, DIM=1)
+     B = RESHAPE( SOURCE=(/ (/ 3, 6, 9 /), (/ 12, 15, 18 /), (/ 21, 24, 27 /) /), SHAPE = (/ 3, 3 /) ) 
+
+     IF ( ANY( B1 /=  B ) ) THEN
+         STOP "INVALID MAXVAL DIM=1"
+     END IF
+
+     B1 = MAXVAL(A, DIM=2)
+     B = RESHAPE( SOURCE=(/ (/ 7, 8, 9 /), (/ 16, 17, 18 /), (/ 25, 26, 27 /) /), SHAPE = (/ 3, 3 /) )
+     IF ( ANY( B1 /=  B ) ) THEN
+         STOP "INVALID MAXVAL DIM=2"
+     END IF
+  
+     B1 = MAXVAL(A, DIM=3)
+     B = RESHAPE( SOURCE=(/ (/ 19, 20, 21 /), (/ 22, 23, 24 /), (/ 25, 26, 27 /) /), SHAPE = (/ 3, 3 /) )
+     IF ( ANY( B1 /=  B ) ) THEN
+         STOP "INVALID MAXVAL DIM=3"
+     END IF
+END PROGRAM P
