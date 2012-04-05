d55b531fad3a03d0247f03a27746802f76c5940c
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-04-05 17:07:08 +0200

    This name is not valid in Fortran

--- src/frontend/cxx-buildscope.c
+++ src/frontend/cxx-buildscope.c
@@ -6381,7 +6381,7 @@ static void set_array_type(type_t** declarator_type,
                 static int vla_counter = 0;
 
                 const char* vla_name = NULL; 
-                uniquestr_sprintf(&vla_name, "__vla_%d", vla_counter);
+                uniquestr_sprintf(&vla_name, "mcc_vla_%d", vla_counter);
                 vla_counter++;
 
                 scope_entry_t* new_vla_dim = new_symbol(decl_context, decl_context.current_scope, vla_name);
