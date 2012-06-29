bsc-pm/mcxx

commit 3829a811fd55e55466abff282fc95ffdd2f89f59

Handle value-dependent initializers like we handle type-dependent
Due to a subtle bug in g++ we have to faithfully keep theses initializers
as well

src/frontend/cxx-exprtype.c
@@ -11270,9 +11270,16 @@ static void compute_nodecl_initializer_clause(AST initializer, decl_context_t de
         default:
             {
                 check_expression_impl_(initializer, decl_context, nodecl_output);
+                char is_type_dependent = nodecl_expr_is_type_dependent(*nodecl_output);
+                char is_value_dependent = nodecl_expr_is_value_dependent(*nodecl_output);
+
                 *nodecl_output = nodecl_make_cxx_initializer(*nodecl_output, 
                         nodecl_get_filename(*nodecl_output), 
                         nodecl_get_line(*nodecl_output));
+
+                // Propagate attributes as needed
+                nodecl_expr_set_is_type_dependent(*nodecl_output, is_type_dependent);
+                nodecl_expr_set_is_value_dependent(*nodecl_output, is_value_dependent);
                 break;
             }
         case AST_INITIALIZER_BRACES:
@@ -11628,11 +11635,13 @@ static void compute_nodecl_equal_initializer(AST initializer, decl_context_t dec
     if (!nodecl_is_err_expr(*nodecl_output))
     {
         char is_type_dependent = nodecl_expr_is_type_dependent(*nodecl_output);
+        char is_value_dependent = nodecl_expr_is_value_dependent(*nodecl_output);
         type_t* t = nodecl_get_type(*nodecl_output);
 
         *nodecl_output = nodecl_make_cxx_equal_initializer(*nodecl_output, 
                 ASTFileName(initializer), ASTLine(initializer));
         nodecl_expr_set_is_type_dependent(*nodecl_output, is_type_dependent);
+        nodecl_expr_set_is_value_dependent(*nodecl_output, is_value_dependent);
 
         nodecl_set_type(*nodecl_output, t);
     }
@@ -11868,7 +11877,8 @@ void check_nodecl_expr_initializer(nodecl_t nodecl_expr,
     type_t* initializer_expr_type = nodecl_get_type(nodecl_expr);
     type_t* declared_type_no_cv = get_unqualified_type(declared_type);
 
-    if (nodecl_expr_is_type_dependent(nodecl_expr))
+    if (nodecl_expr_is_type_dependent(nodecl_expr)
+            || nodecl_expr_is_value_dependent(nodecl_expr))
     {
         *nodecl_output = nodecl_expr;
         nodecl_set_type(*nodecl_output, declared_type_no_cv);
@@ -12092,8 +12102,16 @@ void check_nodecl_equal_initializer(nodecl_t nodecl_initializer,
 {
     ERROR_CONDITION(nodecl_get_kind(nodecl_initializer) != NODECL_CXX_EQUAL_INITIALIZER, "Invalid nodecl", 0);
 
-    nodecl_t nodecl_expr = nodecl_get_child(nodecl_initializer, 0);
-    check_nodecl_initializer_clause(nodecl_expr, decl_context, declared_type, nodecl_output);
+    if (nodecl_expr_is_type_dependent(nodecl_initializer)
+            || nodecl_expr_is_value_dependent(nodecl_initializer))
+    {
+        *nodecl_output = nodecl_initializer;
+    }
+    else
+    {
+        nodecl_t nodecl_expr = nodecl_get_child(nodecl_initializer, 0);
+        check_nodecl_initializer_clause(nodecl_expr, decl_context, declared_type, nodecl_output);
+    }
 }
 
 void check_initialization_nodecl(nodecl_t nodecl_initializer, decl_context_t decl_context, type_t* declared_type, nodecl_t* nodecl_output)

tests/01_parser.dg/success_166.cpp
@@ -1,0 +1,44 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2012 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+  
+  This file is part of Mercurium C/C++ source-to-source compiler.
+  
+  See AUTHORS file in the top level directory for information 
+  regarding developers and contributors.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+
+
+/*
+<testinfo>
+test_generator=config/mercurium
+</testinfo>
+*/
+template <int N>
+struct A {};
+
+template <int M>
+void f()
+{
+    // This cannot be emitted as const int N(M) due to a bug in g++
+    const int N = M;
+
+    A<N> b;
+}

commit 075851a4249c297f57fc78532a89d783df10d1b2

Fix issue introduced in commit 3829a811fd55e55466abff282fc95ffdd2f89f59
Preserve equal initializers only if their initializer-clause is just
an expression. Braced initializers must be preserved even
if they turn value-dependent.

src/frontend/cxx-exprtype.c
@@ -12102,9 +12102,13 @@ void check_nodecl_equal_initializer(nodecl_t nodecl_initializer,
 {
     ERROR_CONDITION(nodecl_get_kind(nodecl_initializer) != NODECL_CXX_EQUAL_INITIALIZER, "Invalid nodecl", 0);
 
-    if (nodecl_expr_is_type_dependent(nodecl_initializer)
-            || nodecl_expr_is_value_dependent(nodecl_initializer))
+    if (nodecl_get_kind(nodecl_initializer) == NODECL_CXX_INITIALIZER
+            && (nodecl_expr_is_type_dependent(nodecl_initializer)
+                || nodecl_expr_is_value_dependent(nodecl_initializer)))
     {
+        // If this is a simple expression that turns to be dependent, then
+        // preserve its equal initializer
+        // template <typename T> void f() { long t = sizeof(T); }
         *nodecl_output = nodecl_initializer;
     }
     else
@@ -12160,9 +12164,9 @@ static void check_nodecl_initializer_clause(nodecl_t initializer_clause,
 
     switch (nodecl_get_kind(initializer_clause))
     {
-        // Default should be an expression
         case NODECL_CXX_INITIALIZER:
             {
+                // This node wraps a simple expression
                 check_nodecl_expr_initializer(
                         nodecl_get_child(initializer_clause, 0), 
                         decl_context, declared_type, nodecl_output);