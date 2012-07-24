cccbe98703d214a1b217d9b00d18cfaa24c27ad2
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-07-24 08:54:48 +0200

    Missing header

---
+++ src/frontend/cxx-nodecl-deep-copy-fwd.h
@@ -0,0 +1,8 @@
+#ifndef CXX_NODECL_DEEP_COPY_FWD_H
+#define CXX_NODECL_DEEP_COPY_FWD_H
+
+#include "cxx-scope-fwd.h"
+
+typedef struct symbol_map_tag symbol_map_t;
+
+#endif // CXX_NODECL_DEEP_COPY_FWD_H
