93dd6f773ae7f16a9d9610ada1d59e530ecca957
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2012-09-20 09:59:38 +0200

    Improve Fortran task tests

--- tests/07_phases_ompss.dg/fortran/success_task_04.f90
+++ tests/07_phases_ompss.dg/fortran/success_task_04.f90
@@ -10,12 +10,31 @@ subroutine fun_task(x, y)
    y(1) = y(1) + 1
 end subroutine
 
+subroutine fun_task_2(x, y)
+   implicit none
+   integer :: x, y(10)
+
+   IF (x /= 42) THEN
+       STOP 1
+   END IF
+
+   IF (y(1) /= 10042) THEN
+       STOP 2
+   END IF
+end subroutine
+
 program main
     implicit none
     interface
 
     !$omp task inout(x, y)
     subroutine fun_task(x, y)
+       integer :: x
+       integer :: y(10)
+    end subroutine
+
+    !$omp task in(x, y)
+    subroutine fun_task_2(x, y)
        integer :: x
        integer :: y(10)
     end subroutine
@@ -28,13 +47,8 @@ program main
    j = 10041
 
    call fun_task(i, j)
+   call fun_task_2(i, j)
+
    !$OMP TASKWAIT
 
-   IF (i /= 42) THEN
-       STOP 1
-   END IF
-
-   IF (j(1) /= 10042) THEN
-       STOP 2
-   END IF
 end program
