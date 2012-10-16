06d2d9ec0e2191198ad1dee21a0404d0599b1a03
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2012-10-16 15:31:18 +0200

    The name of a fortran namelist statement can be a member of a module
    It fixes #1190

--- src/frontend/fortran/fortran03-buildscope.c
+++ src/frontend/fortran/fortran03-buildscope.c
@@ -6434,7 +6434,20 @@ static void build_scope_namelist_stmt(AST a, decl_context_t decl_context,
         new_namelist->kind = SK_NAMELIST;
         new_namelist->file = ASTFileName(a);
         new_namelist->line = ASTLine(a);
-        
+
+        if (decl_context.current_scope->related_entry != NULL
+                && decl_context.current_scope->related_entry->kind == SK_MODULE)
+        {
+            // Make the new namelist a member of this module
+            scope_entry_t* module = decl_context.current_scope->related_entry;
+
+            P_LIST_ADD_ONCE(module->entity_specs.related_symbols,
+                    module->entity_specs.num_related_symbols,
+                    new_namelist);
+
+            new_namelist->entity_specs.in_module = module;
+        }
+
         remove_unknown_kind_symbol(decl_context, new_namelist);
 
         AST it2;
