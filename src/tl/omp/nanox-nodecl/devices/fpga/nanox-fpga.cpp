6f409d0b94fe67f60f2f5a7ab9108cc81a58aa57
Antonio FIlgueras <afilguer@afilguer-Latitude-E6410.(none)>
2012-10-23 18:02:08 +0200

    Initial fpga host code generation

--- src/tl/omp/nanox-nodecl/devices/fpga/nanox-fpga.cpp
+++ src/tl/omp/nanox-nodecl/devices/fpga/nanox-fpga.cpp
@@ -27,12 +27,407 @@
 #include "tl-devices.hpp"
 #include "nanox-fpga.hpp"
 
+#include "tl-nanos.hpp"
+
+
+
 namespace TL { namespace Nanox {
 
+void DeviceFPGA::iold_create_outline(
+
+		const std::string& task_name,
+		const std::string& struct_typename,
+		DataEnvironInfo &data_environ,
+		const OutlineFlags& outline_flags,
+		AST_t reference_tree,
+		ScopeLink sl,
+		Source initial_setup,
+		Source outline_body)
+{
+	/***************** Write the AutoPilot file *****************/
+
+	// Check if the file has already been created (and written)
+	//bool new_file = false;
+
+
+	if (_autoPilotFilename == "") {
+		// Set the file name
+		//_autoPilotFilename = "autoPilot_";
+		_autoPilotFilename = "accelerator_core.c";
+		//_autoPilotFilename += CompilationProcess::get_current_file().get_filename(false);
+		//size_t file_extension = _autoPilotFilename.find_last_of(".");
+		//_autoPilotFilename.erase(file_extension, _autoPilotFilename.length());
+		//_autoPilotFilename += ".c";
+		new_file = true;
+
+	
+		
+		// Remove the intermediate source file
+		mark_file_for_cleanup( _autoPilotFilename.c_str() );
+	}
+
+	const std::string configuration_name = "fpga_task";
+	//const std::string configuration_name = "fpga";
+	//
+	////FIXME: Patch done for autopilot
+	//MOVED to have task name as name of the file
+	//CompilationProcess::add_file(_autoPilotFilename, configuration_name, new_file);
+	
+	//FIXME: Process includes to autopilot files? 
+	// Check it when we get autopilot
+	Source included_files, forward_declaration;
+	AST_t function_tree;
+
+
+	
+	// Check if the task is a function, or it is inlined
+	if (outline_flags.task_symbol != NULL)
+	{
+		// Get the definition of non local symbols[M#J
+		function_tree = outline_flags.task_symbol.get_point_of_declaration();
+		LangConstruct construct (function_tree, sl);
+		ObjectList<IdExpression> extern_occurrences = construct.non_local_symbol_occurrences();
+		DeclarationClosure decl_closure (sl);
+		std::set<Symbol> extern_symbols;
+
+  
+		for (ObjectList<IdExpression>::iterator it = extern_occurrences.begin();
+				it != extern_occurrences.end();
+				it++)
+		{
+			Symbol s = (*it).get_symbol();
+			decl_closure.add(s);
+
+			// TODO: check the symbol is not a global variable
+			extern_symbols.insert(s);
+		}
+
+		forward_declaration << decl_closure.closure() << "\n";
+
+		for (std::set<Symbol>::iterator it = extern_symbols.begin();
+				it != extern_symbols.end(); it++)
+		{
+			forward_declaration << (*it).get_point_of_declaration().prettyprint_external() << "\n";
+		}
+
+		// Check if the task symbol is actually a function definition or a declaration
+		if (FunctionDefinition::predicate(function_tree))
+		{
+			// Check if we have already printed the function definition in the AutoPilot file
+			if (_taskSymbols.count(outline_flags.task_symbol.get_name()) == 0) {
+				forward_declaration << function_tree.get_enclosing_function_definition(false).prettyprint_external();
+
+				// Keep record of which tasks have been printed to the AutoPilot file
+				// in order to avoid repeating them
+				_taskSymbols.insert(outline_flags.task_symbol.get_name());
+				
+				// TODO:Changing entry point. FIXME: Naming problem:
+				//_autoPilotFilename = outline_flags.task_symbol.get_name();	
+				//_autoPilotFilename += "_entry.c";
+				_autoPilotFilename = "accelerator_core.c"; //outline_flags.task_symbol.get_name();	
+				
+				std::cout << "Filename: " << _autoPilotFilename << std::endl;
+
+				Type funct_type = outline_flags.task_symbol.get_type();
+
+
+				ObjectList<Type> params = funct_type.nonadjusted_parameters();
+
+				std::stringstream string_params, params_func_call;
+				std::stringstream pragma_insertion;
+
+				string_params << "(";
+				unsigned int k = 0;
+				for (ObjectList<Type>::iterator it = params.begin();
+						it != params.end();
+						it++, k++){
+
+				  		//std::cerr << "NONADJUSTED -> " << (*it).get_declaration(outline_flags.task_symbol.get_scope(), task_param_list_vector[k]) << std::endl; 
+
+						string_params << (*it).get_declaration(outline_flags.task_symbol.get_scope(), task_param_list_vector[k]);
+
+						params_func_call << task_param_list_vector[k];
+						
+						pragma_insertion << "#pragma AP RESOURCE variable=" << task_param_list_vector[k] << " core=SPRAMD\n";
+
+						if (task_param_list_vector.size() - 1 != k)
+						{
+							string_params << ", ";
+							params_func_call << ", ";
+						}
+				}
+				string_params << "){\n " << pragma_insertion.str() << outline_flags.task_symbol.get_name() << "(" << params_func_call.str() << ");\n}";
+
+				//std::cerr << "void " << outline_flags.task_symbol.get_name() << "_entry"<< string_params.str();
+				
+				//FIXME: Naming problem:
+				//forward_declaration << "\n\nvoid " << outline_flags.task_symbol.get_name() << "_entry"<< string_params.str();
+				forward_declaration << "\n\nvoid accelerator_core"<< string_params.str();
+
+			}
+
+			//FIXME:
+			//exit(-2);
+
+			// Remove the function definition from the original source code
+			function_tree.remove_in_list();
+		}
+		else
+		{
+			// Not a function definition
+			// Create a filter to search for the definition
+			struct FilterFunctionDef : Predicate<AST_t>
+			{
+				private:
+					Symbol _sym;
+					ScopeLink _sl;
+				public:
+					FilterFunctionDef(Symbol sym, ScopeLink sl)
+					: _sym(sym), _sl(sl) { }
+
+					virtual bool do_(const AST_t& a) const
+					{
+						if (!FunctionDefinition::predicate(a))
+							return false;
+
+						FunctionDefinition funct_def(a, _sl);
+
+						Symbol sym = funct_def.get_function_symbol();
+						return _sym == sym;
+					}
+			};
+
+			// Search for the function definition
+			ObjectList<AST_t> funct_def_list =
+					_root.depth_subtrees(FilterFunctionDef(outline_flags.task_symbol, sl));
+
+			if (funct_def_list.size() == 1)
+			{
+				// Check if we have already printed the function definition in the AutoPilot file
+				if (_taskSymbols.count(outline_flags.task_symbol.get_name()) == 0)
+				{
+					forward_declaration << funct_def_list[0].get_enclosing_function_definition(false).prettyprint_external();
+
+					//TODO: Print an entry point
+					//forward_declaration << "void " << outline_flags.task_symbol.get_name() <<  
+
+					// Keep record of which tasks have been printed to the AutoPilot file
+					// in order to avoid repeating them
+					_taskSymbols.insert(outline_flags.task_symbol.get_name());
+				
+				}
+
+
+
+				// Remove the function definition from the original source code
+				funct_def_list[0].remove_in_list();
+			}/*
+			else if (funct_def_list.size() == 0
+					&& _taskSymbols.count(outline_flags.task_symbol.get_name()) > 0)
+			{
+				// We have already removed it and printed it in the  AutoPilot file, do nothing
+			}*/
+			else
+			{
+				std::stringstream msg;
+				msg << "Could not find the task function definition of '"
+						<< outline_flags.task_symbol.get_name()
+						<< "'";
+				internal_error(msg.str().c_str(), 0);
+			}
+		}
+	}
+
+//FIXME: Nanox instrumentation? Check Judit's code
+	Source result, arguments_struct_definition, outline_name, parameter_list, body;
+AST_t function_def_tree = reference_tree.get_enclosing_function_definition();
+	FunctionDefinition enclosing_function(function_def_tree, sl);
+
+	// arguments_struct_definition
+	
+	bool new_file_temp = false;
+
+	CompilationProcess::add_file(_autoPilotFilename, configuration_name, new_file_temp);
+	//CompilationProcess::add_file(_autoPilotFilename, configuration_name, false);
+	
+	Scope sc = sl.get_scope(reference_tree);
+	Symbol struct_typename_sym = sc.get_symbol_from_name(struct_typename);
+
+	if (!struct_typename_sym.is_valid())
+	{
+		running_error("Invalid typename for struct args", 0);
+	}
+
+	arguments_struct_definition
+		<< struct_typename_sym.get_point_of_declaration().prettyprint();
+
+	// outline_name
+	outline_name
+		<< "_fpga_" + task_name
+		;
+
+	// parameter_list
+	parameter_list
+		<< struct_typename << "* _args"
+		;
+
+	// body
+	Source private_vars, final_code;
+
+	body
+		<< private_vars
+		<< initial_setup
+		<< outline_body
+		<< final_code
+		;
+
+	// private_vars
+	ObjectList<DataEnvironItem> data_env_items = data_environ.get_items();
+
+	for (ObjectList<DataEnvironItem>::iterator it = data_env_items.begin();
+			it != data_env_items.end();
+			it++)
+	{
+		if (!it->is_private())
+			continue;
+
+		Symbol sym = it->get_symbol();
+		Type type = sym.get_type();
+
+		private_vars
+			<< type.get_declaration(sym.get_scope(), sym.get_name()) << ";"
+			;
+	}
+
+	// final_code
+	if (outline_flags.barrier_at_end)
+	{
+		final_code
+			<< "nanos_team_barrier();"
+			;
+	}
+
+	if (outline_flags.leave_team)
+	{
+		final_code
+			<< "nanos_leave_team();"
+			;
+	}
+
+	// Parse it in a sibling function context
+	AST_t outline_code_tree =
+			result.parse_declaration(enclosing_function.get_ast(), sl);
+
+	std::ofstream autoPilotFile;
+	if (new_file) //&& !file_opened)
+	{
+		autoPilotFile.open (_autoPilotFilename.c_str(), std::ios_base::app);
+		autoPilotFile << included_files.get_source(false) << "\n";
+	}
+	else
+	{
+		autoPilotFile.open (_autoPilotFilename.c_str(), std::ios_base::app);
+	}
+
+//	file_opened = true;
+
+	//autoPilotFile << "extern \"C\" {\n";
+	autoPilotFile << forward_declaration.get_source(false) << "\n";
+	//autoPiloFile << outline_code_tree.prettyprint_external() << "\n";
+	autoPilotFile << outline_code_tree.prettyprint() << "\n";
+	//autoPilotFile << "}\n";
+	autoPilotFile.close();
+
+
+/******************* Write the C file ******************/
+	// Check if the task is a function, or it is inlined
+	if (outline_flags.task_symbol != NULL)
+	{
+		// We have already removed the function definition
+		// Now replace it for the outline declaration
+		Source function_decl_src;
+/*
+		CXX_LANGUAGE()
+		{
+			function_decl_src
+				<< "extern \"C\" { "
+				;
+		}
+*/
+		function_decl_src
+			<< "void " << outline_name << "(" << struct_typename << "*);"
+			;
+/*
+		CXX_LANGUAGE()
+		{
+			function_decl_src
+				<< "}"
+				;
+		}
+*/
+		AST_t function_decl_tree = function_decl_src.parse_declaration(reference_tree, sl);
+		reference_tree.prepend_sibling_function(function_decl_tree);
+	}
+	else
+	{
+		// Forward declaration of the task outline
+		Source outline_declaration_src;
+
+	// FIXME: We don't support c++ for now
+/*
+		CXX_LANGUAGE()
+		{
+			outline_declaration_src
+				//<< "extern \"C\" { "
+				<< ""
+				;
+		}*/
+
+		outline_declaration_src
+			<< "void " << outline_name << "(" << parameter_list << ");"
+			;
+
+	/*	CXX_LANGUAGE()
+	//	{
+			outline_declaration_src
+				<< "}"
+				;
+		}*/
+		AST_t outline_declaration_tree = outline_declaration_src.parse_declaration(reference_tree, sl);
+		reference_tree.prepend_sibling_function(outline_declaration_tree);
+
+
+
+	}
+	
+	// Create configuration file
+	// FIXME: Naming problem
+		//std::string task_conf_name(outline_flags.task_symbol.get_name());
+	   //task_conf_name.append("_entry.conf");
+	   //task_conf.open(task_conf_name.c_str());
+		
+		std::string task_conf_name("accelerator_core.conf");
+	   //task_conf_name.append("_entry.conf");
+	   task_conf.open(task_conf_name.c_str());
+		
+		//FIXME: OMPSS task name
+		//task_conf_str << "accelerator_name = " << outline_flags.task_symbol.get_name() << "_entry\n";
+		//task_conf_str << "hardware_name = " << outline_flags.task_symbol.get_name() << "_entry\n";
+		task_conf_str << "accelerator_name = accelerator_core\n";
+		task_conf << task_conf_str.str();
+	   task_conf.close();
+
+ 
+
+}
     void DeviceFPGA::create_outline(CreateOutlineInfo& info,
             Nodecl::NodeclBase& outline_placeholder,
             Nodecl::Utils::SymbolMap* &symbol_map)
     {
+
+
+
+
     //    //Unpack DTO
     //    const std::string& outline_name = smp_outline_name(info._outline_name);
     //    OutlineInfo& outline_info = info._outline_info;
@@ -500,8 +895,8 @@ namespace TL { namespace Nanox {
     DeviceFPGA::DeviceFPGA()
         : DeviceProvider(/* device_name */ std::string("smp"))
     {
-        set_phase_name("Nanox SMP support");
-        set_phase_description("This phase is used by Nanox phases to implement SMP device support");
+        set_phase_name("Nanox FPGA support");
+        set_phase_description("This phase is used by Nanox phases to implement FPGA device support");
     }
 
     void DeviceFPGA::pre_run(DTO& dto)
@@ -513,11 +908,75 @@ namespace TL { namespace Nanox {
         DeviceProvider::run(dto);
     }
 
+    std::string fpga_outline_name(const std::string &name)
+    {
+        return "_fpga_" + name;
+    }
+
+
+//    void DeviceFPGA::old_get_device_descriptor(const std::string& task_name,
+//            DataEnvironInfo &data_environ,
+//            const OutlineFlags& outline_flags,
+//            AST_t reference_tree,
+//            ScopeLink sl,
+//            Source &ancillary_device_description,
+//            Source &device_descriptor)
+//    {
+//        Source outline_name;
+//        if (!outline_flags.implemented_outline)
+//        {
+//            outline_name
+//                << "_fpga_" + task_name
+//            ;
+//        }
+//        else
+//        {
+//            outline_name << task_name;
+//        }
+//
+//        ancillary_device_description
+//            << comment("FPGA device descriptor")
+//            << "nanos_smp_args_t " << task_name << "_fpga_args = { (void(*)(void*))0};"
+//            ;
+//
+//        device_descriptor
+//            << "{ nanos_fpga_factory, nanos_fpga_dd_size, &" << task_name << "_fpga_args },"
+//            ;
+//    }
+
     void DeviceFPGA::get_device_descriptor(DeviceDescriptorInfo& info,
             Source &ancillary_device_description,
             Source &device_descriptor,
             Source &fortran_dynamic_init)
     {
+
+    std::string outline_name = info._outline_name;
+    Source device_outline_name;
+
+    device_outline_name << fpga_outline_name(outline_name);
+
+    if (Nanos::Version::interface_is_at_least("master", 5012))
+    {
+        ancillary_device_description
+            << comment("FPGA device descriptor")
+            << "static nanos_smp_args_t "
+            << outline_name << "_args = { (void(*)(void*))" << device_outline_name << "};"
+            ;
+    }
+    else
+    {
+        internal_error("Unsupported Nanos version.", 0);
+    }
+
+    device_descriptor
+        << "{ nanos_fpga_factory,  &" << outline_name << "_args },";
+        ;
+
+
+
+
+
+//    device_descriptor << "{ &nanos_gpu_factory, &" << outline_name << "_args },";
     //    std::string outline_name = smp_outline_name(info._outline_name);
     //    if (!IS_FORTRAN_LANGUAGE)
     //    {
