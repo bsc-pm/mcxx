ced1ab9285c4825e440b81d71e3095ce29789ce9
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-05-22 08:35:16 +0200

    Make sure the field type is set to void* for overallocated items
    This commit fixes #1473

---
+++ tests/07_phases_omp.dg/common/c/success_vla_03.c
@@ -0,0 +1,63 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2013 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+  
+  This file is part of Mercurium C/C++ source-to-source compiler.
+  
+  See AUTHORS file in the top level directory for information
+  regarding developers and contributors.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+
+
+/*
+<testinfo>
+test_generator=config/mercurium-omp
+</testinfo>
+*/
+#include <assert.h>
+
+void f(int n)
+{
+    int v[n];
+    int i;
+    for (i = 0; i < n; i++)
+        v[i] = 3;
+
+#pragma omp task
+    {
+        v[3]++;
+    }
+#pragma omp taskwait
+
+    assert(v[3] == 3);
+
+#pragma omp task shared(v)
+    {
+        v[3]++;
+    }
+#pragma omp taskwait
+
+    assert(v[3] == 4);
+}
+
+int main(int argc, char *argv[])
+{
+    f(10);
+}
