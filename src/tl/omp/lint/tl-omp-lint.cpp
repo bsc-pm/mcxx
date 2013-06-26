40ec31549669c69b75cb3b09fe66737a98f5e012
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-06-26 12:26:57 +0200

    Always emit graph

--- src/tl/omp/lint/tl-omp-lint.cpp
+++ src/tl/omp/lint/tl-omp-lint.cpp
@@ -49,7 +49,7 @@ namespace TL { namespace OpenMP {
 
                 TL::Analysis::ExtensibleGraph* graph = extensible_graphs[0];
 
-                // graph->print_graph_to_dot(false, false, false, false, false, false);
+                graph->print_graph_to_dot(false, false, false, false, false, false);
 
                 // Get all task nodes
                 TL::ObjectList<TL::Analysis::Node*> tasks = graph->get_tasks_list();
