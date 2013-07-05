f9ff3df9f86e531bb464d821fcfd1b662ba87fd8
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2013-07-05 12:39:35 +0200

    fix test

--- tests/07_phases_ompss.dg/c/success_return_task_13.c
+++ tests/07_phases_ompss.dg/c/success_return_task_13.c
@@ -42,9 +42,12 @@ int main()
 {
     int x = 0;
 #pragma omp task
-#pragma omp task
     {
-        x++;
+#pragma omp task
+        {
+            x++;
+        }
+#pragma omp taskwait
     }
 
     foo();
