0b66087985cce79eaa932d331903014ae297ffae
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-07-19 18:24:10 +0200

    Refactor lookup in order to properly look up nested dependen typenames
    This commit fixes #1569

---
+++ tests/01_parser.dg/success_189.cpp
@@ -0,0 +1,56 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2013 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+  
+  This file is part of Mercurium C/C++ source-to-source compiler.
+  
+  See AUTHORS file in the top level directory for information
+  regarding developers and contributors.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+
+
+/*
+<testinfo>
+test_generator=config/mercurium
+</testinfo>
+*/
+template <int N>
+struct A
+{
+    template <int N2>
+    struct B
+    {
+        enum {
+            F = N + N2,
+        };
+
+        typedef A<F> M;
+    };
+
+    typename B<1>::M foo();
+
+    A(long int);
+};
+
+void g(long l2)
+{
+    A<3> a(l2);
+    A<4> a2(l2);
+}
