9d667e60a86f324299ba7b22608b634a65ecaaa8
Sara Royuela <sara.royuela@bsc.es>
2013-07-25 17:50:26 -0700

    Extend Analysis of IVs to store all IV modifications
    InductionVariableData stores now two filds for the IV increment:
    - One with the total increment
    - Other with the list of all increments
    
    The Analysis interface has been extended with a new method
    'get_induction_variable_increment_list' that returns
    the list of modifications suffered by an IV inside a loop.
    
    See #1584

--- src/tl/analysis/singleton/tl-analysis-static-info.cpp
+++ src/tl/analysis/singleton/tl-analysis-static-info.cpp
@@ -146,6 +146,33 @@ namespace Analysis {
         return result;
     }
 
+    ObjectList<const_value_t*> NodeclStaticInfo::get_induction_variable_increment_list( const Nodecl::NodeclBase& n ) const
+    {
+        ObjectList<const_value_t*> result;
+        
+        for( ObjectList<Analysis::Utils::InductionVariableData*>::const_iterator it = _induction_variables.begin( );
+             it != _induction_variables.end( ); ++it )
+        {
+            if ( Nodecl::Utils::equal_nodecls( ( *it )->get_variable( ).get_nodecl( ), n, /* skip conversion nodes */ true ) )
+            {
+                ObjectList<Nodecl::NodeclBase> incr_nodecls = ( *it )->get_increment_list( );
+                for( ObjectList<Nodecl::NodeclBase>::iterator it2 = incr_nodecls.begin( ); it2 != incr_nodecls.end( ); ++it2 )
+                {
+                    result.insert( it2->get_constant( ) );
+                }
+                break;
+            }
+        }
+        
+        if( result.empty( ) )
+        {
+            WARNING_MESSAGE( "You are asking for the increment of an Object ( %s ) "\
+                             "which is not an Induction Variable\n", n.prettyprint( ).c_str( ) );
+        }
+        
+        return result;
+    }
+    
     bool NodeclStaticInfo::is_induction_variable_increment_one( const Nodecl::NodeclBase& n ) const
     {
         bool result = false;
@@ -348,6 +375,27 @@ namespace Analysis {
         return result;
     }
 
+    ObjectList<const_value_t*> AnalysisStaticInfo::get_induction_variable_increment_list( const Nodecl::NodeclBase& scope,
+                                                                                          const Nodecl::NodeclBase& n ) const
+    {
+        ObjectList<const_value_t*> result;
+
+        static_info_map_t::const_iterator scope_static_info = _static_info_map.find( scope );
+        if( scope_static_info == _static_info_map.end( ) )
+        {
+            WARNING_MESSAGE( "Nodecl '%s' is not contained in the current analysis. "\
+                             "Cannot get the list of increments of the induction variable '%s'.'",
+                             scope.prettyprint( ).c_str( ), n.prettyprint( ).c_str( ) );
+        }
+        else
+        {
+            NodeclStaticInfo current_info = scope_static_info->second;
+            result = current_info.get_induction_variable_increment_list( n );
+        }
+
+        return result;
+    }
+    
     bool AnalysisStaticInfo::is_induction_variable_increment_one( const Nodecl::NodeclBase& scope,
                                                                   const Nodecl::NodeclBase& n ) const
     {
