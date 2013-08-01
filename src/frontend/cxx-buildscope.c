9652ef764033e73a35145f13ee967a5a4c35bea7
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-08-01 12:28:21 +0200

    Parse default arguments of member functions in a delayed fashion
    This commit fixes #1597

--- src/frontend/cxx-buildscope.c
+++ src/frontend/cxx-buildscope.c
@@ -5992,8 +5992,74 @@ void finish_class_type(type_t* class_type, type_t* type_info, decl_context_t dec
     }
 }
 
+// Delayed member function declarations (due to default arguments)
 
-struct delayed_function_tag
+struct delayed_function_decl_tag
+{
+    scope_entry_t* entry;
+    decl_context_t decl_context;
+};
+
+static int _next_delayed_function_decl = 0;
+static struct delayed_function_decl_tag _delayed_functions_decl_list[MCXX_MAX_FUNCTIONS_PER_CLASS];
+
+static void build_scope_delayed_add_function_declaration(scope_entry_t* entry, decl_context_t decl_context)
+{
+    ERROR_CONDITION(_next_delayed_function_decl == MCXX_MAX_FUNCTIONS_PER_CLASS, "Too many function declarations delayed\n", 0);
+
+    _delayed_functions_decl_list[_next_delayed_function_decl].entry = entry;
+    _delayed_functions_decl_list[_next_delayed_function_decl].decl_context = decl_context;
+    _next_delayed_function_decl++;
+}
+
+
+static void build_scope_delayed_function_decl_clear_pending(void)
+{
+    DEBUG_CODE()
+    {
+        fprintf(stderr, "BUILDSCOPE: Clearing pending function declarations\n");
+    }
+    _next_delayed_function_decl = 0;
+}
+
+static void build_scope_delayed_function_decl(void)
+{
+    int i;
+    for (i = 0; i < _next_delayed_function_decl; i++)
+    {
+        scope_entry_t* entry = _delayed_functions_decl_list[i].entry;
+        decl_context_t decl_context = _delayed_functions_decl_list[i].decl_context;
+
+        int num_parameters = function_type_get_num_parameters(entry->type_information);
+        if (function_type_get_has_ellipsis(entry->type_information)) 
+            num_parameters--;
+        int j;
+        for (j = 0; j < num_parameters; j++)
+        {
+            if (entry->entity_specs.default_argument_info[j] != NULL
+                    && nodecl_get_kind(entry->entity_specs.default_argument_info[j]->argument) == NODECL_CXX_PARSE_LATER)
+            {
+                // Let's parse it now
+                AST tree = nodecl_get_ast(nodecl_get_child(entry->entity_specs.default_argument_info[j]->argument, 0));
+                ERROR_CONDITION(tree == NULL, "Invalid tree", 0);
+
+                DEBUG_CODE()
+                {
+                    fprintf(stderr, "=== Delayed default argument parsing at '%s' ===\n",
+                            ast_location(tree));
+                }
+
+                check_expression(tree, decl_context,
+                        &(entry->entity_specs.default_argument_info[j]->argument));
+            }
+        }
+    }
+    build_scope_delayed_function_decl_clear_pending();
+}
+
+// Delayed member function definitions
+
+struct delayed_function_def_tag
 {
     AST function_def_tree;
     scope_entry_t* entry;
@@ -6002,16 +6068,16 @@ struct delayed_function_tag
     char is_explicit_specialization;
 };
 
-static int _next_delayed_function = 0;
-static struct delayed_function_tag _max_delayed_functions[MCXX_MAX_FUNCTIONS_PER_CLASS];
+static int _next_delayed_function_def = 0;
+static struct delayed_function_def_tag _delayed_functions_def_list[MCXX_MAX_FUNCTIONS_PER_CLASS];
 
-static void build_scope_delayed_add_delayed_function_def(AST function_def_tree, 
+static void build_scope_delayed_add_delayed_function_def(AST function_def_tree,
         scope_entry_t* entry,
         decl_context_t decl_context,
         char is_template,
         char is_explicit_specialization)
 {
-    ERROR_CONDITION(_next_delayed_function == MCXX_MAX_FUNCTIONS_PER_CLASS,
+    ERROR_CONDITION(_next_delayed_function_def == MCXX_MAX_FUNCTIONS_PER_CLASS,
             "Too many delayed member functions!\n", 0);
 
     DEBUG_CODE()
@@ -6020,29 +6086,29 @@ static void build_scope_delayed_add_delayed_function_def(AST function_def_tree,
                 ast_location(function_def_tree));
     }
 
-    _max_delayed_functions[_next_delayed_function].function_def_tree = function_def_tree;
-    _max_delayed_functions[_next_delayed_function].entry = entry;
-    _max_delayed_functions[_next_delayed_function].decl_context = decl_context;
-    _max_delayed_functions[_next_delayed_function].is_template = is_template;
-    _max_delayed_functions[_next_delayed_function].is_explicit_specialization = is_explicit_specialization;
-    _next_delayed_function++;
+    _delayed_functions_def_list[_next_delayed_function_def].function_def_tree = function_def_tree;
+    _delayed_functions_def_list[_next_delayed_function_def].entry = entry;
+    _delayed_functions_def_list[_next_delayed_function_def].decl_context = decl_context;
+    _delayed_functions_def_list[_next_delayed_function_def].is_template = is_template;
+    _delayed_functions_def_list[_next_delayed_function_def].is_explicit_specialization = is_explicit_specialization;
+    _next_delayed_function_def++;
 }
 
-static void build_scope_delayed_clear_pending(void)
+static void build_scope_delayed_function_def_clear_pending(void)
 {
     DEBUG_CODE()
     {
-        fprintf(stderr, "BUILDSCOPE: Clearing pending functions\n");
+        fprintf(stderr, "BUILDSCOPE: Clearing pending function definition\n");
     }
-    _next_delayed_function = 0;
+    _next_delayed_function_def = 0;
 }
 
-static void build_scope_delayed_functions(nodecl_t* nodecl_output)
+static void build_scope_delayed_function_def(nodecl_t* nodecl_output)
 {
     int i;
-    for (i = 0;  i < _next_delayed_function; i++)
+    for (i = 0;  i < _next_delayed_function_def; i++)
     {
-        struct delayed_function_tag current = _max_delayed_functions[i];
+        struct delayed_function_def_tag current = _delayed_functions_def_list[i];
 
         AST function_def = current.function_def_tree;
         decl_context_t decl_context = current.decl_context;
@@ -6066,7 +6132,7 @@ static void build_scope_delayed_functions(nodecl_t* nodecl_output)
 
         *nodecl_output = nodecl_concat_lists(*nodecl_output, nodecl_funct_def);
     }
-    build_scope_delayed_clear_pending();
+    build_scope_delayed_function_def_clear_pending();
 }
 
 static int _class_specifier_nesting = 0;
@@ -6088,9 +6154,14 @@ void leave_class_specifier(nodecl_t* nodecl_output)
     {
         DEBUG_CODE()
         {
-            fprintf(stderr, "BUILDSCOPE: Building scope of delayed functions\n");
+            fprintf(stderr, "BUILDSCOPE: Building scope of delayed function declarations\n");
         }
-        build_scope_delayed_functions(nodecl_output);
+        build_scope_delayed_function_decl();
+        DEBUG_CODE()
+        {
+            fprintf(stderr, "BUILDSCOPE: Building scope of delayed function definitions\n");
+        }
+        build_scope_delayed_function_def(nodecl_output);
     }
     _class_specifier_nesting--;
     ERROR_CONDITION(_class_specifier_nesting < 0,
@@ -7532,17 +7603,40 @@ static void set_function_parameter_clause(type_t** function_type,
             // Check the default argument
             if (default_argument != NULL)
             {
-                if (!check_expression(default_argument, decl_context, &nodecl_default_argument))
+                if (gather_info->inside_class_specifier)
                 {
-                    if (!checking_ambiguity())
-                    {
-                        error_printf("%s: error: could not check default argument expression '%s'\n",
-                                ast_location(default_argument),
-                                prettyprint_in_buffer(default_argument));
-                    }
+                    // We have to allow this case (somebody decided that this made sense)
+                    //
+                    // struct A
+                    // {
+                    //   void f(int i = E);
+                    //   enum { E = 3 };
+                    // };
+                    //
+                    AST parent = ast_get_parent(default_argument);
 
-                    *function_type = get_error_type();
-                    return;
+                    nodecl_default_argument = nodecl_make_cxx_parse_later(ast_get_locus(default_argument));
+                    // Keep the current tree
+                    nodecl_set_child(nodecl_default_argument, 0, _nodecl_wrap(default_argument));
+                    // the parent was changed by nodecl_set_child, so fix it (otherwise the tree becomes inconsistent)
+                    ast_set_parent(default_argument, parent);
+
+                    // We will delay these function declarations in register_function
+                }
+                else
+                {
+                    if (!check_expression(default_argument, decl_context, &nodecl_default_argument))
+                    {
+                        if (!checking_ambiguity())
+                        {
+                            error_printf("%s: error: could not check default argument expression '%s'\n",
+                                    ast_location(default_argument),
+                                    prettyprint_in_buffer(default_argument));
+                        }
+
+                        *function_type = get_error_type();
+                        return;
+                    }
                 }
             }
 
@@ -8956,6 +9050,7 @@ static scope_entry_t* register_function(AST declarator_id, type_t* declarator_ty
         }
 
         int i;
+        char already_delayed = 0;
         for (i = 0; i < gather_info->num_parameters; i++)
         {
             if (!nodecl_is_null(gather_info->arguments_info[i].argument))
@@ -8967,6 +9062,15 @@ static scope_entry_t* register_function(AST declarator_id, type_t* declarator_ty
                     gather_info->arguments_info[i].argument;
                 new_entry->entity_specs.default_argument_info[i]->context = 
                     gather_info->arguments_info[i].context;
+
+                if (nodecl_get_kind(new_entry->entity_specs.default_argument_info[i]->argument) == NODECL_CXX_PARSE_LATER
+                        && !already_delayed)
+                {
+                    ERROR_CONDITION(decl_context.current_scope->kind != CLASS_SCOPE,
+                            "Invalid parse later default argument", 0);
+                    build_scope_delayed_add_function_declaration(new_entry, decl_context);
+                    already_delayed = 1;
+                }
             }
         }
 
@@ -12216,6 +12320,7 @@ static scope_entry_t* build_scope_member_function_definition(decl_context_t decl
 
     gather_info.is_template = is_template;
     gather_info.is_explicit_specialization = is_explicit_specialization;
+    gather_info.inside_class_specifier = 1;
 
     type_t* type_info = NULL;
 
@@ -12355,6 +12460,8 @@ static void build_scope_default_or_delete_member_function_definition(decl_contex
         nodecl_t* nodecl_output)
 {
     gather_decl_spec_t gather_info;
+    gather_info.inside_class_specifier = 1;
+
     memset(&gather_info, 0, sizeof(gather_info));
 
     type_t* class_type = get_actual_class_type(class_info);
@@ -12505,6 +12612,7 @@ static void build_scope_member_simple_declaration(decl_context_t decl_context, A
     gather_info.is_template = is_template;
     gather_info.is_explicit_specialization = is_explicit_specialization;
     gather_info.current_access = current_access;
+    gather_info.inside_class_specifier = 1;
 
     type_t* class_type = NULL;
     const char* class_name = "";
