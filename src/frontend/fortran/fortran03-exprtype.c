cffbe76a952d236d6921c464aad96682f9b9ce2f
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-08-09 12:32:26 +0200

    Fix function references when the use associated name includes an intrinsic
    Also do not add twice intrinsics in function name queries.
    
    This commit fixes #1629

--- src/frontend/fortran/fortran03-exprtype.c
+++ src/frontend/fortran/fortran03-exprtype.c
@@ -2767,7 +2767,9 @@ static void check_called_symbol_list(
         nodecl_t* nodecl_simplify)
 {
     scope_entry_t* symbol = NULL;
-    char intrinsic_named_like_generic_spec = 0;
+    char intrinsic_name_can_be_called = 0;
+    // char generic_name_can_be_called = 0;
+    char hide_intrinsics = 0;
 
     int explicit_num_actual_arguments = *num_actual_arguments;
 
@@ -2786,7 +2788,6 @@ static void check_called_symbol_list(
             if (current_generic_spec->entity_specs.is_builtin
                     && is_computed_function_type(current_generic_spec->type_information))
             {
-                intrinsic_named_like_generic_spec = 1;
                 scope_entry_t* specific_intrinsic = fortran_solve_generic_intrinsic_call(current_generic_spec,
                         nodecl_actual_arguments,
                         explicit_num_actual_arguments,
@@ -2794,10 +2795,9 @@ static void check_called_symbol_list(
 
                 if (specific_intrinsic != NULL)
                 {
-                    // If an intrinsic matches, we do not have to try anymore
+                    intrinsic_name_can_be_called = 1;
                     specific_symbol_set = entry_list_merge(entry_list_new(specific_intrinsic),
                             specific_symbol_set);
-                    break;
                 }
             }
             else if (current_generic_spec->entity_specs.is_generic_spec)
@@ -2808,9 +2808,17 @@ static void check_called_symbol_list(
 
                 if (current_specific_symbol_set != NULL)
                 {
+                    // generic_name_can_be_called = 1;
                     // This may be overwritten when more than one generic specifier
                     // can match, which is wrong
                     *generic_specifier_symbol = current_generic_spec;
+
+                    // If we find a USE consistent call to a associated name
+                    // then INTRINSICS must be ignored to prioritize USEs
+                    if (current_generic_spec->entity_specs.from_module != NULL)
+                    {
+                        hide_intrinsics = 1;
+                    }
                 }
 
                 specific_symbol_set = entry_list_merge(current_specific_symbol_set,
@@ -2826,13 +2834,33 @@ static void check_called_symbol_list(
 
         entry_list_iterator_free(it);
 
+        if (entry_list_size(specific_symbol_set) > 1)
+        {
+            scope_entry_list_t* filtered_specific_symbol_set = NULL;
+
+            for (it = entry_list_iterator_begin(specific_symbol_set);
+                    !entry_list_iterator_end(it);
+                    entry_list_iterator_next(it))
+            {
+                scope_entry_t* entry = entry_list_iterator_current(it);
+                if (hide_intrinsics && entry->entity_specs.is_builtin)
+                    continue;
+
+                filtered_specific_symbol_set = entry_list_add_once(filtered_specific_symbol_set, entry);
+            }
+            entry_list_iterator_free(it);
+
+            entry_list_free(specific_symbol_set);
+            specific_symbol_set = filtered_specific_symbol_set;
+        }
+
         if (specific_symbol_set == NULL)
         {
             if (!checking_ambiguity())
             {
                 error_printf("%s: error: no specific interfaces%s match the generic interface '%s' in function reference\n",
                         ast_location(location),
-                        intrinsic_named_like_generic_spec ? " or intrinsic procedures" : "",
+                        intrinsic_name_can_be_called ? " or intrinsic procedures" : "",
                         fortran_prettyprint_in_buffer(procedure_designator));
             }
             *result_type = get_error_type();
@@ -2844,7 +2872,7 @@ static void check_called_symbol_list(
             {
                 error_printf("%s: error: several specific interfaces%s match generic interface '%s' in function reference\n",
                         ast_location(location),
-                        intrinsic_named_like_generic_spec ? " or intrinsic procedures" : "",
+                        intrinsic_name_can_be_called ? " or intrinsic procedures" : "",
                         fortran_prettyprint_in_buffer(procedure_designator));
                 for (it = entry_list_iterator_begin(specific_symbol_set);
                         !entry_list_iterator_end(it);
@@ -2853,9 +2881,9 @@ static void check_called_symbol_list(
                     scope_entry_t* current_generic_spec = entry_list_iterator_current(it);
                     if (current_generic_spec->entity_specs.is_generic_spec)
                     {
-                    info_printf("%s: info: specific interface '%s' matches\n",
-                            locus_to_str(current_generic_spec->locus),
-                            current_generic_spec->symbol_name);
+                        info_printf("%s: info: specific interface '%s' matches\n",
+                                locus_to_str(current_generic_spec->locus),
+                                current_generic_spec->symbol_name);
                     }
                     else if (current_generic_spec->entity_specs.is_builtin)
                     {
