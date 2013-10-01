8dc657fcbf15c2ca4d7271a10bcac8ad02d0734e
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2013-10-01 16:27:28 +0200

    Emit virtual for TemplateFunctionCode as well
    This commit fixes #1699

--- src/tl/codegen/base/cxx/codegen-cxx.cpp
+++ src/tl/codegen/base/cxx/codegen-cxx.cpp
@@ -1974,6 +1974,11 @@ CxxBase::Ret CxxBase::visit(const Nodecl::TemplateFunctionCode& node)
         decl_spec_seq += "inline ";
     }
 
+    if (symbol.is_virtual() && symbol.is_defined_inside_class())
+    {
+        decl_spec_seq += "virtual ";
+    }
+
     if (symbol.is_explicit_constructor()
             && symbol.is_defined_inside_class())
     {
