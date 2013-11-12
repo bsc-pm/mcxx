23bf78d3f1946d76af093ee1b23b535bc40afc3c
Diego Caballero <diego.caballero@bsc.es>
2013-11-12 13:14:03 +0100

    Add vector mul div and mod to the calc
    See #1821

--- src/tl/tl-nodecl-calc.cpp
+++ src/tl/tl-nodecl-calc.cpp
@@ -687,6 +687,51 @@ namespace Nodecl
         }
     }
 
+    Calculator::Ret Calculator::visit(const Nodecl::VectorMul& n)
+    {
+        TL::ObjectList<const_value_t*> lhs = walk(n.get_lhs());
+        TL::ObjectList<const_value_t*> rhs = walk(n.get_rhs());
+        
+        if (lhs.empty() || rhs.empty())
+        {
+            return TL::ObjectList<const_value_t*>();
+        }
+        else
+        {
+            return TL::ObjectList<const_value_t*>(1, const_value_mul(lhs[0], rhs[0]));
+        }
+    }
+    
+    Calculator::Ret Calculator::visit(const Nodecl::VectorDiv& n)
+    {
+        TL::ObjectList<const_value_t*> lhs = walk(n.get_lhs());
+        TL::ObjectList<const_value_t*> rhs = walk(n.get_rhs());
+        
+        if (lhs.empty() || rhs.empty())
+        {
+            return TL::ObjectList<const_value_t*>();
+        }
+        else
+        {
+            return TL::ObjectList<const_value_t*>(1, const_value_div(lhs[0], rhs[0]));
+        }
+    }
+    
+    Calculator::Ret Calculator::visit(const Nodecl::VectorMod& n)
+    {
+        TL::ObjectList<const_value_t*> lhs = walk(n.get_lhs());
+        TL::ObjectList<const_value_t*> rhs = walk(n.get_rhs());
+        
+        if (lhs.empty() || rhs.empty())
+        {
+            return TL::ObjectList<const_value_t*>();
+        }
+        else
+        {
+            return TL::ObjectList<const_value_t*>(1, const_value_mod(lhs[0], rhs[0]));
+        }
+    }
+ 
     Calculator::Ret Calculator::visit(const Nodecl::VectorLiteral& n)
     {
         return TL::ObjectList<const_value_t*>(1, n.get_constant());
