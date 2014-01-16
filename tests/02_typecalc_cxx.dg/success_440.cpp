aa577cf386d21b35829680b1305fef8c864e89f8
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-01-16 08:25:56 +0100

    __cplusplus is 1 for old versions of g++

--- tests/02_typecalc_cxx.dg/success_440.cpp
+++ tests/02_typecalc_cxx.dg/success_440.cpp
@@ -28,8 +28,19 @@ bool const cpp98 = fon< fun< 1 >>::three >::two >::one;
 
 int main(void)
 {
-    // printf("this is a %sC++11 compiler, version is %ld\n", (cpp98 ? "pre-" : ""), __cplusplus);
-    assert((__cplusplus == 199711 && cpp98)
+    // Old versions of g++ incorrectly defined __cplusplus as 1
+#if defined(__GNUC__)
+  #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7)
+      const long cxx_98_value = 199711L;
+  #else
+      const long cxx_98_value = 1;
+  #endif
+#else
+    const long cxx_98_value = 199711L;
+#endif
+
+
+    assert((__cplusplus == cxx_98_value && cpp98)
             || (__cplusplus == 201103 && !cpp98));
     return 0;
 }
