8787c3ca471622381a78c737705b839784f0ccdd
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2014-01-21 11:18:12 +0100

    Fix some issues in tl-datareference.cpp related with fortran pointers to arrays

---
+++ tests/07_phases_ompss.dg/fortran/success_task_22.f90
@@ -0,0 +1,19 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER, POINTER :: PTR(:, :)
+
+    ALLOCATE(PTR(10, 5))
+
+    PTR = 1
+
+    !$OMP TASK INOUT(PTR)
+        PTR(1, :)= -2
+    !$OMP END TASK
+
+    !$OMP TASKWAIT
+    if (ANY(PTR(1,:) /= -2)) STOP 1
+    if (ANY(PTR(2:,:) /= 1)) STOP 2
+END PROGRAM P
