bsc-pm/mcxx

commit 0a506d6060666ffff097cba570d5efa3f536aaa5

Use the right way for parsing in other languages
Modifying CURRENT_CONFIGURATION->source_language is not enough

src/tl/omp/nanox-nodecl/devices/cuda/nanox-cuda.cpp
@@ -989,12 +989,12 @@ void DeviceCUDA::phase_cleanup(DTO& data_flow)
 
         bool is_fortan = IS_FORTRAN_LANGUAGE;
         if (is_fortan)
-            CURRENT_CONFIGURATION->source_language = SOURCE_LANGUAGE_C;
+            SourceLanguage::source_language = SourceLanguage::C;
 
         phase->codegen_top_level(_cuda_file_code, ancillary_file);
 
         if (is_fortan)
-            CURRENT_CONFIGURATION->source_language = SOURCE_LANGUAGE_FORTRAN;
+            SourceLanguage::source_language = SourceLanguage::Fortran;
 
         fclose(ancillary_file);
 

src/tl/omp/nanox-nodecl/tl-nanox-array-descriptor.cpp
@@ -76,9 +76,9 @@ namespace TL { namespace Nanox {
             ;
 
         // Parse as C
-        CURRENT_CONFIGURATION->source_language = SOURCE_LANGUAGE_C;
+        TL::Source::source_language = SourceLanguage::C;
         Nodecl::List n = new_src.parse_global(original_scope).as<Nodecl::List>();
-        CURRENT_CONFIGURATION->source_language = SOURCE_LANGUAGE_FORTRAN;
+        TL::Source::source_language = SourceLanguage::Current;
 
         Nodecl::List& extra_c_code = _lowering->get_extra_c_code();
 

src/tl/omp/nanox-nodecl/tl-nanox-ptr.cpp
@@ -141,7 +141,7 @@ namespace TL { namespace Nanox {
             // Parse as C
             Source::source_language = SourceLanguage::C;
             Nodecl::List n = src.parse_global(original_scope).as<Nodecl::List>();
-            Source::source_language = SourceLanguage::Fortran;
+            Source::source_language = SourceLanguage::Current;
 
             Nodecl::List& extra_c_code = Lowering::get_extra_c_code();
 

commit 60f42180fe1536078d31288ae5da3db9c517faad

Fix issue introduced in [0a506d6060666ffff097cba570d5efa3f536aaa5]

src/tl/omp/nanox-nodecl/devices/cuda/nanox-cuda.cpp
@@ -989,12 +989,12 @@ void DeviceCUDA::phase_cleanup(DTO& data_flow)
 
         bool is_fortan = IS_FORTRAN_LANGUAGE;
         if (is_fortan)
-            SourceLanguage::source_language = SourceLanguage::C;
+            Source::source_language = SourceLanguage::C;
 
         phase->codegen_top_level(_cuda_file_code, ancillary_file);
 
         if (is_fortan)
-            SourceLanguage::source_language = SourceLanguage::Fortran;
+            Source::source_language = SourceLanguage::Fortran;
 
         fclose(ancillary_file);
 