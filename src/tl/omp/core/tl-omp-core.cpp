f6386e4a6a3df9ad76d8d7d36143b4cd745db047
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-02-06 17:59:41 +0100

    Pop TargetContext if created implicitly
    This should fix the problems uncoverd by Florentino
    
    See #1927

--- src/tl/omp/core/tl-omp-core.cpp
+++ src/tl/omp/core/tl-omp-core.cpp
@@ -1567,6 +1567,11 @@ namespace TL
         }
         void Core::task_handler_post(TL::PragmaCustomStatement construct)
         {
+            if (!_target_context.empty()
+                    && _target_context.top().is_implicit)
+            {
+                _target_context.pop();
+            }
             _openmp_info->pop_current_data_sharing();
         }
 
@@ -1579,6 +1584,11 @@ namespace TL
         void Core::task_handler_post(TL::PragmaCustomDeclaration construct)
         {
             // Do nothing
+            if (!_target_context.empty()
+                    && _target_context.top().is_implicit)
+            {
+                _target_context.pop();
+            }
         }
 
         void Core::taskwait_handler_pre(TL::PragmaCustomDirective construct)
