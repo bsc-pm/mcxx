86e0d251c863cf3e6e813c63f5c1269d4b6322f2
Diego Caballero <diego.caballero@bsc.es>
2014-02-20 14:15:01 +0100

    Fix tl-omp.hpp missing file & double free

--- src/tl/analysis/singleton/tl-vectorization-analysis.cpp
+++ src/tl/analysis/singleton/tl-vectorization-analysis.cpp
@@ -398,8 +398,6 @@ namespace Analysis {
             // Add adjacent dimension
             alignment += it_alignment;
             
-            free(dimension_sizes);
-            
             _nesting_level--;
             
             free( dimension_sizes );
