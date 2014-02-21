9e8392b074bc18d6b3a6aa6cdb06f7e83e6d9636
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-02-21 18:07:57 +0100

    Fix instantiation of nondependent function calls

--- src/frontend/cxx-exprtype.c
+++ src/frontend/cxx-exprtype.c
@@ -19870,7 +19870,8 @@ static void instantiate_reference(nodecl_instantiate_expr_visitor_t* v, nodecl_t
 
 static void instantiate_function_call(nodecl_instantiate_expr_visitor_t* v, nodecl_t node)
 {
-    nodecl_t nodecl_called = instantiate_expr_walk(v, nodecl_get_child(node, 0));
+    // This does not have to be instantiated
+    nodecl_t nodecl_called = nodecl_shallow_copy(nodecl_get_child(node, 0));
 
     nodecl_t nodecl_argument_list = nodecl_get_child(node, 1);
 
@@ -19900,19 +19901,7 @@ static void instantiate_function_call(nodecl_instantiate_expr_visitor_t* v, node
                 current_arg);
     }
 
-    nodecl_t function_form = nodecl_null();
-    scope_entry_t* called_symbol = nodecl_get_symbol(nodecl_called);
-    if (called_symbol != NULL
-            && is_template_specialized_type(called_symbol->type_information))
-    {
-        function_form =
-            nodecl_make_cxx_function_form_template_id(
-                    nodecl_get_locus(nodecl_called));
-
-        template_parameter_list_t* template_args =
-            nodecl_get_template_parameters(nodecl_called);
-        nodecl_set_template_parameters(function_form, template_args);
-    }
+    nodecl_t function_form = nodecl_shallow_copy(nodecl_get_child(node, 2));
 
     v->nodecl_result = cxx_nodecl_make_function_call(
             nodecl_called,
