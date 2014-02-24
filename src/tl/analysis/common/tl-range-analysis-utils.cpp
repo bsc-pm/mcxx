74b22561ce9f6c63e10cee45666500f5a43ff433
Sara Royuela <sara.royuela@bsc.es>
2014-02-24 15:07:56 +0100

    Reimplement ranges arithmetic
    Our implmentation of the interval's arithmetic was based
    on the arithmetic used to put bounds on rounding errors,
    but what we need is to apply the arithmetic defined for sets in ranges.
    In this commit we implement the three basic operations: union,
    intersection and substraction.

--- src/tl/analysis/common/tl-range-analysis-utils.cpp
+++ src/tl/analysis/common/tl-range-analysis-utils.cpp
@@ -41,184 +41,194 @@ namespace Utils {
      * we cannot tell whether the range is empty or not.
      * Being conservative, we answer false when we don't know the accurate answer
      */
-    bool empty_interval( const Nodecl::Analysis::ClosedInterval& c )
+    bool is_empty_range( const Nodecl::Analysis::Range& c )
     {
-        bool result = false;
-        if ( c.get_lower( ).is_constant( ) && c.get_upper( ).is_constant( ) )
-        {
-            result = const_value_is_negative( const_value_sub( c.get_upper( ).get_constant( ), c.get_lower( ).get_constant( ) ) );
-        }
-        return result;
+        return c.get_lower().is<Nodecl::Analysis::EmptyRange>();
     }
     
-    // [a, b] + [c, d] = [a+d, b+c]
-    Nodecl::NodeclBase interval_add( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
+    Nodecl::Analysis::Range get_empty_range( const TL::Type& t )
     {
-        Nodecl::NodeclBase lb = Nodecl::Add::make( c1.get_lower( ).shallow_copy( ), c2.get_lower( ).shallow_copy( ), c1.get_type( ) );
-        Nodecl::NodeclBase ub = Nodecl::Add::make( c1.get_upper( ).shallow_copy( ), c2.get_upper( ).shallow_copy( ), c1.get_type( ) );
-        return Nodecl::Analysis::ClosedInterval::make( lb, ub, c1.get_type( ) );
+        return Nodecl::Analysis::Range::make( Nodecl::Analysis::EmptyRange::make(), Nodecl::NodeclBase::null(), t );
     }
     
-    // [a, b] − [c, d] = [a-d, b-c]
-    Nodecl::NodeclBase interval_sub( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
+    // A[al, au] − B[bl, bu] =  ∅, A and B completely overlap or B contains A
+    //                          A, A and B do not overlap
+    //                          [al, bl] U [bu, au], A contains B
+    //                          [al, bl], al < bl
+    //                          [au, bu], al > bl
+    // When the operation cannot be computed, we return the nodecl representing the operation
+    Nodecl::NodeclBase range_sub( const Nodecl::Analysis::Range& r1, const Nodecl::Analysis::Range& r2 )
     {
-        Nodecl::NodeclBase lb = Nodecl::Minus::make( c1.get_lower( ).shallow_copy( ), c2.get_upper( ).shallow_copy( ), c1.get_type( ) );
-        Nodecl::NodeclBase ub = Nodecl::Minus::make( c1.get_upper( ).shallow_copy( ), c2.get_lower( ).shallow_copy( ), c1.get_type( ) );
-        return Nodecl::Analysis::ClosedInterval::make( lb, ub, c1.get_type( ) );
-    }
-    
-    // [a, b] * [c, d] = [min(a*c, a*d, b*c, b*d), max(a*c, a*d, b*c, b*d)]
-    Nodecl::NodeclBase range_mul( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
-    {
+        if( is_empty_range( r1 ) || is_empty_range( r2 ) )
+            return r1.shallow_copy();
+            
         Nodecl::NodeclBase result;
         
-        Nodecl::NodeclBase c1_lb = c1.get_lower( );   Nodecl::NodeclBase c1_ub = c1.get_upper( );
-        Nodecl::NodeclBase c2_lb = c2.get_lower( );   Nodecl::NodeclBase c2_ub = c2.get_upper( );
-        TL::Type t = c1_lb.get_type( );
+        Nodecl::NodeclBase r1_lb = r1.get_lower();   Nodecl::NodeclBase r1_ub = r1.get_upper();
+        Nodecl::NodeclBase r2_lb = r2.get_lower();   Nodecl::NodeclBase r2_ub = r2.get_upper();
+        TL::Type t = r1_lb.get_type();
         
-        if( c1_lb.is_constant( ) && c1_ub.is_constant( ) && c2_lb.is_constant( ) && c2_ub.is_constant( ) )
-        {   // Having constant values we can apply the arithmetic
-            const_value_t* c1_lb_c = c1_lb.get_constant( );   const_value_t* c1_ub_c = c1_ub.get_constant( );
-            const_value_t* c2_lb_c = c2_lb.get_constant( );   const_value_t* c2_ub_c = c2_ub.get_constant( );
-            const_value_t* candidates[4] = {
-                const_value_mul( c1_lb_c, c2_lb_c ), const_value_mul( c1_lb_c, c2_ub_c ),
-                const_value_mul( c1_ub_c, c2_lb_c ), const_value_mul( c1_ub_c, c2_ub_c ) 
-            };
-            const_value_t* min = candidates[0];
-            const_value_t* max = candidates[0];
-            
-            int i;
-            for( i=1; i<4; ++i ) {
-                if( const_value_is_positive( const_value_sub( min, candidates[i] ) ) )
-                    min = candidates[i];
-                if( const_value_is_negative( const_value_sub( max, candidates[i] ) ) )
-                    max = candidates[i];
+        if( ( r1_lb == r2_lb ) && ( r1_ub == r2_ub ) )
+        {   // The two ranges are exactly the same
+            result = get_empty_range( t );
+        }
+        else if( r1_lb.is_constant() && r1_ub.is_constant() && r2_lb.is_constant() && r2_ub.is_constant() )
+        {   // Let's compare the constant values
+            const_value_t* r1_lb_c = r1_lb.get_constant();
+            const_value_t* r1_ub_c = r1_ub.get_constant();
+            const_value_t* r2_lb_c = r2_lb.get_constant();
+            const_value_t* r2_ub_c = r2_ub.get_constant();
+            if( const_value_is_positive( const_value_sub( r1_lb_c, r2_ub_c ) ) || 
+                const_value_is_positive( const_value_sub( r2_lb_c, r1_ub_c ) ) )
+            {   // r1 and r2 do not overlap
+                result = r1.shallow_copy();
             }
-                
-            result = Nodecl::Analysis::ClosedInterval::make( const_value_to_nodecl( min ), const_value_to_nodecl( max ), t );
+            else if( const_value_is_zero( const_value_sub( r1_lb_c, r2_lb_c ) ) && 
+                     const_value_is_zero( const_value_sub( r1_ub_c, r2_ub_c ) ) )
+            {   // r1 and r2 are the same range
+                result = get_empty_range( t );
+            }
+            else if( const_value_is_positive( const_value_sub( r1_lb_c, r2_lb_c ) ) && 
+                     const_value_is_positive( const_value_sub( r2_ub_c, r1_ub_c ) ) )
+            {   // r2 contains r1
+                result = get_empty_range( t );
+            }
+            else if( const_value_is_positive( const_value_sub( r2_lb_c, r1_lb_c ) ) && 
+                     const_value_is_positive( const_value_sub( r1_ub_c, r2_ub_c ) ) )
+            {   // r2 is contained in r1
+                Nodecl::Analysis::Range tmp1 = Nodecl::Analysis::Range::make( r1_lb.shallow_copy(), r2_lb.shallow_copy(), t );
+                Nodecl::Analysis::Range tmp2 = Nodecl::Analysis::Range::make( r2_ub.shallow_copy(), r1_ub.shallow_copy(), t );
+                result = Nodecl::Analysis::RangeUnion::make( tmp1, tmp2, t );
+            }
+            else
+            {   // r1 and r2 overlap at some point
+                Nodecl::NodeclBase lb, ub;
+                if( const_value_is_positive( const_value_sub( r2_lb_c, r1_lb_c ) ) )
+                {   // r1 lower bound is smaller than r2
+                    lb = r1_lb.shallow_copy( );
+                    ub = r2_lb.shallow_copy( );
+                }
+                else
+                {   // r2 lower bound is smaller than r1
+                    lb = r2_ub.shallow_copy( );
+                    ub = r1_ub.shallow_copy( );
+                }
+            }
         }
         else
-        {   // Otherwise, we represent the operations with Nodecl operations
-            Nodecl::List expr_list = Nodecl::List::make( 
-                    Nodecl::Mul::make( c1_lb.shallow_copy( ), c2_lb.shallow_copy( ), t ), 
-                    Nodecl::Mul::make( c1_lb.shallow_copy( ), c2_ub.shallow_copy( ), t ), 
-                    Nodecl::Mul::make( c1_ub.shallow_copy( ), c2_lb.shallow_copy( ), t ), 
-                    Nodecl::Mul::make( c1_ub.shallow_copy( ), c2_ub.shallow_copy( ), t ) );
-            Nodecl::Analysis::Minimum lhs = Nodecl::Analysis::Minimum::make( expr_list, t );
-            Nodecl::Analysis::Maximum rhs = Nodecl::Analysis::Maximum::make( expr_list.shallow_copy( ), t );
-            result = Nodecl::Analysis::ClosedInterval::make( lhs, rhs, t );
+        {   // We are not able to synthesize the result
+            result = Nodecl::Analysis::RangeSub::make( r1, r2, t );
         }
         
         return result;
     }
     
-    // [a, b] / [c, d] = [min(a/c, a/d, b/c, b/d), max(a/c, a/d, b/c, b/d)]
-    Nodecl::NodeclBase range_div( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
+    // A[al, au] ∩ B[bl, bu] = (al<bl<au || al<bu<au) ? [max(al, bl), min(au, bu)]      -> overlap
+    //                                                : ∅
+    Nodecl::NodeclBase range_intersection( const Nodecl::Analysis::Range& r1, const Nodecl::Analysis::Range& r2 )
     {
+        if( is_empty_range( r1 ) )
+            return r2.shallow_copy();
+        else if( is_empty_range( r2 ) )
+            return r1.shallow_copy();
+        
+        Nodecl::NodeclBase result;
+
+        Nodecl::NodeclBase r1_lb = r1.get_lower();   Nodecl::NodeclBase r1_ub = r1.get_upper();
+        Nodecl::NodeclBase r2_lb = r2.get_lower();   Nodecl::NodeclBase r2_ub = r2.get_upper();
+        TL::Type t = r1_lb.get_type();
+        if( ( r1_lb == r2_lb ) && ( r1_ub == r2_ub ) )
+        {   // The two ranges are exactly the same
+            result = r1.shallow_copy( );
+        }
+        else if( r1_lb.is_constant() && r1_ub.is_constant() && r2_lb.is_constant() && r2_ub.is_constant() )
+        {   // Let's compare the constant values
+            const_value_t* r1_lb_c = r1_lb.get_constant();
+            const_value_t* r1_ub_c = r1_ub.get_constant();
+            const_value_t* r2_lb_c = r2_lb.get_constant();
+            const_value_t* r2_ub_c = r2_ub.get_constant();
+            
+            if( const_value_is_positive( const_value_sub( r1_lb_c, r2_ub_c ) ) || 
+                const_value_is_positive( const_value_sub( r2_lb_c, r1_ub_c ) ) )
+            {   // r1 and r2 do not overlap
+                result = get_empty_range( t );
+            }
+            else if( const_value_is_zero( const_value_sub( r1_lb_c, r2_lb_c ) ) && 
+                     const_value_is_zero( const_value_sub( r1_ub_c, r2_ub_c ) ) )
+            {   // r1 and r2 are the same range
+                result = r1.shallow_copy();
+            }
+            else
+            {   // r1 and r2 overlap in some way
+                Nodecl::NodeclBase lb, ub ;
+                if( const_value_is_positive( const_value_sub( r1_lb_c, r2_lb_c ) ) )
+                {
+                    lb = r1_lb.shallow_copy();
+                    ub = r2_ub.shallow_copy();
+                }
+                else
+                {
+                    lb = r2_lb.shallow_copy();
+                    ub = r1_ub.shallow_copy();
+                }
+                result = Nodecl::Analysis::Range::make( lb, ub, t );
+            }
+        }
+        else
+        {   // We are not able to synthesize the result
+            result = Nodecl::Analysis::RangeIntersection::make( r1.shallow_copy(), r2.shallow_copy(), t );
+        }
+        
+        return result;
+    }
+    
+    // A[al, au] ∪ B[bl, bu] = (al<bl<au || al<bu<au) ? [min(al, bl), max(au, bu)]      -> overlap
+    //                                                : [al, au] ∪ [bl, bu]             -> cannot synthesize the result as a unique range
+    Nodecl::NodeclBase range_union( const Nodecl::Analysis::Range& r1, const Nodecl::Analysis::Range& r2 )
+    {
+        if( is_empty_range( r1 ) && is_empty_range( r2 ) )
+            return r1.shallow_copy();
+            
         Nodecl::NodeclBase result;
         
-        Nodecl::NodeclBase c1_lb = c1.get_lower( );   Nodecl::NodeclBase c1_ub = c1.get_upper( );
-        Nodecl::NodeclBase c2_lb = c2.get_lower( );   Nodecl::NodeclBase c2_ub = c2.get_upper( );
-        TL::Type t = c1_lb.get_type( );
-        
-        if( c1_lb.is_constant( ) && c1_ub.is_constant( ) &&  c2_lb.is_constant( ) && c2_ub.is_constant( ) ) 
-        {   // Having constant values we can apply the arithmetic
-            const_value_t* c1_lb_c = c1_lb.get_constant( );   const_value_t* c1_ub_c = c1_ub.get_constant( );
-            const_value_t* c2_lb_c = c2_lb.get_constant( );   const_value_t* c2_ub_c = c2_ub.get_constant( );
-            const_value_t* candidates[4] = {
-                const_value_div( c1_lb_c, c2_lb_c ), const_value_div( c1_lb_c, c2_ub_c ),
-                const_value_div( c1_ub_c, c2_lb_c ), const_value_div( c1_ub_c, c2_ub_c )
-            };
-            const_value_t* min = candidates[0];
-            const_value_t* max = candidates[0];
-            for( int i=1; i<4; ++i ) {
-                if( const_value_is_positive( const_value_sub( min, candidates[i] ) ) )
-                    min = candidates[i];
-                if( const_value_is_negative( const_value_sub( max, candidates[i] ) ) )
-                    max = candidates[i];
-            }
-            
-            result = Nodecl::Analysis::ClosedInterval::make( const_value_to_nodecl( min ), const_value_to_nodecl( max ), t );
-            
-        } else {
-            if( ( c2_lb.is_constant( ) && const_value_is_zero( c2_lb.get_constant( ) ) ) || 
-                ( c2_ub.is_constant( ) && const_value_is_zero( c2_ub.get_constant( ) ) ) ) 
-            {
-                WARNING_MESSAGE( "Division by an interval containing zero is not defined under the basic interval arithmetic." 
-                                 "As a result of this division we return the maximum interval\n", 0 );
-                result = Nodecl::Analysis::OpenInterval::make( Nodecl::Analysis::MinusInfinity::make( ), Nodecl::Analysis::PlusInfinity::make( ), t );
-            }
-            else
-            {
-                Nodecl::List expr_list = Nodecl::List::make( 
-                        Nodecl::Div::make( c1_lb.shallow_copy( ), c2_lb.shallow_copy( ), t ), 
-                        Nodecl::Div::make( c1_lb.shallow_copy( ), c2_ub.shallow_copy( ), t ), 
-                        Nodecl::Div::make( c1_ub.shallow_copy( ), c2_lb.shallow_copy( ), t ), 
-                        Nodecl::Div::make( c1_ub.shallow_copy( ), c2_ub.shallow_copy( ), t ) );
-                Nodecl::Analysis::Minimum lhs = Nodecl::Analysis::Minimum::make( expr_list, t );
-                Nodecl::Analysis::Maximum rhs = Nodecl::Analysis::Maximum::make( expr_list.shallow_copy( ), t );
-                result = Nodecl::Analysis::ClosedInterval::make( lhs, rhs, t );
-            }
+        Nodecl::NodeclBase r1_lb = r1.get_lower();   Nodecl::NodeclBase r1_ub = r1.get_upper();
+        Nodecl::NodeclBase r2_lb = r2.get_lower();   Nodecl::NodeclBase r2_ub = r2.get_upper();
+        TL::Type t = r1_lb.get_type();
+        if( ( r1_lb == r2_lb ) && ( r1_ub == r2_ub ) )
+        {   // The two ranges are exactly the same
+            result = r1.shallow_copy( );
         }
-        
-        return result;
-    }
-    
-    // [a, b] ∩ [c, d] = [max(a, c), min(b, d)]
-    Nodecl::NodeclBase range_intersection( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
-    {
-        Nodecl::NodeclBase result;
-        
-        Nodecl::NodeclBase c1_lb = c1.get_lower( );   Nodecl::NodeclBase c1_ub = c1.get_upper( );
-        Nodecl::NodeclBase c2_lb = c2.get_lower( );   Nodecl::NodeclBase c2_ub = c2.get_upper( );
-        TL::Type t = c1_lb.get_type( );
-        
-        if( !empty_interval( c1 ) && !empty_interval( c2 ) && 
-            c1_lb.is_constant( ) && c1_ub.is_constant( ) && c2_lb.is_constant( ) && c2_ub.is_constant( ) )
+        else if( r1_lb.is_constant() && r1_ub.is_constant() && r2_lb.is_constant() && r2_ub.is_constant() )
         {
-            Nodecl::NodeclBase lb, ub;
-            if( const_value_is_positive( const_value_sub( c1_lb.get_constant( ), c2_lb.get_constant( ) ) ) )
-                lb = c1_lb;
+            const_value_t* r1_lb_c = r1_lb.get_constant();
+            const_value_t* r1_ub_c = r1_ub.get_constant();
+            const_value_t* r2_lb_c = r2_lb.get_constant();
+            const_value_t* r2_ub_c = r2_ub.get_constant();
+            
+            if( const_value_is_positive( const_value_sub( r1_lb_c, r2_ub_c ) ) || 
+                const_value_is_positive( const_value_sub( r2_lb_c, r1_ub_c ) ) )
+            {   // r1 and r2 do not overlap
+                result = Nodecl::Analysis::RangeUnion::make( r1.shallow_copy(), r2.shallow_copy(), t );
+            }
+            else if( const_value_is_zero( const_value_sub( r1_lb_c, r2_lb_c ) ) && 
+                     const_value_is_zero( const_value_sub( r1_ub_c, r2_ub_c ) ) )
+            {   // r1 and r2 are the same range
+                result = r1.shallow_copy();
+            }
             else
-                lb = c2_lb;
-            if( const_value_is_positive( const_value_sub( c1_ub.get_constant( ), c2_ub.get_constant( ) ) ) )
-                ub = c2_ub;
-            else
-                lb = c1_ub;
-            result = Nodecl::Analysis::ClosedInterval::make( lb, ub, t );
+            {   // r1 and r2 overlap in some way
+                Nodecl::NodeclBase lb = const_value_is_positive( const_value_sub( r1_lb_c, r2_lb_c ) ) ? r2_lb.shallow_copy() 
+                                                                                                       : r1_lb.shallow_copy();
+                Nodecl::NodeclBase ub = const_value_is_positive( const_value_sub( r1_ub_c, r2_ub_c ) ) ? r1_ub.shallow_copy() 
+                                                                                                       : r2_ub.shallow_copy();
+                result = Nodecl::Analysis::Range::make( lb, ub, t );
+            }
         }
         else
         {
-            Nodecl::Analysis::Maximum lb = Nodecl::Analysis::Maximum::make( Nodecl::List::make( c1_lb.shallow_copy( ), c2_lb.shallow_copy( ) ), t );
-            Nodecl::Analysis::Minimum ub = Nodecl::Analysis::Minimum::make( Nodecl::List::make( c1_ub.shallow_copy( ), c2_ub.shallow_copy( ) ), t );
-            result = Nodecl::Analysis::ClosedInterval::make( lb, ub, t );
+            result = Nodecl::Analysis::RangeUnion::make( r1.shallow_copy(), r2.shallow_copy(), t );
         }
         
-        return result;
-    }
-    
-    // [a, b] ∪ [c, d] = [min(a, c), max(b, d)]
-    Nodecl::NodeclBase range_union( const Nodecl::Analysis::ClosedInterval& c1, const Nodecl::Analysis::ClosedInterval& c2 )
-    {
-        Nodecl::Analysis::ClosedInterval result;
-        
-        Nodecl::NodeclBase c1_lb = c1.get_lower( ).shallow_copy( );   Nodecl::NodeclBase c1_ub = c1.get_upper( ).shallow_copy( );
-        Nodecl::NodeclBase c2_lb = c2.get_lower( ).shallow_copy( );   Nodecl::NodeclBase c2_ub = c2.get_upper( ).shallow_copy( );
-        TL::Type t = c1_lb.get_type( );
-        
-        if( !empty_interval( c1 ) && !empty_interval( c2 ) && 
-            c1_lb.is_constant( ) && c1_ub.is_constant( ) && c2_lb.is_constant( ) && c2_ub.is_constant( ) )
-        {
-            Nodecl::NodeclBase lb = ( const_value_is_positive( const_value_sub( c1_lb.get_constant( ), c2_lb.get_constant( ) ) ) ? c2_lb : c1_lb );
-            Nodecl::NodeclBase ub = ( const_value_is_positive( const_value_sub( c1_ub.get_constant( ), c2_ub.get_constant( ) ) ) ? c1_ub : c2_ub );
-            result = Nodecl::Analysis::ClosedInterval::make( lb, ub, t );
-        }
-        else
-        {
-            Nodecl::Analysis::Minimum lb = Nodecl::Analysis::Minimum::make( Nodecl::List::make( c1_lb.shallow_copy( ), c2_lb.shallow_copy( ) ), t );
-            Nodecl::Analysis::Maximum ub = Nodecl::Analysis::Maximum::make( Nodecl::List::make( c1_ub.shallow_copy( ), c2_ub.shallow_copy( ) ), t );
-            result = Nodecl::Analysis::ClosedInterval::make( lb, ub, t );
-        }
         return result;
     }
 
