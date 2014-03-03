e600c201d2c90980d5f3a339c47a5cea8fefe325
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-03-03 18:15:41 +0100

    Disable invalid test

--- tests/01_fortran.dg/success_constant_expr_07.f90
+++ tests/01_fortran.dg/success_constant_expr_07.f90
@@ -1,11 +1,12 @@
 ! <testinfo>
-! test_generator=config/mercurium-fortran
+! test_ignore=yes
 ! </testinfo>
 PROGRAM P
     TYPE T
         INTEGER, POINTER :: X
     END TYPE T
 
+    ! This turned to be not valid Fortran
     TYPE(T), PARAMETER :: A = T(NULL())
 
     PRINT *, A % X
