3512ba60967b832e7b01e945504add226dccb8de
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-04-23 00:06:18 +0200

    Add hash

---
+++ lib/dhash_str.h
@@ -0,0 +1,20 @@
+#ifndef DHASH_STR_H
+#define DHASH_STR_H
+
+// Hash for strings
+
+typedef void* dhash_info_t;
+
+typedef struct dhash_str_tag dhash_str_t;
+
+dhash_str_t* dhash_str_new(int initial_size);
+void dhash_str_destroy(dhash_str_t*);
+void* dhash_str_query(dhash_str_t*, const char* key);
+void dhash_str_insert(dhash_str_t*, const char* key, dhash_info_t info);
+void dhash_str_remove(dhash_str_t*, const char* key);
+
+typedef void dhash_walk_fn(const char* key, void* info, void *walk_info);
+
+void dhash_str_walk(dhash_str_t*, dhash_walk_fn walk_fn, void* walk_info);
+
+#endif // DHASH_STR_H
