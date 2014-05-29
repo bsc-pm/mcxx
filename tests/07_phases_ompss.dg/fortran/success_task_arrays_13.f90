c260f13d2cc0945f3be4394b91fe3e2e91113657
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-05-29 09:20:49 +0200

    If no range is given for an assumed size array-section it means one element
    This commit fixes #2118

---
+++ tests/07_phases_ompss.dg/fortran/success_task_arrays_13.f90
@@ -0,0 +1,23 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER :: X(10, 20) 
+
+    X = 1
+    CALL FOO(X)
+    !$OMP TASKWAIT
+
+    IF (ANY(X(1, 1:10) /= 2)) STOP 1
+END PROGRAM P
+
+SUBROUTINE FOO(X)
+IMPLICIT NONE
+INTEGER :: X(10, *)
+
+!$OMP TASK INOUT(X(1, 1:10))
+   X(1, 1:10) = X(1, 1:10) + 1
+!$OMP END TASK
+
+END SUBROUTINE FOO
