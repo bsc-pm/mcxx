dd0d6f2347e690020b194fab318d05c9eb9bd114
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-06-06 17:00:42 +0200

    Progress commit

--- src/tl/omp/core/tl-omp-target.cpp
+++ src/tl/omp/core/tl-omp-target.cpp
@@ -381,7 +381,8 @@ namespace TL
                 DataSharingEnvironment& data_sharing,
                 const ObjectList<Nodecl::NodeclBase>& list,
                 CopyDirection copy_direction,
-                TargetInfo& target_info)
+                TargetInfo& target_info,
+                bool in_ompss_mode)
         {
             TL::ObjectList<CopyItem> items;
 
@@ -402,61 +403,68 @@ namespace TL
                     continue;
                 }
 
-                Symbol sym = expr.get_base_symbol();
-                OpenMP::DataSharingAttribute data_sharing_attr = data_sharing.get_data_sharing(sym);
+                // In OmpSs copies we may fix the data-sharing to something more natural
+                if (in_ompss_mode)
+                {
+                    Symbol sym = expr.get_base_symbol();
+                    OpenMP::DataSharingAttribute data_sharing_attr = data_sharing.get_data_sharing(sym);
 
-                std::cerr << "CORE-TARGET: " << sym.get_name() << " has data-sharing: " << string_of_data_sharing(data_sharing_attr) << std::endl;
+                    if (expr.is<Nodecl::Symbol>())
+                    {
+                        if (data_sharing_attr == DS_UNDEFINED)
+                        {
+                            warn_printf("%s: warning: symbol '%s' does not have any data sharing, assuming 'shared'\n",
+                                    construct.get_locus_str().c_str(),
+                                    sym.get_name().c_str());
+                            // Make it shared if we know nothing about this entity
+                            data_sharing.set_data_sharing(sym, DS_SHARED,
+                                    "specified in copy_in/copy_out/copy_inout but no data-sharing was defined for it");
+                        }
 
-                // if (expr.is<Nodecl::Symbol>())
-                // {
-                //     if (data_sharing_attr == DS_UNDEFINED)
-                //     {
-                //         warn_printf("%s: warning: symbol '%s' does not have any data sharing, assuming 'shared'\n",
-                //                 construct.get_locus_str().c_str(),
-                //                 sym.get_name().c_str());
-                //         // Make it shared if we know nothing about this entity
-                //         data_sharing.set_data_sharing(sym, DS_SHARED,
-                //                 "specified in copy_in/copy_out/copy_inout but no data-sharing was defined for it");
-                //     }
+                        if ((data_sharing_attr & DS_PRIVATE) == DS_PRIVATE)
+                        {
+                            if ((data_sharing_attr & DS_IMPLICIT) != DS_IMPLICIT)
+                            {
+                                // This is an explicit data sharing of a private
+                                // entity, which is being copied, this is wrong
+                                running_error("%s: error: invalid non-shared data-sharing for copied entity '%s'\n",
+                                        construct.get_locus_str().c_str(),
+                                        sym.get_name().c_str());
+                            }
+                            else
+                            {
+                                // Otherwise just override the sharing attribute with shared
+                                data_sharing.set_data_sharing(sym, (OpenMP::DataSharingAttribute)(DS_SHARED | DS_IMPLICIT),
+                                        "entity was privatized but it appears in copy_in/copy_out/copy_inout, "
+                                        "so it has been coerced to shared");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Type sym_type = sym.get_type();
+                        if (sym_type.is_any_reference())
+                        {
+                            sym_type = sym_type.references_to();
+                        }
 
-                //     if ((data_sharing_attr & DS_PRIVATE) == DS_PRIVATE)
-                //     {
-                //         if ((data_sharing_attr & DS_IMPLICIT) != DS_IMPLICIT)
-                //         {
-                //             // This is an explicit data sharing of a private
-                //             // entity, which is being copied, this is wrong
-                //             running_error("%s: error: invalid non-shared data-sharing for copied entity '%s'\n",
-                //                     construct.get_locus_str().c_str(),
-                //                     sym.get_name().c_str());
-                //         }
-                //         else
-                //         {
-                //             // Otherwise just override the sharing attribute with shared
-                //             data_sharing.set_data_sharing(sym, (OpenMP::DataSharingAttribute)(DS_SHARED | DS_IMPLICIT),
-                //                     "entity was privatized but it appears in copy_in/copy_out/copy_inout, "
-                //                     "so it has been coerced to shared");
-                //         }
-                //     }
-                // }
-                // else
-                // {
-                //     Type sym_type = sym.get_type();
-                //     if (sym_type.is_any_reference())
-                //     {
-                //         sym_type = sym_type.references_to();
-                //     }
-
-                //     if (sym_type.is_array())
-                //     {
-                //         data_sharing.set_data_sharing(sym, (DataSharingAttribute)(DS_SHARED | DS_IMPLICIT),
-                //                 "it is an array mentioned in a non-trivial way in a copy_in/copy_out/copy_inout clause");
-                //     }
-                //     else
-                //     {
-                //         data_sharing.set_data_sharing(sym, (DataSharingAttribute)(DS_FIRSTPRIVATE | DS_IMPLICIT),
-                //                 "it is an object mentioned in a non-trivial way in a copy_in/copy_out/copy_inout clause");
-                //     }
-                // }
+                        // Overwrite the data sharing if it was not set explicitly
+                        if ((data_sharing_attr & DS_IMPLICIT) == DS_IMPLICIT
+                                || data_sharing_attr == DS_UNDEFINED)
+                        {
+                            if (sym_type.is_array())
+                            {
+                                data_sharing.set_data_sharing(sym, (DataSharingAttribute)(DS_SHARED | DS_IMPLICIT),
+                                        "it is an array mentioned in a non-trivial way in a copy_in/copy_out/copy_inout clause");
+                            }
+                            else
+                            {
+                                data_sharing.set_data_sharing(sym, (DataSharingAttribute)(DS_FIRSTPRIVATE | DS_IMPLICIT),
+                                        "it is an object mentioned in a non-trivial way in a copy_in/copy_out/copy_inout clause");
+                            }
+                        }
+                    }
+                }
 
                 CopyItem copy_item(expr, copy_direction);
                 items.append(copy_item);
@@ -500,17 +508,20 @@ namespace TL
             add_copy_items(construct, data_sharing,
                     target_ctx.copy_in,
                     COPY_DIR_IN,
-                    target_info);
+                    target_info,
+                    in_ompss_mode());
 
             add_copy_items(construct, data_sharing,
                     target_ctx.copy_out,
                     COPY_DIR_OUT,
-                    target_info);
+                    target_info,
+                    in_ompss_mode());
 
             add_copy_items(construct, data_sharing,
                     target_ctx.copy_inout,
                     COPY_DIR_INOUT,
-                    target_info);
+                    target_info,
+                    in_ompss_mode());
 
             target_info.set_file(target_ctx.file);
             target_info.set_name(target_ctx.name);
@@ -567,17 +578,20 @@ namespace TL
                 add_copy_items(construct, data_sharing,
                         dep_list_in,
                         COPY_DIR_IN,
-                        target_info);
+                        target_info,
+                        in_ompss_mode());
 
                 add_copy_items(construct, data_sharing,
                         dep_list_out,
                         COPY_DIR_OUT,
-                        target_info);
+                        target_info,
+                        in_ompss_mode());
 
                 add_copy_items(construct, data_sharing,
                         dep_list_inout,
                         COPY_DIR_INOUT,
-                        target_info);
+                        target_info,
+                        in_ompss_mode());
             }
 
             if (this->in_ompss_mode()
