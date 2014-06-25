dbf3bf44d76a5f814094d33d138fa1f53016f438
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-06-25 08:48:44 +0200

    Instantiate NODECL_FIELD_DESIGNATOR and NODECL_INDEX_DESIGNATOR

--- src/frontend/cxx-exprtype.c
+++ src/frontend/cxx-exprtype.c
@@ -21852,6 +21852,38 @@ static void instantiate_structured_value(nodecl_instantiate_expr_visitor_t* v, n
     //FIXME: We should check this new structured value
 }
 
+static void instantiate_field_designator(nodecl_instantiate_expr_visitor_t* v, nodecl_t node)
+{
+    nodecl_t nodecl_name = nodecl_shallow_copy(nodecl_get_child(node, 0));
+    nodecl_t nodecl_next = instantiate_expr_walk(v, nodecl_get_child(node, 1));
+
+    v->nodecl_result =
+        nodecl_make_field_designator(
+                nodecl_name,
+                nodecl_next,
+                nodecl_get_type(node),
+                nodecl_get_locus(node));
+
+    nodecl_set_constant(v->nodecl_result,
+            nodecl_get_constant(node));
+}
+
+static void instantiate_index_designator(nodecl_instantiate_expr_visitor_t* v, nodecl_t node)
+{
+    nodecl_t nodecl_index = instantiate_expr_walk(v, nodecl_get_child(node, 0));
+    nodecl_t nodecl_next = instantiate_expr_walk(v, nodecl_get_child(node, 1));
+
+    v->nodecl_result =
+        nodecl_make_index_designator(
+                nodecl_index,
+                nodecl_next,
+                nodecl_get_type(node),
+                nodecl_get_locus(node));
+
+    nodecl_set_constant(v->nodecl_result,
+            nodecl_get_constant(node));
+}
+
 static void instantiate_reference(nodecl_instantiate_expr_visitor_t* v, nodecl_t node)
 {
     nodecl_t nodecl_op = instantiate_expr_walk(v, nodecl_get_child(node, 0));
@@ -22742,6 +22774,9 @@ static void instantiate_expr_init_visitor(nodecl_instantiate_expr_visitor_t* v, 
     NODECL_VISITOR(v)->visit_bitwise_not = instantiate_expr_visitor_fun(instantiate_unary_op);
 
     NODECL_VISITOR(v)->visit_structured_value = instantiate_expr_visitor_fun(instantiate_structured_value);
+
+    NODECL_VISITOR(v)->visit_field_designator = instantiate_expr_visitor_fun(instantiate_field_designator);
+    NODECL_VISITOR(v)->visit_index_designator = instantiate_expr_visitor_fun(instantiate_index_designator);
 
     // Function call
     NODECL_VISITOR(v)->visit_function_call = instantiate_expr_visitor_fun(instantiate_function_call);
