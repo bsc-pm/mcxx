bsc-pm/mcxx

commit 934c786b915b7074bbeefe150acb8abdf731dc54

Fixes in CFG Analysis
Due to transforming Liveness analysis in a bottom-up analysis,
a problem in the structure of the PCFG has appeared: the occurrence
of return statements in the middle of the code causes their container
graph node to have their Exit node unconnected. This breaks the assumed
rule that all graph nodes start with an Entry node and finish with
an Exit node. This commit fixes this problem and also fixes the method
that cleans up the PCFG from bottom-up to handles this extrange situation.

This commit fixes test defs_01.c, broken in commit
6a7a083d784e6a7a4c8769eee3147999da186c74.

src/tl/analysis/pcfg/tl-extensible-graph.cpp
@@ -285,63 +285,6 @@ namespace Analysis {
         clear_unnecessary_nodes();
         concat_sequential_nodes();
     }
-    
-    void ExtensibleGraph::remove_unnecessary_connections_rec(Node* n)
-    {
-        if(!n->is_visited_extgraph())
-        {
-            n->set_visited_extgraph(true);
-            
-            ObjectList<Node*> children = n->get_children();
-            if(n->is_graph_node())
-            {   // Only graph nodes may be unnecessarily connected 
-                
-                // First try recursively in the inner nodes
-                remove_unnecessary_connections_rec(n->get_graph_entry_node());
-                
-                // Second check whether the current graph node has to be unconnected
-                Node* exit = n->get_graph_exit_node();
-                if(exit->get_entry_edges().empty())
-                {   // If the exit of the graph node has no parent, 
-                    // then the graph node should not have any children
-                    std::queue<Node*> nodes_to_disconnect;
-                    nodes_to_disconnect.push(n);
-                    while(!nodes_to_disconnect.empty())
-                    {
-                        Node* n_ = nodes_to_disconnect.front();
-                        nodes_to_disconnect.pop();
-                        
-                        ObjectList<Node*> tmp = n_->get_children();
-                        for(ObjectList<Node*>::iterator it = tmp.begin(); it != tmp.end(); ++it)
-                        {
-                            disconnect_nodes(n_, *it);
-                            if((*it)->get_entry_edges().empty() && !(*it)->is_graph_node())
-                            {   // We have removed the only parent that this node had => keep removing unconnected node
-                                // But do not disconnect graph nodes, because they may have unexpected inner connections
-                                // We do not delete the nodes because they may be structurally necessary (i.e.: the exit node of a graph node)
-                                if((*it)->is_exit_node())
-                                    nodes_to_disconnect.push((*it)->get_outer_node());
-                                else
-                                    nodes_to_disconnect.push(*it);
-                            }
-                        }
-                    }
-                }
-            }
-            
-            // Get again the children, because we may have deleted some of them
-            children = n->get_children();
-            for(ObjectList<Node*>::iterator it = children.begin(); it != children.end(); ++it)
-                remove_unnecessary_connections_rec(*it);
-        }
-    }
-    
-    void ExtensibleGraph::remove_unnecessary_connections()
-    {
-        Node* entry = _graph->get_graph_entry_node();
-        remove_unnecessary_connections_rec(entry);
-        clear_visits_extgraph(entry);
-    }
 
     void ExtensibleGraph::concat_sequential_nodes()
     {
@@ -802,39 +745,50 @@ namespace Analysis {
         }
     }
 
-    static void clear_visits_backwards_rec(Node* n)
+    static void clear_visits_backwards_rec(Node* n, Node* sc)
     {
-        if(n->is_visited())
+        if (n->is_visited())
         {
             n->set_visited(false);
-
-            if(n->is_graph_node())
-                clear_visits_backwards_rec(n->get_graph_exit_node());
+            if (n->is_graph_node())
+                clear_visits_backwards_rec(n->get_graph_exit_node(), sc);
 
             if (n->is_entry_node())
             {
-                n = n->get_outer_node();
+                // If the parents of the outer node have still one child that must be clean up
+                // within the scope of the cleaning, then do not keep cleaning this path
+                // because we will still visit the outer from that child
+                bool all_visited = true;
+                const ObjectList<Node*>& children = n->get_outer_node()->get_children();
+                for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
+                {
+                    if (!(*it)->is_visited() && ExtensibleGraph::node_contains_node(sc, *it))
+                        all_visited = false;
+                }
+
+                if (all_visited)
+                    n = n->get_outer_node();
                 n->set_visited(false);  // Be sure we do not miss any node
                                         // in case we clean up from the middle of the graph
             }
             const ObjectList<Node*>& parents = n->get_parents();
-            for(ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
-                clear_visits_backwards_rec(*it);
+            for (ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
+                clear_visits_backwards_rec(*it, sc);
         }
     }
 
-    void ExtensibleGraph::clear_visits_backwards(Node* n)
+    void ExtensibleGraph::clear_visits_backwards(Node* n, Node* sc)
     {
         if(n->is_graph_node())
         {
             n->set_visited(false);
             const ObjectList<Node*>& parents = n->get_parents();
             for(ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
-                clear_visits_backwards_rec(*it);
+                clear_visits_backwards_rec(*it, n);
         }
         else
         {
-            clear_visits_backwards_rec(n);
+            clear_visits_backwards_rec(n, sc);
         }
     }
 

src/tl/analysis/liveness/tl-liveness.cpp
@@ -54,7 +54,7 @@ namespace Analysis {
         // Note: 'n', which is the most outer node of the graph, must be cleaned up separatedly
         //       because clear_visits_backwards skips entering the first node it is called with
         //       in case it is a graph node
-        ExtensibleGraph::clear_visits_backwards(exit);
+        ExtensibleGraph::clear_visits_backwards(exit, graph);
         graph->set_visited(false);
 
         // Common Liveness analysis
@@ -63,7 +63,7 @@ namespace Analysis {
         {
             changed = false;
             solve_live_equations_rec(graph, changed);
-            ExtensibleGraph::clear_visits_backwards(exit);
+            ExtensibleGraph::clear_visits_backwards(exit, graph);
             graph->set_visited(false);
         }
     }

src/tl/analysis/pcfg/tl-extensible-graph.hpp
@@ -298,10 +298,6 @@ namespace Analysis {
         //! during the construction of the graph but do not represent any statement of the code, and also
         //! concatenates the nodes that will be executed sequentially for sure (Basic Blocks)
         void dress_up_graph();
-        
-        //! Return nodes may cause unconnected parts of the code to be connected
-        //! This method removes this dead connections
-        void remove_unnecessary_connections();
 
         //! This method concatenates a list of nodes into only one
         /*!
@@ -346,7 +342,7 @@ namespace Analysis {
         static void clear_visits_aux_in_level(Node* node, Node* outer_node);
 
         //! Set to false the attribute #_visited of those nodes whose post-dominator is node @node
-        static void clear_visits_backwards(Node* node);
+        static void clear_visits_backwards(Node* node, Node* graph);
 
 
         // *** DOT Graph *** //

src/tl/analysis/pcfg/tl-pcfg-visitor.cpp
@@ -57,8 +57,9 @@ namespace Analysis {
         _pcfg = graph;
     }
 
-    ExtensibleGraph* PCFGVisitor::parallel_control_flow_graph(const NBase& n,
-                                                              const std::map<Symbol, NBase>& asserted_funcs)
+    ExtensibleGraph* PCFGVisitor::parallel_control_flow_graph(
+            const NBase& n,
+            const std::map<Symbol, NBase>& asserted_funcs)
     {
         _asserted_funcs = asserted_funcs;
 
@@ -74,17 +75,44 @@ namespace Analysis {
         // some of they may have no parent (a return has been found inside the context they belong to)
         _pcfg->connect_nodes(_utils->_last_nodes, pcfg_exit);   
         // Get the exit node of the most outer context (generated by the code function) if exists
-        // This may not exist if the PCFG is generated for something different from a
+        // This may not exist if the PCFG is generated for something different from a function code
         Node* returns_exit = pcfg_exit;
-        ObjectList<Node*> pcfg_exit_parents = pcfg_exit->get_parents( );
+        const ObjectList<Node*>& pcfg_exit_parents = pcfg_exit->get_parents( );
         if( ( pcfg_exit_parents.size( ) == 1 ) && ( pcfg_exit_parents[0]->is_context_node( ) ) )
             returns_exit = pcfg_exit_parents[0]->get_graph_exit_node( );
-        _pcfg->connect_nodes( _utils->_return_nodes, returns_exit );
-        _utils->_return_nodes.clear( );
+        for (ObjectList<Node*>::iterator it = _utils->_return_nodes.begin();
+             it != _utils->_return_nodes.end(); ++it)
+        {
+            Node* source = *it;
+            Node* outer = source->get_outer_node();
+            Node* exit = outer->get_graph_exit_node();
+            ObjectList<Node*> parents = exit->get_parents();
+            if (parents.empty())
+            {   // The exit node E1 of this graph node G1 is not connected:
+                // 1.- Connect the return Ri to E1
+                // 2.- Traverse G outer nodes while Exit(G) has no parents and
+                //     Connect G[i] to Exit(G[i+1]) and Disconnect Outer(Exit(G[i+1])) from all its children
+                //     When we find an Exit(G) that already has some parent (another path leads to it)
+                //     connect G[i] to the exit node of the whole graph
+                while (parents.empty())
+                {
+                    _pcfg->connect_nodes(source, exit);
+                    _pcfg->disconnect_nodes(outer, outer->get_children());
+                    source = outer;
+                    outer = outer->get_outer_node();
+                    if (outer == NULL)
+                        goto next_it;
+                    exit = outer->get_graph_exit_node();
+                    parents = exit->get_parents();
+                }
+            }
+            _pcfg->connect_nodes(source, returns_exit);
+next_it:    ;
+        }
+        _utils->_return_nodes.clear();
 
         //_pcfg->dress_up_graph( );
-        _pcfg->remove_unnecessary_connections();
-        
+
         return _pcfg;
     }
 

src/tl/analysis/pcfg/tl-task-sync.cpp
@@ -1227,7 +1227,7 @@ task_synchronized:      break;
         //             find_last_synchronization_point_in_children( task, task_outer );
         //         }
         
-        ExtensibleGraph::clear_visits_backwards( task );
+        ExtensibleGraph::clear_visits_backwards(task, _graph->get_graph());
     }
 
     void TaskConcurrency::find_last_synchronization_point_in_parents( Node* current )

tests/07_phases_analysis.dg/defs_01.c
@@ -36,14 +36,19 @@
 int x = 0;
 
 #pragma analysis_check assert_decl live_in(x) defined(x) upper_exposed(x)
-// int main(int argc, char *argv[])
-void foo()
+int main(int argc, char *argv[])
 {
-    int a;
-    #pragma analysis_check assert defined(a)
-    a = 0;
-    
+    int a, b;
+    #pragma analysis_check assert defined(a, b)
+    a = b = 0;
+
     x++;
-    
-//     return 0;
+
+    #pragma analysis_check assert_decl live_out(x)
+    if(x)
+        return 0;
+    #pragma analysis_check assert live_in(x) dead(b)
+    b = x + 20;
+
+    return 0;
 }

commit 29a26cad0a0103501a90063131edc7323e7ec0e2

Fix PCFG backwards clean-up
This mistake was introduced in commit 934c786b915b7074bbeefe150acb8abdf731dc54.

src/tl/analysis/pcfg/tl-extensible-graph.cpp
@@ -758,15 +758,14 @@ namespace Analysis {
                 // If the parents of the outer node have still one child that must be clean up
                 // within the scope of the cleaning, then do not keep cleaning this path
                 // because we will still visit the outer from that child
-                bool all_visited = true;
+                bool all_cleaned = true;
                 const ObjectList<Node*>& children = n->get_outer_node()->get_children();
                 for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
                 {
-                    if (!(*it)->is_visited() && ExtensibleGraph::node_contains_node(sc, *it))
-                        all_visited = false;
+                    if ((*it)->is_visited() && ExtensibleGraph::node_contains_node(sc, *it))
+                        all_cleaned = false;
                 }
-
-                if (all_visited)
+                if (all_cleaned)
                     n = n->get_outer_node();
                 n->set_visited(false);  // Be sure we do not miss any node
                                         // in case we clean up from the middle of the graph

tests/07_phases_analysis.dg/reach_defs_02.cpp
@@ -1,0 +1,64 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2012 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+  
+  This file is part of Mercurium C/C++ source-to-source compiler.
+  
+  See AUTHORS file in the top level directory for information
+  regarding developers and contributors.
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+
+
+/*
+<testinfo>
+test_generator=config/mercurium-analysis
+test_nolink=yes
+</testinfo>
+*/
+
+#include <string>
+
+std::string UNKNOWN = "UNKNOWN";
+
+int foo(int n)
+{
+    int result;
+
+    if (n > 2)
+    {
+        if (n < 100)
+        {
+            if (n > 50)
+                return 1;
+            return 2;
+        }
+        else
+        {
+            #pragma analysis_check assert live_in(n) live_out(n)
+            result = n / 0;
+        }
+        
+        #pragma analysis_check assert reaching_definition_in(n: UNKNOWN) reaching_definition_out(result: n/0)
+        result = n / 0;
+    }
+    
+    #pragma analysis_check assert reaching_definition_in(result: n/0)
+    return result;
+}