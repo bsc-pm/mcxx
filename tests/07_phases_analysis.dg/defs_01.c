bsc-pm/mcxx

commit 6a7a083d784e6a7a4c8769eee3147999da186c74

Fix Liveness Analysis for tasks
This analysis is now more conservative in the sense that: if there
is a possible path where a variable is alive, then we keep it alive.

This commit changes the directionality of the data-flow algorithm from
top-bottom to bottom-top, this way the algorithm will iterate less times.
This change has broken the test 'defs_01.c'. This is because return
statements in the middle of the code cause some graph nodes to have
their exit nodes unconnected.
Because of that, the assumed condition of having all graph nodes with
an entry node at the beginning and an exit node at the end does not
fulfill.

This commit fixes the test that was broken with the temporary commit
78c04e6e7d4cf26f279ac068a03e2f8a48e121a3.

src/tl/analysis/liveness/tl-liveness.cpp
@@ -47,107 +47,151 @@ namespace Analysis {
         tc.compute_tasks_concurrency();
         
         Node* graph = _graph->get_graph();
+        Node* exit = graph->get_graph_exit_node();
 
         // Compute initial info (liveness only regarding the current node => UE vars)
-        gather_live_initial_information(graph);
-        ExtensibleGraph::clear_visits(graph);
+        initialize_live_sets(graph);
+        // Note: 'n', which is the most outer node of the graph, must be cleaned up separatedly
+        //       because clear_visits_backwards skips entering the first node it is called with
+        //       in case it is a graph node
+        ExtensibleGraph::clear_visits_backwards(exit);
+        graph->set_visited(false);
 
         // Common Liveness analysis
-        solve_live_equations(graph);
-        ExtensibleGraph::clear_visits(graph);
-    }
-
-    void Liveness::gather_live_initial_information(Node* current)
-    {
-        if(current->is_visited())
-            return;
-
-        current->set_visited(true);
-
-        if(current->is_exit_node())
-            return;
-
-        if (current->is_graph_node())
-        {
-            gather_live_initial_information(current->get_graph_entry_node());
-            set_graph_node_liveness(current, NULL);
-        }
-        else if (!current->is_entry_node())
-        {
-            current->set_live_in(current->get_ue_vars());
-        }
-
-        const ObjectList<Edge*>& exit_edges = current->get_exit_edges();
-        for (ObjectList<Edge*>::const_iterator it = exit_edges.begin(); it != exit_edges.end(); ++it)
-        {
-            gather_live_initial_information((*it)->get_target());
-        }
-    }
-
-    void Liveness::solve_live_equations(Node* current)
-    {
         bool changed = true;
         while(changed)
         {
             changed = false;
-            solve_live_equations_rec(current, changed, NULL);
-            ExtensibleGraph::clear_visits(current);
+            solve_live_equations_rec(graph, changed);
+            ExtensibleGraph::clear_visits_backwards(exit);
+            graph->set_visited(false);
         }
     }
 
-    void Liveness::solve_live_equations_rec(Node* current, bool& changed, Node* container_task)
+    void Liveness::initialize_live_sets(Node* n)
     {
-        if (current->is_visited())
+        if (n->is_visited())
             return;
 
-        current->set_visited(true);
+        n->set_visited(true);
 
-        if(current->is_exit_node())
+        if (n->is_entry_node())
             return;
 
-        if(current->is_omp_task_node())
+        if (n->is_graph_node())
         {
-            solve_live_equations_rec(current->get_graph_entry_node(), changed, current);
-            set_graph_node_liveness(current, container_task);
+            initialize_live_sets(n->get_graph_exit_node());
+            set_graph_node_liveness(n);
         }
-        else if (current->is_graph_node())
+        else if (!n->is_exit_node())
         {
-            solve_live_equations_rec(current->get_graph_entry_node(), changed, container_task);
-            set_graph_node_liveness(current, container_task);
+            n->set_live_in(n->get_ue_vars());
         }
-        else if (!current->is_entry_node())
+
+        const ObjectList<Node*>& parents = n->get_parents();
+        for (ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
+            initialize_live_sets(*it);
+    }
+
+    void Liveness::solve_live_equations_rec(Node* n, bool& changed)
+    {
+        if (n->is_visited())
+            return;
+
+        n->set_visited(true);
+
+        if (n->is_entry_node())
+            return;
+
+        if (n->is_graph_node())
         {
-            const NodeclSet& old_live_in = current->get_live_in_vars();
-            const NodeclSet& old_live_out = current->get_live_out_vars();
+            if (n->is_omp_task_node())
+                solve_task_live_equations_rec(n->get_graph_exit_node(), changed, n);
+            else
+                solve_live_equations_rec(n->get_graph_exit_node(), changed);
+            set_graph_node_liveness(n);
+        }
+        else if (!n->is_exit_node())
+        {
+            // 1.- Gather old liveness sets
+            const NodeclSet& old_live_out = n->get_live_out_vars();
+            const NodeclSet& old_live_in = n->get_live_in_vars();
 
-            // Computing Live Out
-            const NodeclSet& live_out = compute_live_out(current, container_task);
+            // 2.- Compute new liveness sets
+            // 2.1.- Compute Live Out: LO(x) = U LI(y), forall y ∈ Succ(x)
+            const NodeclSet& live_out = compute_successors_live_in(n);;
+            // 2.2.-Compute Live In: LI(x) = UE(x) U ( LO(x) - KILL(x) )
+            const NodeclSet& live_in = Utils::nodecl_set_union(n->get_ue_vars(),
+                    Utils::nodecl_set_difference(live_out, n->get_killed_vars()));
 
-            // Computing Live In
-            const NodeclSet& live_in = Utils::nodecl_set_union(current->get_ue_vars(),
-                    Utils::nodecl_set_difference(live_out, current->get_killed_vars()));
-
+            // 3.- Compare the two sets to see whether something has changed and, if yes, set the new values
             if (!Utils::nodecl_set_equivalence(old_live_in, live_in) ||
                 !Utils::nodecl_set_equivalence(old_live_out, live_out))
             {
-                current->set_live_in(live_in);
-                current->set_live_out(live_out);
+                n->set_live_in(live_in);
+                n->set_live_out(live_out);
                 changed = true;
             }
         }
 
-        const ObjectList<Node*>& children = current->get_children();
-        for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
-        {
-            solve_live_equations_rec(*it, changed, container_task);
-        }
+        const ObjectList<Node*>& parents = n->get_parents();
+        for (ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
+            solve_live_equations_rec(*it, changed);
     }
 
-    NodeclSet Liveness::compute_live_out(Node* current, Node* container_task)
+    void Liveness::solve_task_live_equations_rec(Node* n, bool& changed, Node* task)
     {
-        NodeclSet live_out, succ_live_in;
+        if(!n->is_exit_node())
+            return;
 
-        const ObjectList<Node*>& children = current->get_children();
+        n->set_visited(true);
+
+        // 1.- Compute the task successors LI set
+        const ObjectList<Node*>& parents = n->get_parents();
+        ERROR_CONDITION(parents.size()!=1,
+                        "The number of parents of a task exit node must be 1 (a flush node), but %d found.\n",
+                        parents.size());
+        Node* exit_flush = parents[0];
+        NodeclSet succ_live_in = compute_successors_live_in(exit_flush);
+
+        // 2.- Add to the list of successors, the flow successors of the Task Creation node of the current task
+        Node* task_creation = ExtensibleGraph::get_task_creation_from_task(task);
+        const ObjectList<Node*>& tc_children = task_creation->get_children();
+        for (ObjectList<Node*>::const_iterator it = tc_children.begin(); it != tc_children.end(); ++it)
+        {
+            if(*it != task)
+                succ_live_in = Utils::nodecl_set_union(succ_live_in, (*it)->get_live_in_vars());
+        }
+
+        // 3.- Remove from the set of successors LI those variables private to the task
+        const NodeclSet& private_vars = task->get_all_private_vars();
+        for (NodeclSet::const_iterator it = private_vars.begin(); it != private_vars.end(); ++it)
+            if (Utils::nodecl_set_contains_nodecl(*it, succ_live_in))
+                succ_live_in.erase(*it);
+
+        // 4.- Gather the old liveness sets
+        const NodeclSet& old_live_out = exit_flush->get_live_out_vars();
+        const NodeclSet& old_live_in = exit_flush->get_live_in_vars();
+
+        // 5.- Compare the two sets to see whether something has changed and, if yes, set the new values
+        if (!Utils::nodecl_set_equivalence(old_live_in, succ_live_in) ||
+            !Utils::nodecl_set_equivalence(old_live_out, succ_live_in))
+        {
+            exit_flush->set_live_out(succ_live_in);
+            exit_flush->set_live_in(succ_live_in);
+            changed = true;
+        }
+
+        // 6.- Keep iterating normally within the task
+        const ObjectList<Node*>& flush_parents = exit_flush->get_parents();
+        for (ObjectList<Node*>::const_iterator it = flush_parents.begin(); it != flush_parents.end(); ++it)
+            solve_live_equations_rec(*it, changed);
+    }
+
+    NodeclSet Liveness::compute_successors_live_in(Node* n)
+    {
+        NodeclSet succ_live_in;
+        const ObjectList<Node*>& children = n->get_children();
         for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
         {
             bool child_is_exit = (*it)->is_exit_node();
@@ -163,7 +207,7 @@ namespace Analysis {
                     exit_outer_node = (child_is_exit ? outer_children[0]->get_outer_node() : NULL);
                 }
                 // Get the Live in of the current successors
-                for(ObjectList<Node*>::iterator itoc = outer_children.begin(); itoc != outer_children.end(); ++itoc)
+                for (ObjectList<Node*>::iterator itoc = outer_children.begin(); itoc != outer_children.end(); ++itoc)
                 {
                     const NodeclSet& outer_live_in = (*itoc)->get_live_in_vars();
                     succ_live_in.insert(outer_live_in.begin(), outer_live_in.end());
@@ -173,93 +217,53 @@ namespace Analysis {
             {
                 succ_live_in = (*it)->get_live_in_vars();
             }
-
-            if(container_task != NULL)
-            {   // remove those variables that are killed in any task that runs concurrently
-                const ObjectList<Node*>& concurrent_tasks = _graph->get_task_concurrent_tasks(container_task);
-                for(ObjectList<Node*>::const_iterator itc = concurrent_tasks.begin(); itc != concurrent_tasks.end(); ++itc)
-                {
-                    const NodeclSet& task_killed_vars = (*itc)->get_killed_vars();
-                    for(NodeclSet::const_iterator itk = task_killed_vars.begin(); itk != task_killed_vars.end(); ++itk)
-                    {
-                        succ_live_in.erase(*itk);
-                    }
-                }
-            }
-            else if(current->is_omp_task_creation_node())
-            {   // remove those variables that are killed in the task I create
-                Node* created_task = current->get_children()[0];
-                const NodeclSet& task_killed_vars = created_task->get_killed_vars();
-                for(NodeclSet::const_iterator itk = task_killed_vars.begin(); itk != task_killed_vars.end(); ++itk)
-                {
-                    succ_live_in.erase(*itk);
-                }
-            }
-            live_out = Utils::nodecl_set_union(live_out, succ_live_in);
         }
-
-        return live_out;
+        return succ_live_in;
     }
 
-    void Liveness::set_graph_node_liveness(Node* current, Node* container_task)
+    void Liveness::set_graph_node_liveness(Node* n)
     {
-        if (current->is_graph_node())
-        {
-            // 1.- LI(graph) = U LI(inner entries)
-            NodeclSet live_in;
-            const ObjectList<Node*>& entries = current->get_graph_entry_node()->get_children();
-            for (ObjectList<Node*>::const_iterator it = entries.begin(); it != entries.end(); ++it)
-            {
-                live_in = Utils::nodecl_set_union(live_in, (*it)->get_live_in_vars());
-            }
-            // 1.1.- Delete those variables which are local to the graph
-            if (current->is_context_node())
-            {   // Variables declared within the current context
-                Scope sc(current->get_graph_related_ast().retrieve_context());
-                NodeclSet graph_li;
-                for(NodeclSet::iterator it = live_in.begin(); it != live_in.end(); ++it)
-                {
-                    const NBase& it_base = Utils::get_nodecl_base(*it);
-                    if (!it_base.retrieve_context().scope_is_enclosed_by(sc))
-                        graph_li.insert(*it);
-                }
-                current->set_live_in(graph_li);
-            }
-            else if (current->is_omp_task_node())
-            {   // Variables private to the task
-                NodeclSet graph_li;
-                NodeclSet p_vars = current->get_private_vars();
-                for (NodeclSet::iterator it = live_in.begin(); it != live_in.end(); ++it)
-                {
-                    if (p_vars.find(*it) == p_vars.end())
-                        graph_li.insert(*it);
-                }
-                current->set_live_in(graph_li);
-            }
-            else
-            {
-                current->set_live_in(live_in);
-            }
+        if (!n->is_graph_node())
+            return;
 
-            // 2.- LO(graph) = U LI(S), where S successor(graph)
-            const NodeclSet& live_out = compute_live_out(current, container_task);
-            // 2.1.- Delete those variables which are local to the graph
-            if (current->is_omp_task_node())
-            {   // Variables private or firstprivate to the task
-                NodeclSet graph_lo;
-                NodeclSet all_p_vars = current->get_all_private_vars();
-                for (NodeclSet::iterator it = live_in.begin(); it != live_in.end(); ++it)
-                {
-                    if (all_p_vars.find(*it) == all_p_vars.end())
-                        graph_lo.insert(*it);
-                }
-                current->set_live_out(graph_lo);
-            }
-            else
+        // 1.- LO(graph) = U L0(inner exits)
+        NodeclSet live_out;
+        const ObjectList<Node*>& parents = n->get_graph_exit_node()->get_parents();
+        for (ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
+            live_out = Utils::nodecl_set_union(live_out, (*it)->get_live_out_vars());
+        n->set_live_out(live_out);
+
+        // 2.- LI(graph) = U LI(inner entries)
+        NodeclSet all_live_in;
+        const ObjectList<Node*>& children = n->get_graph_entry_node()->get_children();
+        for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
+            all_live_in = Utils::nodecl_set_union(all_live_in, (*it)->get_live_in_vars());
+        // 2.1.- Delete those variables which are local to the graph
+        NodeclSet live_in;
+        if (n->is_context_node())
+        {   // Variables declared within the current context
+            Scope sc(n->get_graph_related_ast().retrieve_context());
+            for(NodeclSet::iterator it = all_live_in.begin(); it != all_live_in.end(); ++it)
             {
-                current->set_live_out(live_out);
+                const NBase& it_base = Utils::get_nodecl_base(*it);
+                if (!it_base.retrieve_context().scope_is_enclosed_by(sc))
+                    live_in.insert(*it);
             }
         }
+        else if (n->is_omp_task_node())
+        {   // Variables private to the task
+            NodeclSet p_vars = n->get_private_vars();
+            for (NodeclSet::iterator it = all_live_in.begin(); it != all_live_in.end(); ++it)
+            {
+                if (p_vars.find(*it) == p_vars.end())
+                    live_in.insert(*it);
+            }
+        }
+        else
+        {
+            live_in = all_live_in;
+        }
+        n->set_live_in(live_in);
     }
 
     // ***************************** END class implementing liveness analysis ***************************** //

tests/07_phases_analysis.dg/defs_01.c
@@ -36,13 +36,14 @@
 int x = 0;
 
 #pragma analysis_check assert_decl live_in(x) defined(x) upper_exposed(x)
-int main(int argc, char *argv[])
+// int main(int argc, char *argv[])
+void foo()
 {
     int a;
-    //     #pragma analysis_check assert defined(a)
+    #pragma analysis_check assert defined(a)
     a = 0;
     
     x++;
     
-    return 0;
+//     return 0;
 }

commit 934c786b915b7074bbeefe150acb8abdf731dc54

Fixes in CFG Analysis
Due to transforming Liveness analysis in a bottom-up analysis,
a problem in the structure of the PCFG has appeared: the occurrence
of return statements in the middle of the code causes their container
graph node to have their Exit node unconnected. This breaks the assumed
rule that all graph nodes start with an Entry node and finish with
an Exit node. This commit fixes this problem and also fixes the method
that cleans up the PCFG from bottom-up to handles this extrange situation.

This commit fixes test defs_01.c, broken in commit
6a7a083d784e6a7a4c8769eee3147999da186c74.

src/tl/analysis/liveness/tl-liveness.cpp
@@ -54,7 +54,7 @@ namespace Analysis {
         // Note: 'n', which is the most outer node of the graph, must be cleaned up separatedly
         //       because clear_visits_backwards skips entering the first node it is called with
         //       in case it is a graph node
-        ExtensibleGraph::clear_visits_backwards(exit);
+        ExtensibleGraph::clear_visits_backwards(exit, graph);
         graph->set_visited(false);
 
         // Common Liveness analysis
@@ -63,7 +63,7 @@ namespace Analysis {
         {
             changed = false;
             solve_live_equations_rec(graph, changed);
-            ExtensibleGraph::clear_visits_backwards(exit);
+            ExtensibleGraph::clear_visits_backwards(exit, graph);
             graph->set_visited(false);
         }
     }

src/tl/analysis/pcfg/tl-extensible-graph.cpp
@@ -285,63 +285,6 @@ namespace Analysis {
         clear_unnecessary_nodes();
         concat_sequential_nodes();
     }
-    
-    void ExtensibleGraph::remove_unnecessary_connections_rec(Node* n)
-    {
-        if(!n->is_visited_extgraph())
-        {
-            n->set_visited_extgraph(true);
-            
-            ObjectList<Node*> children = n->get_children();
-            if(n->is_graph_node())
-            {   // Only graph nodes may be unnecessarily connected 
-                
-                // First try recursively in the inner nodes
-                remove_unnecessary_connections_rec(n->get_graph_entry_node());
-                
-                // Second check whether the current graph node has to be unconnected
-                Node* exit = n->get_graph_exit_node();
-                if(exit->get_entry_edges().empty())
-                {   // If the exit of the graph node has no parent, 
-                    // then the graph node should not have any children
-                    std::queue<Node*> nodes_to_disconnect;
-                    nodes_to_disconnect.push(n);
-                    while(!nodes_to_disconnect.empty())
-                    {
-                        Node* n_ = nodes_to_disconnect.front();
-                        nodes_to_disconnect.pop();
-                        
-                        ObjectList<Node*> tmp = n_->get_children();
-                        for(ObjectList<Node*>::iterator it = tmp.begin(); it != tmp.end(); ++it)
-                        {
-                            disconnect_nodes(n_, *it);
-                            if((*it)->get_entry_edges().empty() && !(*it)->is_graph_node())
-                            {   // We have removed the only parent that this node had => keep removing unconnected node
-                                // But do not disconnect graph nodes, because they may have unexpected inner connections
-                                // We do not delete the nodes because they may be structurally necessary (i.e.: the exit node of a graph node)
-                                if((*it)->is_exit_node())
-                                    nodes_to_disconnect.push((*it)->get_outer_node());
-                                else
-                                    nodes_to_disconnect.push(*it);
-                            }
-                        }
-                    }
-                }
-            }
-            
-            // Get again the children, because we may have deleted some of them
-            children = n->get_children();
-            for(ObjectList<Node*>::iterator it = children.begin(); it != children.end(); ++it)
-                remove_unnecessary_connections_rec(*it);
-        }
-    }
-    
-    void ExtensibleGraph::remove_unnecessary_connections()
-    {
-        Node* entry = _graph->get_graph_entry_node();
-        remove_unnecessary_connections_rec(entry);
-        clear_visits_extgraph(entry);
-    }
 
     void ExtensibleGraph::concat_sequential_nodes()
     {
@@ -802,39 +745,50 @@ namespace Analysis {
         }
     }
 
-    static void clear_visits_backwards_rec(Node* n)
+    static void clear_visits_backwards_rec(Node* n, Node* sc)
     {
-        if(n->is_visited())
+        if (n->is_visited())
         {
             n->set_visited(false);
-
-            if(n->is_graph_node())
-                clear_visits_backwards_rec(n->get_graph_exit_node());
+            if (n->is_graph_node())
+                clear_visits_backwards_rec(n->get_graph_exit_node(), sc);
 
             if (n->is_entry_node())
             {
-                n = n->get_outer_node();
+                // If the parents of the outer node have still one child that must be clean up
+                // within the scope of the cleaning, then do not keep cleaning this path
+                // because we will still visit the outer from that child
+                bool all_visited = true;
+                const ObjectList<Node*>& children = n->get_outer_node()->get_children();
+                for (ObjectList<Node*>::const_iterator it = children.begin(); it != children.end(); ++it)
+                {
+                    if (!(*it)->is_visited() && ExtensibleGraph::node_contains_node(sc, *it))
+                        all_visited = false;
+                }
+
+                if (all_visited)
+                    n = n->get_outer_node();
                 n->set_visited(false);  // Be sure we do not miss any node
                                         // in case we clean up from the middle of the graph
             }
             const ObjectList<Node*>& parents = n->get_parents();
-            for(ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
-                clear_visits_backwards_rec(*it);
+            for (ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
+                clear_visits_backwards_rec(*it, sc);
         }
     }
 
-    void ExtensibleGraph::clear_visits_backwards(Node* n)
+    void ExtensibleGraph::clear_visits_backwards(Node* n, Node* sc)
     {
         if(n->is_graph_node())
         {
             n->set_visited(false);
             const ObjectList<Node*>& parents = n->get_parents();
             for(ObjectList<Node*>::const_iterator it = parents.begin(); it != parents.end(); ++it)
-                clear_visits_backwards_rec(*it);
+                clear_visits_backwards_rec(*it, n);
         }
         else
         {
-            clear_visits_backwards_rec(n);
+            clear_visits_backwards_rec(n, sc);
         }
     }
 

src/tl/analysis/pcfg/tl-extensible-graph.hpp
@@ -298,10 +298,6 @@ namespace Analysis {
         //! during the construction of the graph but do not represent any statement of the code, and also
         //! concatenates the nodes that will be executed sequentially for sure (Basic Blocks)
         void dress_up_graph();
-        
-        //! Return nodes may cause unconnected parts of the code to be connected
-        //! This method removes this dead connections
-        void remove_unnecessary_connections();
 
         //! This method concatenates a list of nodes into only one
         /*!
@@ -346,7 +342,7 @@ namespace Analysis {
         static void clear_visits_aux_in_level(Node* node, Node* outer_node);
 
         //! Set to false the attribute #_visited of those nodes whose post-dominator is node @node
-        static void clear_visits_backwards(Node* node);
+        static void clear_visits_backwards(Node* node, Node* graph);
 
 
         // *** DOT Graph *** //

src/tl/analysis/pcfg/tl-pcfg-visitor.cpp
@@ -57,8 +57,9 @@ namespace Analysis {
         _pcfg = graph;
     }
 
-    ExtensibleGraph* PCFGVisitor::parallel_control_flow_graph(const NBase& n,
-                                                              const std::map<Symbol, NBase>& asserted_funcs)
+    ExtensibleGraph* PCFGVisitor::parallel_control_flow_graph(
+            const NBase& n,
+            const std::map<Symbol, NBase>& asserted_funcs)
     {
         _asserted_funcs = asserted_funcs;
 
@@ -74,17 +75,44 @@ namespace Analysis {
         // some of they may have no parent (a return has been found inside the context they belong to)
         _pcfg->connect_nodes(_utils->_last_nodes, pcfg_exit);   
         // Get the exit node of the most outer context (generated by the code function) if exists
-        // This may not exist if the PCFG is generated for something different from a
+        // This may not exist if the PCFG is generated for something different from a function code
         Node* returns_exit = pcfg_exit;
-        ObjectList<Node*> pcfg_exit_parents = pcfg_exit->get_parents( );
+        const ObjectList<Node*>& pcfg_exit_parents = pcfg_exit->get_parents( );
         if( ( pcfg_exit_parents.size( ) == 1 ) && ( pcfg_exit_parents[0]->is_context_node( ) ) )
             returns_exit = pcfg_exit_parents[0]->get_graph_exit_node( );
-        _pcfg->connect_nodes( _utils->_return_nodes, returns_exit );
-        _utils->_return_nodes.clear( );
+        for (ObjectList<Node*>::iterator it = _utils->_return_nodes.begin();
+             it != _utils->_return_nodes.end(); ++it)
+        {
+            Node* source = *it;
+            Node* outer = source->get_outer_node();
+            Node* exit = outer->get_graph_exit_node();
+            ObjectList<Node*> parents = exit->get_parents();
+            if (parents.empty())
+            {   // The exit node E1 of this graph node G1 is not connected:
+                // 1.- Connect the return Ri to E1
+                // 2.- Traverse G outer nodes while Exit(G) has no parents and
+                //     Connect G[i] to Exit(G[i+1]) and Disconnect Outer(Exit(G[i+1])) from all its children
+                //     When we find an Exit(G) that already has some parent (another path leads to it)
+                //     connect G[i] to the exit node of the whole graph
+                while (parents.empty())
+                {
+                    _pcfg->connect_nodes(source, exit);
+                    _pcfg->disconnect_nodes(outer, outer->get_children());
+                    source = outer;
+                    outer = outer->get_outer_node();
+                    if (outer == NULL)
+                        goto next_it;
+                    exit = outer->get_graph_exit_node();
+                    parents = exit->get_parents();
+                }
+            }
+            _pcfg->connect_nodes(source, returns_exit);
+next_it:    ;
+        }
+        _utils->_return_nodes.clear();
 
         //_pcfg->dress_up_graph( );
-        _pcfg->remove_unnecessary_connections();
-        
+
         return _pcfg;
     }
 

src/tl/analysis/pcfg/tl-task-sync.cpp
@@ -1227,7 +1227,7 @@ task_synchronized:      break;
         //             find_last_synchronization_point_in_children( task, task_outer );
         //         }
         
-        ExtensibleGraph::clear_visits_backwards( task );
+        ExtensibleGraph::clear_visits_backwards(task, _graph->get_graph());
     }
 
     void TaskConcurrency::find_last_synchronization_point_in_parents( Node* current )

tests/07_phases_analysis.dg/defs_01.c
@@ -36,14 +36,19 @@
 int x = 0;
 
 #pragma analysis_check assert_decl live_in(x) defined(x) upper_exposed(x)
-// int main(int argc, char *argv[])
-void foo()
+int main(int argc, char *argv[])
 {
-    int a;
-    #pragma analysis_check assert defined(a)
-    a = 0;
-    
+    int a, b;
+    #pragma analysis_check assert defined(a, b)
+    a = b = 0;
+
     x++;
-    
-//     return 0;
+
+    #pragma analysis_check assert_decl live_out(x)
+    if(x)
+        return 0;
+    #pragma analysis_check assert live_in(x) dead(b)
+    b = x + 20;
+
+    return 0;
 }