86d898bb5afab29e78205268b4ffa9c314106907
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2014-11-21 22:14:03 +0100

    Inline AST and nodecl most used routines

---
+++ src/frontend/cxx-ast-inline.h
@@ -0,0 +1,548 @@
+#ifndef CXX_AST_INLINE_H
+#define CXX_AST_INLINE_H
+
+#include "mem.h"
+#include "cxx-process.h"
+#include <stdint.h>
+
+MCXX_BEGIN_DECLS
+
+// Definition of the type
+struct AST_tag
+{
+    // Node type (1024 different node types)
+    node_t node_type:10;
+
+    // This is a bitmap for the sons
+    unsigned int bitmap_sons:MCXX_MAX_AST_CHILDREN;
+
+    // Number of ambiguities of this node
+    int num_ambig;
+
+    // Parent node
+    struct AST_tag* parent;
+
+    // Node locus
+    const locus_t* locus;
+
+    // Textual information linked to the node
+    // normally the symbol or the literal
+    const char* text;
+
+    union
+    {
+        // The children of this tree (except for AST_AMBIGUITY)
+        struct AST_tag** children;
+        // When type == AST_AMBIGUITY, all intepretations are here
+        struct AST_tag** ambig;
+    };
+
+    // This is used by nodecl trees
+    struct nodecl_expr_info_tag* expr_info;
+};
+
+
+static inline node_t ast_get_type(const_AST a)
+{
+    return a->node_type;
+}
+
+static inline AST ast_get_parent(const_AST a)
+{
+    return a->parent;
+}
+
+static inline unsigned int ast_get_line(const_AST a)
+{
+    return locus_get_line(a->locus);
+}
+
+static inline const char* ast_get_text(const_AST a)
+{
+    return a->text;
+}
+
+static inline void ast_set_text(AST a, const char* str)
+{
+    a->text = str;
+}
+
+static inline void ast_set_type(AST a, node_t node_type)
+{
+    a->node_type = node_type;
+}
+
+static inline int ast_bitmap_to_index(unsigned int bitmap, int num)
+{
+    ERROR_CONDITION(((1 << num) & bitmap) == 0,
+            "Invalid bitmap!", 0);
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+    bitmap = bitmap & (~(~0U << num));
+    return __builtin_popcount(bitmap);
+#else
+    int i;
+    int s = 0;
+    for (i = 0; i < num; i++)
+    {
+        s += bitmap & 1;
+        bitmap = bitmap >> 1;
+    }
+
+    return s;
+#endif
+}
+
+static inline int ast_son_num_to_son_index(const_AST a, int num_son)
+{
+    return ast_bitmap_to_index(a->bitmap_sons, num_son);
+}
+
+static inline char ast_has_son(const_AST a, int son)
+{
+    return (((1 << son) & a->bitmap_sons) != 0);
+}
+
+static inline AST ast_get_child(const_AST a, int num_child)
+{
+    if (ast_has_son(a, num_child))
+    {
+        return a->children[ast_son_num_to_son_index(a, num_child)];
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+static inline void ast_set_parent(AST a, AST parent)
+{
+    a->parent = parent;
+}
+
+static inline int ast_count_bitmap(unsigned int bitmap)
+{
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+    return __builtin_popcount(bitmap);
+#else
+    int i;
+    int s = 0;
+    for (i = 0; i < MCXX_MAX_AST_CHILDREN; i++)
+    {
+        s += bitmap & 1;
+        bitmap = bitmap >> 1;
+    }
+
+    return s;
+#endif
+}
+
+static inline AST ast_make(node_t type, int __num_children UNUSED_PARAMETER, 
+        AST child0, AST child1, AST child2, AST child3, 
+        const locus_t* location, const char *text)
+{
+    AST result = (AST)xmalloc(sizeof(*result));
+    // ERROR_CONDITION(result & 0x1 != 0, "Invalid pointer for AST", 0);
+
+    result->node_type = type;
+
+    int num_children = 0;
+    unsigned int bitmap_sons = 0;
+
+    result->bitmap_sons = bitmap_sons;
+    result->parent = NULL;
+    result->locus = location;
+
+    result->text = text;
+
+    bitmap_sons =
+        (!!child0)
+        | (!!child1 << 1)
+        | (!!child2 << 2)
+        | (!!child3 << 3);
+    num_children = ast_count_bitmap(bitmap_sons);
+
+    result->bitmap_sons = bitmap_sons;
+    result->children = (AST*)xmalloc(
+            sizeof(*result->children) *
+            num_children);
+
+    int idx = 0;
+#define ADD_SON(n) \
+    if (child##n != NULL) \
+    { \
+        result->children[idx] = child##n; \
+        child##n->parent = result; \
+        idx++; \
+    }
+
+    ADD_SON(0);
+    ADD_SON(1);
+    ADD_SON(2);
+    ADD_SON(3);
+#undef ADD_SON
+
+    result->expr_info = NULL;
+
+    return result;
+}
+
+// This function works both for shrinking or widening
+static inline void ast_reallocate_children(AST a, int num_child, AST new_child)
+{
+    // Reallocate childrens
+    // Save the old children array
+    AST* old_children = a->children;
+    // And the old children bitmap
+    unsigned int old_bitmap = a->bitmap_sons;
+
+    // Enable or disable this new son depending on it being null
+    if (new_child != NULL)
+    {
+        a->bitmap_sons = (a->bitmap_sons | (1 << num_child));
+    }
+    else
+    {
+        a->bitmap_sons = (a->bitmap_sons & (~(1 << num_child)));
+    }
+
+    a->children = (AST*)xmalloc(sizeof(*a->children) *
+            (ast_count_bitmap(a->bitmap_sons)));
+
+    // Now for every old son, update the new children
+    int i;
+    for (i = 0; i < MCXX_MAX_AST_CHILDREN; i++)
+    {
+        // Note that when shrinking the node ast_has_son
+        // will always return false for num_child.
+        if (ast_has_son(a, i))
+        {
+            if (i == num_child)
+            {
+                // The new son
+                a->children[ast_son_num_to_son_index(a, i)] = new_child;
+            }
+            else
+            {
+                // Old sons that are updated their position
+                a->children[ast_son_num_to_son_index(a, i)] = 
+                    old_children[ast_bitmap_to_index(old_bitmap, i)];
+            }
+        }
+    }
+
+    // Now xfree the old children (if any)
+    if (old_children != NULL)
+        xfree(old_children);
+}
+
+static inline void ast_set_child_but_parent(AST a, int num_child, AST new_child)
+{
+    if (new_child == NULL)
+    {
+        if (ast_has_son(a, num_child))
+        {
+            // Shrink the node if we have it
+            ast_reallocate_children(a, num_child, new_child);
+        }
+    }
+    else
+    {
+        if (ast_has_son(a, num_child))
+        {
+            a->children[ast_son_num_to_son_index(a, num_child)] = new_child;
+        }
+        else
+        {
+            // This will widen the node
+            ast_reallocate_children(a, num_child, new_child);
+        }
+    }
+}
+
+static inline void ast_set_child(AST a, int num_child, AST new_child)
+{
+    ast_set_child_but_parent(a, num_child, new_child);
+    if (new_child != NULL)
+    {
+        new_child->parent = a;
+    }
+}
+
+static inline int ast_num_children(const_AST a)
+{
+    return ast_count_bitmap(a->bitmap_sons);
+}
+
+static inline AST ast_list_leaf(AST a)
+{
+    ERROR_CONDITION(a == NULL, "Invalid tree", 0);
+    AST result = ast_make(AST_NODE_LIST, 2, NULL, a, NULL, NULL, 
+            ast_get_locus(a), ast_get_text(a));
+
+    return result;
+}
+
+static inline AST ast_list(AST list, AST last_elem)
+{
+    ERROR_CONDITION(last_elem == NULL, "Invalid tree", 0);
+    const char* filename = NULL;
+    if (list != NULL)
+    {
+        filename = ast_get_filename(list);
+    }
+    else
+    {
+        filename = ast_get_filename(last_elem);
+    }
+
+    AST a = ast_make(AST_NODE_LIST, 2, list, last_elem, NULL, NULL, 
+            make_locus(filename, ast_get_line(last_elem), 0), ast_get_text(last_elem));
+
+    return a;
+}
+
+static inline AST ast_list_head(AST list)
+{
+    if (list == NULL)
+        return NULL;
+
+    if (ASTType(list) != AST_NODE_LIST)
+        return NULL;
+
+    AST iter;
+    for_each_element(list, iter)
+    {
+        return iter;
+    }
+
+    return NULL;
+}
+
+static inline AST ast_list_concat(AST before, AST after)
+{
+    if (before == NULL)
+        return after;
+    if (after == NULL)
+        return before;
+
+    if (ASTType(before) != AST_NODE_LIST
+            || ASTType(after) != AST_NODE_LIST)
+        return NULL;
+
+    AST head_after = ast_list_head(after);
+
+    ast_set_child(head_after, 0, before);
+    return after;
+}
+
+
+static inline const char* ast_location(const_AST a)
+{
+    return locus_to_str(a->locus);
+}
+
+static inline int ast_get_num_ambiguities(const_AST a)
+{
+    return a->num_ambig;
+}
+
+static inline AST ast_get_ambiguity(const_AST a, int num)
+{
+    return a->ambig[num];
+}
+
+static inline void ast_fix_parents_inside_intepretation(AST node)
+{
+    if (node == NULL)
+        return;
+
+    if (ast_get_type(node) == AST_AMBIGUITY)
+        return;
+
+    int i;
+    for (i = 0; i < MCXX_MAX_AST_CHILDREN; i++)
+    {
+        if (ast_get_child(node, i) != NULL)
+        {
+            ast_set_parent(ast_get_child(node, i), node);
+
+            ast_fix_parents_inside_intepretation(ast_get_child(node, i));
+        }
+    }
+}
+
+int ast_num_of_given_child(const_AST parent, const_AST child)
+{
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        if (ast_get_child(parent, i) == child)
+            return i;
+    }
+
+    return -1;
+}
+
+// Be careful when handling ambiguity nodes, the set of interpretations may not
+// be an actual tree but a DAG
+static inline AST ast_make_ambiguous(AST son0, AST son1)
+{
+    if (ASTType(son0) == AST_AMBIGUITY)
+    {
+        if (ASTType(son1) == AST_AMBIGUITY)
+        {
+            int original_son0 = son0->num_ambig;
+
+            son0->num_ambig += son1->num_ambig;
+            son0->ambig = (AST*) xrealloc(son0->ambig, sizeof(*(son0->ambig)) * son0->num_ambig);
+
+            int i;
+            for (i = 0; i < son1->num_ambig; i++)
+            {
+                son0->ambig[original_son0 + i] = son1->ambig[i];
+            }
+
+            return son0;
+        }
+        else
+        {
+            son0->num_ambig++;
+            son0->ambig = (AST*) xrealloc(son0->ambig, sizeof(*(son0->ambig)) * son0->num_ambig);
+            son0->ambig[son0->num_ambig-1] = son1;
+
+            return son0;
+        }
+    }
+    else if (ASTType(son1) == AST_AMBIGUITY)
+    {
+        son1->num_ambig++;
+        son1->ambig = (AST*) xrealloc(son1->ambig, sizeof(*(son1->ambig)) * son1->num_ambig);
+        son1->ambig[son1->num_ambig-1] = son0;
+
+        return son1;
+    }
+    else
+    {
+        AST result = ASTLeaf(AST_AMBIGUITY, make_locus("", 0, 0), NULL);
+
+        result->num_ambig = 2;
+        result->ambig = (AST*) xmalloc(sizeof(*(result->ambig))* result->num_ambig);
+        result->ambig[0] = son0;
+        result->ambig[1] = son1;
+        result->locus = son0->locus;
+
+        return result;
+    }
+}
+
+
+static inline void ast_replace(AST dest, const_AST src)
+{
+    *dest = *src;
+}
+
+static inline void ast_free(AST a)
+{
+    if (a == NULL)
+        return;
+
+    // Already visited. See below
+    if (__builtin_expect(((((intptr_t)a->parent) & 0x1) == 0x1), 0))
+        return;
+
+    // Tag this node as visited to avoid infinite recursion under the presence
+    // of cycles (note that this works as long as AST pointers are at least
+    // aligned to two bytes)
+    a->parent = (struct AST_tag*)(((intptr_t)a->parent) | 0x1);
+
+    if (ast_get_type(a) == AST_AMBIGUITY)
+    {
+        int i;
+        for (i = 0; i < ast_get_num_ambiguities(a); i++)
+        {
+            ast_free(ast_get_ambiguity(a, i));
+        }
+    }
+    else
+    {
+        int i;
+        for (i = 0; i < MCXX_MAX_AST_CHILDREN; i++)
+        {
+            ast_free(ast_get_child(a, i));
+        }
+    }
+
+    xfree(a->expr_info);
+    xfree(a->children);
+    // Clear the node for safety
+    // __builtin_memset(a, 0, sizeof(*a));
+    xfree(a);
+}
+
+static inline void ast_replace_with_ambiguity(AST a, int n)
+{
+    ERROR_CONDITION(n >= ast_get_num_ambiguities(a),
+        "There is no such option (%d) in this ambiguous node (options = %d)", n, ast_get_num_ambiguities(a));
+    ERROR_CONDITION(n < 0, "Invalid node number (%d)", n);
+
+    AST parent = ASTParent(a);
+
+    // DEBUG_CODE()
+    // {
+    //     fprintf(stderr, "*** Choosing '%s' in the ambiguity tree %p (%s) using %p\n", 
+    //             ast_print_node_type(ASTType(ast_get_ambiguity(a, n))), a, ast_location(ast_get_ambiguity(a, n)), 
+    //             ast_get_ambiguity(a, n));
+    // }
+
+    AST chosen_ambig = ast_get_ambiguity(a, n);
+    ast_replace(a, chosen_ambig);
+
+    // Correctly relink to the parent
+    ast_set_parent(a, parent);
+    ast_fix_parents_inside_intepretation(a);
+
+    // if (!ASTCheck(a))
+    // {
+    //     internal_error("*** INCONSISTENT TREE DETECTED IN DISAMBIGUATED TREE %p ***\n", a);
+    // }
+}
+
+static inline const locus_t* ast_get_locus(const_AST a)
+{
+    return a->locus;
+}
+
+static inline void ast_set_locus(AST a, const locus_t* locus)
+{
+    a->locus = locus;
+}
+
+static inline const char *ast_get_filename(const_AST a)
+{
+    return locus_get_filename(a->locus);
+}
+
+static inline int ast_node_size(void)
+{
+    return sizeof(struct AST_tag);
+}
+
+static inline struct nodecl_expr_info_tag* ast_get_expr_info(const_AST a)
+{
+    return a->expr_info;
+}
+
+static inline void ast_set_expr_info(AST a, struct nodecl_expr_info_tag* expr_info)
+{
+    a->expr_info = expr_info;
+}
+
+static inline const char* ast_node_type_name(node_t n)
+{
+    return ast_node_names[n];
+}
+
+
+MCXX_END_DECLS
+
+#endif
--- src/frontend/cxx-nodecl.h
+++ src/frontend/cxx-nodecl.h
@@ -41,110 +41,110 @@ MCXX_BEGIN_DECLS
 
 #define NODECL_STATIC_NULL { NULL }
 
-nodecl_t nodecl_null(void);
-char nodecl_is_null(nodecl_t t);
+static inline nodecl_t nodecl_null(void);
+static inline char nodecl_is_null(nodecl_t t);
 
 // Returns the internal AST
-AST nodecl_get_ast(nodecl_t t);
+static inline AST nodecl_get_ast(nodecl_t t);
 
 // Replicates the tree but does not change symbols or other indirectly mentioned symbols
 nodecl_t nodecl_shallow_copy(nodecl_t t);
 
 // Duplicates the node like nodecl_shallow_copy but does not copy children. Creates a leaf node
-nodecl_t nodecl_duplicate(nodecl_t t);
+static inline nodecl_t nodecl_duplicate(nodecl_t t);
 
 // Replicates the tree and duplicates bound variables. Free variables may be replaced using the given map
 nodecl_t nodecl_deep_copy(nodecl_t, decl_context_t new_decl_context, symbol_map_t* symbol_map);
 
 // Parent
-nodecl_t nodecl_get_parent(nodecl_t t);
-void nodecl_set_parent(nodecl_t node, nodecl_t parent);
+static inline nodecl_t nodecl_get_parent(nodecl_t t);
+static inline void nodecl_set_parent(nodecl_t node, nodecl_t parent);
 
 // Children
-nodecl_t nodecl_get_child(nodecl_t n, int i);
+static inline nodecl_t nodecl_get_child(nodecl_t n, int i);
 
 // Constant values
-char nodecl_is_constant(nodecl_t t);
-void nodecl_set_constant(nodecl_t t, const_value_t* cval);
-const_value_t* nodecl_get_constant(nodecl_t t);
+static inline char nodecl_is_constant(nodecl_t t);
+static inline void nodecl_set_constant(nodecl_t t, const_value_t* cval);
+static inline const_value_t* nodecl_get_constant(nodecl_t t);
 
 // Text
-void nodecl_set_text(nodecl_t t, const char*);
-const char* nodecl_get_text(nodecl_t);
+static inline void nodecl_set_text(nodecl_t t, const char*);
+static inline const char* nodecl_get_text(nodecl_t);
 
 // Type
-type_t* nodecl_get_type(nodecl_t);
-void nodecl_set_type(nodecl_t, type_t*);
+static inline type_t* nodecl_get_type(nodecl_t);
+static inline void nodecl_set_type(nodecl_t, type_t*);
 
 // Symbol
-scope_entry_t* nodecl_get_symbol(nodecl_t);
-void nodecl_set_symbol(nodecl_t, scope_entry_t*);
+static inline scope_entry_t* nodecl_get_symbol(nodecl_t);
+static inline void nodecl_set_symbol(nodecl_t, scope_entry_t*);
 
 // Template parameters (C++ only)
-void nodecl_set_template_parameters(nodecl_t n, template_parameter_list_t* template_parameters);
-template_parameter_list_t* nodecl_get_template_parameters(nodecl_t n);
+static inline void nodecl_set_template_parameters(nodecl_t n, template_parameter_list_t* template_parameters);
+static inline template_parameter_list_t* nodecl_get_template_parameters(nodecl_t n);
 
 // Location
-const locus_t* nodecl_get_locus(nodecl_t);
+static inline const locus_t* nodecl_get_locus(nodecl_t);
 
-const char* nodecl_get_filename(nodecl_t);
-unsigned int nodecl_get_line(nodecl_t);
-const char* nodecl_locus_to_str(nodecl_t);
+static inline const char* nodecl_get_filename(nodecl_t);
+static inline unsigned int nodecl_get_line(nodecl_t);
+static inline const char* nodecl_locus_to_str(nodecl_t);
 
-void nodecl_set_locus(nodecl_t n, const locus_t* locus);
+static inline void nodecl_set_locus(nodecl_t n, const locus_t* locus);
 
-void nodecl_set_locus_as(nodecl_t n, nodecl_t loc);
+static inline void nodecl_set_locus_as(nodecl_t n, nodecl_t loc);
 
 // Kind of node
-node_t nodecl_get_kind(nodecl_t);
+static inline node_t nodecl_get_kind(nodecl_t);
 
 // 'list' parameter can be a 'nodecl_null()'
-nodecl_t nodecl_append_to_list(nodecl_t list, nodecl_t element);
+static inline nodecl_t nodecl_append_to_list(nodecl_t list, nodecl_t element);
 
 // Returns the head of the list
-nodecl_t nodecl_list_head(nodecl_t list);
+static inline nodecl_t nodecl_list_head(nodecl_t list);
 
 // Either list1 or list2 can be 'nodecl_null()'
-nodecl_t nodecl_concat_lists(nodecl_t list1, nodecl_t list2);
+static inline nodecl_t nodecl_concat_lists(nodecl_t list1, nodecl_t list2);
 
 // States that this nonnull nodecl is a list
-char nodecl_is_list(nodecl_t);
+static inline char nodecl_is_list(nodecl_t);
 
 // States that this nodecl is a list
-char nodecl_is_list_or_null(nodecl_t);
+static inline char nodecl_is_list_or_null(nodecl_t);
 
 // Unpack a list. Do not forget to free the returned pointer
-nodecl_t* nodecl_unpack_list(nodecl_t n, int *num_items);
+static inline nodecl_t* nodecl_unpack_list(nodecl_t n, int *num_items);
 
 // Length of a list
-int nodecl_list_length(nodecl_t list);
+static inline int nodecl_list_length(nodecl_t list);
 
 // Wrap (use sparingly)
-nodecl_t _nodecl_wrap(AST);
+static inline nodecl_t _nodecl_wrap(AST);
 
 // Generic routines (meant for generic processing)
-nodecl_t nodecl_generic_make(node_t, const locus_t* location);
-void nodecl_set_child(nodecl_t, int, nodecl_t);
+static inline nodecl_t nodecl_generic_make(node_t, const locus_t* location);
+static inline void nodecl_set_child(nodecl_t, int, nodecl_t);
 
 // Free a temporally allocated nodecl
-void nodecl_free(nodecl_t);
+static inline void nodecl_free(nodecl_t);
 
-char nodecl_expr_is_value_dependent(nodecl_t);
-void nodecl_expr_set_is_value_dependent(nodecl_t, char);
+static inline char nodecl_expr_is_value_dependent(nodecl_t);
+static inline void nodecl_expr_set_is_value_dependent(nodecl_t, char);
 
-char nodecl_expr_is_type_dependent(nodecl_t);
-void nodecl_expr_set_is_type_dependent(nodecl_t, char);
+static inline char nodecl_expr_is_type_dependent(nodecl_t);
+static inline void nodecl_expr_set_is_type_dependent(nodecl_t, char);
 
-char nodecl_is_err_expr(nodecl_t);
-char nodecl_is_err_stmt(nodecl_t);
+static inline char nodecl_is_err_expr(nodecl_t);
+static inline char nodecl_is_err_stmt(nodecl_t);
 
 // Retrieve the context from a node (or the currently compiled file global
 // context if not context is found)
-decl_context_t nodecl_retrieve_context(nodecl_t);
+static inline decl_context_t nodecl_retrieve_context(nodecl_t);
 
 // These only work on NODECL_CONTEXT or NODECL_PRAGMA_CONTEXT
-decl_context_t nodecl_get_decl_context(nodecl_t n);
-void nodecl_set_decl_context(nodecl_t, decl_context_t);
+static inline decl_context_t nodecl_get_decl_context(nodecl_t n);
+static inline void nodecl_set_decl_context(nodecl_t, decl_context_t);
 
 // Exchange
 DEPRECATED void nodecl_exchange(nodecl_t old_nod, nodecl_t new_node);
@@ -167,5 +167,7 @@ AST* nodecl_get_placeholder(nodecl_t n);
 void nodecl_set_placeholder(nodecl_t n, AST* p);
 
 MCXX_END_DECLS
+
+#include "cxx-nodecl-inline.h"
 
 #endif // CXX_NODECL_H
