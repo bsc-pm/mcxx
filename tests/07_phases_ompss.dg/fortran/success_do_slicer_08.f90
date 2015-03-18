08ea0c55a765953df7731013f87f80a5895831a2
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2015-03-18 17:52:21 +0100

    Make sure we are not accidentally repeating labels
    This commit fixes #2347

---
+++ tests/07_phases_ompss.dg/fortran/success_do_slicer_08.f90
@@ -0,0 +1,27 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+      SUBROUTINE S
+      IMPLICIT NONE
+
+      INTEGER :: I, J, W
+
+!$OMP PARALLEL DO
+      DO 40 I = 1, 100
+      IF (W > 20) THEN
+      DO 41 J = 1, 200
+41    CONTINUE
+      ELSE IF (W <= 20) THEN
+       DO 42 J = 1, 200
+42    CONTINUE
+      ELSE
+       DO 43 J = 1, 200
+43    CONTINUE
+      END IF
+40    CONTINUE
+
+      END SUBROUTINE S
+
+      PROGRAM MAIN
+          CALL S
+      END PROGRAM MAIN
