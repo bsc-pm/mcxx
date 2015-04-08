ecc41278777b2123415230b5ec48893658aab031
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2015-04-08 13:47:35 +0200

    Make sure we preserve the cv-qualifier when updating types after implicit
    const qualification is used by the FE frontend to represent the PARAMETER
    attribute of names.
    
    This commit fixes #2373

---
+++ tests/01_fortran.dg/success_implicit_variables_005.f90
@@ -0,0 +1,10 @@
+! <testinfo>
+! test_generator=config/mercurium-fortran
+! </testinfo>
+PROGRAM MAIN
+      PARAMETER (namx=10)
+      IMPLICIT REAL*8 (a-h,o-z)
+      DIMENSION x1a(namx)
+
+      PRINT *, X1A
+END PROGRAM MAIN
