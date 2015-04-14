fa4590ddac9b69a855a8ce1532e7793b3f4294c0
Sara Royuela <sara.royuela@bsc.es>
2015-04-14 15:53:41 +0200

    Add minimal support for alias analysis
    See #2381

---
+++ src/tl/analysis/aliasing/tl-alias-analysis.hpp
@@ -0,0 +1,59 @@
+/*--------------------------------------------------------------------
+  (C) Copyright 2006-2014 Barcelona Supercomputing Center
+                          Centro Nacional de Supercomputacion
+
+  This file is part of Mercurium C/C++ source-to-source compiler.
+
+  See AUTHORS file in the top level directory for information
+  regarding developers and contributors.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option ) any later version.
+
+  Mercurium C/C++ source-to-source compiler is distributed in the hope
+  that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the GNU Lesser General Public License for more
+  details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Mercurium C/C++ source-to-source compiler; if
+  not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+  Cambridge, MA 02139, USA.
+--------------------------------------------------------------------*/
+
+#ifndef TL_ALIAS_ANALYSIS_HPP
+#define TL_ALIAS_ANALYSIS_HPP
+
+#include "tl-nodecl-visitor.hpp"
+#include "tl-tribool.hpp"
+
+namespace TL {
+namespace Analysis {
+
+    tribool accesses_may_be_alias(const Nodecl::NodeclBase& n, const Nodecl::NodeclBase& m);
+
+    class LIBTL_CLASS AliasAnalysis : public Nodecl::ExhaustiveVisitor<void>
+    {
+    private:
+        Symbol _base_symbol;
+        tribool _may_cause_aliasing;
+
+    public:
+        AliasAnalysis();
+
+        Ret visit(const Nodecl::Symbol& n);
+        Ret visit(const Nodecl::ArraySubscript& n);
+        Ret visit(const Nodecl::ClassMemberAccess& n);
+        Ret visit(const Nodecl::Dereference& n);
+        
+        Symbol get_base_symbol() const;
+        tribool may_cause_aliasing() const;
+    };
+
+}
+}
+
+#endif      // TL_ALIAS_ANALYSIS_HPP
