39571e8f6ad6ae77b29fe16e6671a5dfb828444b
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2015-06-03 17:29:09 +0200

    Add testing support for C11

---
+++ tests/02_typecalc_c11.dg/success_atomic_01.c
@@ -0,0 +1,23 @@
+/*
+<testinfo>
+test_generator="config/mercurium-c11"
+</testinfo>
+*/
+
+_Atomic int x0 = 0;
+_Atomic signed int x1 = 0;
+_Atomic unsigned int x2 = 0;
+
+_Atomic long x3 = 0;
+_Atomic signed long x4 = 0;
+_Atomic unsigned long x5 = 0;
+
+_Atomic short x6 = 0;
+_Atomic signed short x7 = 0;
+_Atomic unsigned short x8 = 0;
+
+_Atomic _Bool x9 = 0;
+
+_Atomic char x10 = 0;
+_Atomic signed char x11 = 0;
+_Atomic unsigned char x12 = 0;
