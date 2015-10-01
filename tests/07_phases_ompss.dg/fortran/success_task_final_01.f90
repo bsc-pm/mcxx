cc8183e0da6d767e5e90e64c8fc6f481ee25d469
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2015-10-01 16:42:01 +0200

    Minor improvement in the generation of the final statements
    With this new commit, the 'omp_in_final' function is transformed to a '1'
    when we're generating the final statements of a task.
    
    I'm also adding a few interesting tests where this new feature is tested.
    
    See #2230

---
+++ tests/07_phases_ompss.dg/fortran/success_task_final_01.f90
@@ -0,0 +1,41 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+FUNCTION OMP_IN_FINAL() RESULT(X)
+    IMPLICIT NONE
+    LOGICAL X
+    STOP -1
+    X = .FALSE.
+END FUNCTION OMP_IN_FINAL
+
+RECURSIVE FUNCTION F(N) RESULT(RES)
+    IMPLICIT NONE
+    INTEGER :: N, RES
+    LOGICAL :: X
+    LOGICAL, EXTERNAL  :: OMP_IN_FINAL
+    RES = 0
+    IF (N > 0) THEN
+    !$OMP TASK SHARED(RES)
+        RES = F(N-1)
+        IF(OMP_IN_FINAL()) THEN
+            RES = RES + 1
+        ELSE
+            RES = RES + 2
+        ENDIF
+    !$OMP END TASK
+    ENDIF
+END FUNCTION F
+
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER :: RES
+    INTEGER, EXTERNAL :: F
+    RES = 0
+
+    !$OMP TASK SHARED(RES) FINAL(.TRUE.)
+        RES = F(10)
+    !$OMP END TASK
+    !$OMP TASKWAIT
+
+    IF (RES /= 10) STOP -2
+END PROGRAM P
