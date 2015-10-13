50904a6c2f8c7394c00f33900dc8594dd978fa94
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2015-10-13 12:45:12 +0200

    Protect test for >=GCC-4.8

--- tests/04_compat_gcc.dg/success_vector_05.c
+++ tests/04_compat_gcc.dg/success_vector_05.c
@@ -6,9 +6,11 @@ test_generator=config/mercurium
 
 typedef __attribute__((vector_size(16))) unsigned int v4sui;
 
-v4sui f(v4sui ua)
+v4sui f(v4sui ua, v4sui ub)
 {
-    v4sui ub = ua == ua;
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8)
+    ub = ua == ua;
+#endif
 
     return ub;
 }
