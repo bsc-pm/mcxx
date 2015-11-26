6a0f7c887db1f732b3372d9faab2e06ded55301c
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2015-11-26 15:31:40 +0100

    Missing initializations diagnosed by valgrind

--- src/tl/omp/simd/tl-omp-simd.cpp
+++ src/tl/omp/simd/tl-omp-simd.cpp
@@ -42,9 +42,17 @@ namespace TL {
 
         Simd::Simd()
             : PragmaCustomCompilerPhase(),
-            _simd_enabled(false), _svml_enabled(false), _fast_math_enabled(false),
-            _avx2_enabled(false), _knc_enabled(false), _knl_enabled(false),
-            _spml_enabled(false), _only_adjacent_accesses_enabled(false), _overlap_in_place(false)
+            _simd_enabled(false),
+            _svml_enabled(false),
+            _fast_math_enabled(false),
+            _avx2_enabled(false),
+            _neon_enabled(false),
+            _romol_enabled(false),
+            _knc_enabled(false),
+            _knl_enabled(false),
+            _spml_enabled(false),
+            _only_adjacent_accesses_enabled(false),
+            _overlap_in_place(false)
         {
             set_phase_name("Vectorize OpenMP SIMD parallel IR");
             set_phase_description("This phase vectorize the OpenMP SIMD parallel IR");
