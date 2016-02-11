e6e8e56eb0454da105a6949b9e9dec2c4328e65b
Diego Caballero <diego.caballero@bsc.es>
2016-02-11 18:15:20 +0100

    Missing underscore in AVX2 store intrinsic

--- src/tl/vectorization/vector-lowering/avx2/backend/tl-vector-backend-avx2.cpp
+++ src/tl/vectorization/vector-lowering/avx2/backend/tl-vector-backend-avx2.cpp
@@ -1776,6 +1776,7 @@ namespace Vectorization
 
         intrin_name << AVX2_INTRIN_PREFIX
             << mask_prefix
+            << "_"
             << intrin_op_name
             << "_"
             << intrin_type_suffix
