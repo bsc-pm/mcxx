e120bd4bb357d6e9a08ef97c6b8fe8d996f10add
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2016-03-22 11:14:43 +0100

    More fixes in compile versions

--- tests/07_phases_ompss.dg/fortran/success_do_modules.F90
+++ tests/07_phases_ompss.dg/fortran/success_do_modules.F90
@@ -1,13 +1,13 @@
 ! <testinfo>
 ! test_generator="config/mercurium-ompss"
-! compile_versions="nanox_mercurium_mod0 nanox_mercurium_mod1"
-! test_FFLAGS_nanox_mercurium_mod0="-DMOD0"
-! test_FFLAGS_nanox_mercurium_mod1="-DMOD1"
-! test_compile_fail_nanox_mercurium_mod0_nanos6_mercurium=yes
-! test_compile_fail_nanox_mercurium_mod1_nanos6_mercurium=yes
-! test_compile_fail_nanox_mercurium_mod0_nanos6_imfc=yes
-! test_compile_fail_nanox_mercurium_mod1_nanos6_imfc=yes
-! test_compile_fail_nanox_mercurium_mod1_nanox_imfc=yes
+! compile_versions="mod0 mod1"
+! test_FFLAGS_mod0="-DMOD0"
+! test_FFLAGS_mod1="-DMOD1"
+! test_compile_fail_mod0_nanos6_mercurium=yes
+! test_compile_fail_mod1_nanos6_mercurium=yes
+! test_compile_fail_mod0_nanos6_imfc=yes
+! test_compile_fail_mod1_nanos6_imfc=yes
+! test_compile_fail_mod1_nanox_imfc=yes
 ! </testinfo>
 
 #ifdef MOD0
