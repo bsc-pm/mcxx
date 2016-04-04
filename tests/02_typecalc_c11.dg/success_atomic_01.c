adc119893fa35a7548446620b52234b05b8f7956
Daniel Peyrolon <daniel.peyrolon@bsc.es>
2016-04-04 16:23:32 +0200

    Protect test for C11 atomics

--- tests/02_typecalc_c11.dg/success_atomic_01.c
+++ tests/02_typecalc_c11.dg/success_atomic_01.c
@@ -4,6 +4,7 @@ test_generator="config/mercurium-c11"
 </testinfo>
 */
 
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)
 _Atomic int x0 = 0;
 _Atomic signed int x1 = 0;
 _Atomic unsigned int x2 = 0;
@@ -21,3 +22,4 @@ _Atomic _Bool x9 = 0;
 _Atomic char x10 = 0;
 _Atomic signed char x11 = 0;
 _Atomic unsigned char x12 = 0;
+#endif
