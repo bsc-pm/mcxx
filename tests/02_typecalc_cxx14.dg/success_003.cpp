e73cc4b8007207f1c4764379e6e47fd1641d2639
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-08-22 11:25:52 +0200

    More work refactoring the lexer
    In this commit, I have created a new definition for the binary literals

---
+++ tests/02_typecalc_cxx14.dg/success_003.cpp
@@ -0,0 +1,12 @@
+/*
+<testinfo>
+test_generator="config/mercurium-cxx14"
+</testinfo>
+*/
+
+void foo() {
+    int x1 = 0b1001;
+    int x2 = 0B1001;
+    int x3 = 0b11111111'11111111'11111111'11111111;
+    int x4 = 0B11111111'11111111'11111111'11111111;
+}
