a16d90b610f54236766b6329346f71c1433a171e
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-08-30 16:21:18 +0200

    Tests
    See #2640

---
+++ tests/02_typecalc_cxx11.dg/success_222.cpp
@@ -0,0 +1,20 @@
+/*
+<testinfo>
+test_generator="config/mercurium-cxx11"
+</testinfo>
+*/
+template < typename T>
+class List { };
+
+enum M { VAL1, VAL2 };
+
+typedef int K;
+template < int S = 0 >
+List< K > foo();
+
+template <typename T>
+class X
+{
+    template <  M val = VAL1 >
+    List< T > foo2();
+};
