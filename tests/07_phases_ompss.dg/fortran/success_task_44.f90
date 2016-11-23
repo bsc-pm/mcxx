5e353abbf825956fa73aaf41b4e39b69aa026a21
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-11-23 21:16:08 +0100

    Enhancing data sharings support in Nanos6
    This commit adds support to firstprivatize an allocatable
    
    See #2626

---
+++ tests/07_phases_ompss.dg/fortran/success_task_44.f90
@@ -0,0 +1,16 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER, ALLOCATABLE :: X(:)
+
+    ALLOCATE(X(1000))
+
+    X = 0
+    !$OMP TASK FIRSTPRIVATE(X)
+        X = 1
+    !$OMP END TASK
+    !$OMP TASKWAIT
+    IF (ANY(X /= 0)) STOP 1
+END PROGRAM P
