4dea815c18ed5f7b69dc78fca9e6ec52e379b5a1
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-12-13 14:06:57 +0100

    Adding support to the commutative clause in Fortran See #2625

---
+++ tests/07_phases_ompss.dg/fortran/success_task_45.f90
@@ -0,0 +1,24 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss
+! </testinfo>
+PROGRAM P
+    IMPLICIT NONE
+    INTEGER, ALLOCATABLE :: X(:)
+    INTEGER, PARAMETER :: N = 1000
+    INTEGER :: RES, I
+
+
+    ALLOCATE(X(N))
+
+    X = 1
+    rEs = 0
+
+    DO I=1, N
+        !$OMP TASK COMMUTATIVE(RES) IN(X(I))
+            RES = RES + X(I)
+        !$OMP END TASK
+    ENDDO
+    !$OMP TASKWAIT
+
+    IF (RES /= N) STOP 1
+END PROGRAM P
