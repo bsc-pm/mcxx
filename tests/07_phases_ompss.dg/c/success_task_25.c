a490be1547a4f84e48d7f9d0bcbcb146517e6326
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2016-12-27 11:18:59 +0100

    Capturing some extra variables that are not specified in any data-sharing clause...
    ... but they are mentioned in other tasking clauses
    
    It fixes #2665

---
+++ tests/07_phases_ompss.dg/c/success_task_25.c
@@ -0,0 +1,34 @@
+/*
+<testinfo>
+test_generator=config/mercurium-ompss
+</testinfo>
+*/
+#include<assert.h>
+
+int x;
+int *v;
+
+#pragma omp task inout(v[x])
+void f()
+{
+    assert(v[x] == 1);
+    v[x]--;
+}
+
+int main()
+{
+    x = 0;
+    int a = 1;
+    v = &a;
+
+    f();
+
+    #pragma omp task inout(v[x])
+    {
+        assert(v[x] == 0);
+        v[x]++;
+    }
+    #pragma omp taskwait
+    assert(v[x] == 1);
+    return 0;
+}
