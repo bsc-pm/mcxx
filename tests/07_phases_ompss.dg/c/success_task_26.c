59b8cba725edc1510abc81a41a4885a3e476f791
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-03-09 20:48:44 +0100

    The FinalStmtGenerator was not treating the Taskwait with dependences
    It fixes #2698

---
+++ tests/07_phases_ompss.dg/c/success_task_26.c
@@ -0,0 +1,19 @@
+/*
+<testinfo>
+test_generator=config/mercurium-ompss
+test_nolink=yes
+</testinfo>
+*/
+
+// This test was designed to stress our transformation of the final clause.
+// There is no need to execute it
+int main()
+{
+    int x = 0;
+    #pragma omp task inout(x)
+    {
+        x++;
+        #pragma omp taskwait inout(x)
+    }
+    #pragma omp taskwait
+}
