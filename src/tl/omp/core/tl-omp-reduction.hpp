a7c0c952f7845d15f59f4c8bf04b05d6fb1c81dc
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-03-10 09:42:40 +0100

    Adding a comment

--- src/tl/omp/core/tl-omp-reduction.hpp
+++ src/tl/omp/core/tl-omp-reduction.hpp
@@ -70,6 +70,7 @@ namespace TL { namespace OpenMP {
             // AST_INIT_DECLARATOR node. Otherwise it will be false.
             bool _is_initialization;
 
+            // The function symbol that represents the reduction operator
             TL::Symbol _symbol;
 
             // Constructor
