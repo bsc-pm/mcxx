b22c0cc4ce5250c9d9f4ae5e067f3d6b59a7a2dc
Roger Ferrer Ibanez <rofirrim@gmail.com>
2017-05-20 08:53:51 +0100

    Fix for invalidly scanned Hollerith constant

---
+++ tests/01_fortran.dg/success_hollerith_03.f
@@ -0,0 +1,10 @@
+C23456
+! <testinfo>
+! test_generator="config/mercurium-fortran run"
+! </testinfo>
+      PROGRAM MAIN
+      IMPLICIT NONE
+      CHARACTER*8H123
+
+      H123 = '12345678'
+      END PROGRAM MAIN
