bc3fa6d43285b61f6e52e821f2d44ded9941456c
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-07-21 17:51:12 +0200

    Do not execute these tests with gfortran 4.7

--- tests/01_fortran.dg/success_intrinsic_etime_01.F90
+++ tests/01_fortran.dg/success_intrinsic_etime_01.F90
@@ -2,8 +2,8 @@
 ! test_generator="config/mercurium-fortran run"
 ! </testinfo>
           program test_etime
-              ! They broke this in versions 4.5 and 4.6. All others are OK
-#if defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4  && ( __GNUC_MINOR__ < 5 || __GNUC_MINOR__ >= 7)))
+              ! They broke this in versions 4.5, 4.6 and 4.7. All others are OK
+#if defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4  && ( __GNUC_MINOR__ < 5 || __GNUC_MINOR__ > 7)))
               integer(8) :: i, j
               real, dimension(2) :: tarray
               real :: result
--- tests/01_fortran.dg/success_intrinsic_system_01.F90
+++ tests/01_fortran.dg/success_intrinsic_system_01.F90
@@ -12,8 +12,8 @@ END SUBROUTINE MAIN_2
 SUBROUTINE MAIN_2
     IMPLICIT NONE
 
-    ! They broke this in versions 4.5 and 4.6. All others are OK
-#if defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4  && ( __GNUC_MINOR__ < 5 || __GNUC_MINOR__ >= 7)))
+    ! They broke this in versions 4.5, 4.6 and 4.7. All others are OK
+#if defined(__GNUC__) && ((__GNUC__ > 4) || (__GNUC__ == 4  && ( __GNUC_MINOR__ < 5 || __GNUC_MINOR__ > 7)))
     INTEGER :: STAT
 
     CALL SYSTEM("ls")
