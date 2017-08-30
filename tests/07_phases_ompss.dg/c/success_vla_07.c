c25519f9a446bb784309b4a44fc34700bf87a0cd
Ferran Pallarès Roca <ferran.pallares@bsc.es>
2017-08-30 09:31:13 +0200

    Add missing test regarding pointers to VLAs in OmpSs-v2
    - See issue #2752.
    - See commit 1e86bde3a.

---
+++ tests/07_phases_ompss.dg/c/success_vla_07.c
@@ -0,0 +1,34 @@
+/*
+<testinfo>
+test_generator=(config/mercurium-ompss "config/mercurium-ompss-v2 openmp-compatibility")
+test_CFLAGS="-std=gnu99 -Werror"
+</testinfo>
+*/
+
+#include<assert.h>
+#include<stdlib.h>
+
+void v(int n) {
+    int (*v1)[n] = (int (*)[n])malloc(n*sizeof(int));
+    for (int i = 0; i < n; ++i)
+        (*v1)[i] = 0;
+
+    #pragma omp task firstprivate(v1)
+    {
+        for (int i = 0; i < n; ++i)
+            (*v1)[i] = 7;
+    }
+
+    #pragma omp taskwait
+
+    for (int i = 0; i < n; ++i)
+        assert((*v1)[i] == 7);
+
+    free(v1);
+}
+
+int main(int argc, char*argv[])
+{
+    v(100);
+    return 0;
+}
