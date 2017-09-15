02cb91de8b71562f6ce1513fe2b45a6c2e535058
Ferran Pallarès Roca <ferran.pallares@bsc.es>
2017-09-15 16:50:34 +0200

    Make OmpSs-v2 C++ 'atomic' test also fail for intel ICC <17
    - The test is marked as 'compile_fail' because the 'atomic' clause does
    not yet fully work with the final clause transformation on C++. See
    issue #2757.
    
    - The test should be protected not to be executed on ICC <17, as older
    versions seem to have some floating point precision issues. See issue #2758.
    Currently, it is not protected as it's marked 'compile_fail', and
    protecting it would make it work for ICC <17.

--- tests/07_phases_ompss_v2.dg/cxx/success_atomic_01.cpp
+++ tests/07_phases_ompss_v2.dg/cxx/success_atomic_01.cpp
@@ -2,10 +2,21 @@
 <testinfo>
 test_generator="config/mercurium-ompss-v2"
 test_compile_fail_nanos6_mercurium=yes
+test_compile_fail_nanos6_imcxx=yes
 </testinfo>
 */
 
-#if !defined(__ICC) || (__ICC >= 1700)
+// FIXME: This test has two issues:
+//
+//  - It is marked as 'compile_fail' because the 'atomic' clause does not yet
+//    fully work with the final clause transformation on C++. See issue #2757.
+//
+//  - It should be protected not to be executed on ICC <17, as older versions
+//    seem to have some floating point precision issues. See issue #2758.
+//    Currently, it is not protected as it's marked 'compile_fail', and
+//    protecting it would make it work for ICC <17.
+
+//#if !defined(__ICC) || (__ICC >= 1700)
 #include <cassert>
 
 #define epsilon 0.00025
@@ -31,10 +42,10 @@ int main()
 
     return 0;
 }
-#else
-// ICC <17.0 seems to have precision issues with exact real representation
-int main()
-{
-    return 0;
-}
-#endif
+//#else
+//// ICC <17.0 seems to have precision issues with exact real representation
+//int main()
+//{
+//    return 0;
+//}
+//#endif
