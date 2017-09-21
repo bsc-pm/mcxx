8fb6e9718fc60c256ce4f786b258d0735bfdacb6
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-09-21 11:14:46 +0200

    Add some OmpSs-v2 tests using the release construct
    It fixes #2761

---
+++ tests/07_phases_ompss_v2.dg/cxx/success_release_01.cpp
@@ -0,0 +1,21 @@
+/*
+<testinfo>
+test_generator="config/mercurium-ompss-v2"
+</testinfo>
+*/
+#include<assert.h>
+
+int main()
+{
+    int x = 1;
+    #pragma oss task inout(x)
+    {
+        x++;
+        #pragma oss release out(x)
+        assert(x == 2);
+    }
+    #pragma oss task in(x)
+    assert(x == 2);
+
+    #pragma oss taskwait
+}
---
+++ tests/07_phases_ompss_v2.dg/fortran/success_release_01.f90
@@ -0,0 +1,20 @@
+! <testinfo>
+! test_generator=config/mercurium-ompss-v2
+! </testinfo>
+PROGRAM P
+IMPLICIT NONE
+INTEGER :: X
+
+X = 1
+!$OSS TASK INOUT(X)
+    X = X + 1
+    !$OSS RELEASE OUT(X)
+    IF (X /= 2) STOP 1
+!$OSS END TASK
+
+!$OSS TASK IN(X)
+    IF (X /= 2) STOP 1
+!$OSS END TASK
+
+!$OSS TASKWAIT
+END PROGRAM P
