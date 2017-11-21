fa1aed9117d48aa997aa3c154ace0ddfdf35657f
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-11-21 08:55:33 +0100

    OpenMP::Core should assume that tasks are TIED by default
    In OmpSs we have a flag to change this default behavior
    
    It fixes #2778

--- src/tl/omp/core/tl-omp-core.cpp
+++ src/tl/omp/core/tl-omp-core.cpp
@@ -51,7 +51,7 @@ namespace TL { namespace OpenMP {
         : PragmaCustomCompilerPhase(),
         _ompss_mode(false),
         _copy_deps_by_default(true),
-        _untied_tasks_by_default(true),
+        _untied_tasks_by_default(false),
         _discard_unused_data_sharings(false),
         _allow_shared_without_copies(false),
         _allow_array_reductions(true),
@@ -175,7 +175,7 @@ namespace TL { namespace OpenMP {
 
     void Core::set_untied_tasks_by_default_from_str(const std::string& str)
     {
-        parse_boolean_option("untied_tasks", str, _untied_tasks_by_default, "Assuming true.");
+        parse_boolean_option("untied_tasks", str, _untied_tasks_by_default, "Assuming false.");
     }
 
     void Core::set_discard_unused_data_sharings_from_str(const std::string& str)
