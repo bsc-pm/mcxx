a58eb7d6e18f2653470ba07fbf76ad23a732ed1e
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2017-11-23 07:36:53 +0100

    Moving all content from tl-target-information.cpp to its header

--- src/tl/omp/nanox-nodecl/tl-target-information.hpp
+++ src/tl/omp/nanox-nodecl/tl-target-information.hpp
@@ -49,15 +49,15 @@ namespace TL
                 // '_device_name' should append its name to the generic  outline name.
                 std::string _outline_name;
 
-                ObjectList<Nodecl::NodeclBase> _ndrange_exprs;
-                ObjectList<Nodecl::NodeclBase> _shmem_exprs;
-                ObjectList<Nodecl::NodeclBase> _onto_exprs;
-
                 // The value of the 'file' clause
                 std::string _file;
 
                 // The value of the 'name' clause
                 std::string _name;
+
+                ObjectList<Nodecl::NodeclBase> _ndrange_exprs;
+                ObjectList<Nodecl::NodeclBase> _shmem_exprs;
+                ObjectList<Nodecl::NodeclBase> _onto_exprs;
 
                 // Devices information
                 ObjectList<std::string> _device_names;
@@ -66,36 +66,87 @@ namespace TL
 
             public:
                 TargetInformation() {}
-                ~TargetInformation(){}
 
-                void set_outline_name(std::string outline_name);
-                std::string get_outline_name() const;
+                void set_outline_name(std::string outline_name)
+                {
+                    _outline_name = outline_name;
+                }
 
-                void add_device_name(std::string device_name);
-                ObjectList<std::string> get_device_names() const;
+                std::string get_outline_name() const
+                {
+                    return _outline_name;
+                }
 
-                void set_file(std::string file);
-                std::string get_file() const;
+                void add_device_name(std::string device_name)
+                {
+                    _device_names.append(device_name);
+                }
 
-                void set_name(std::string name);
-                std::string get_name() const;
+                ObjectList<std::string> get_device_names() const
+                {
+                    return _device_names;
+                }
 
-                void set_ndrange(const ObjectList<Nodecl::NodeclBase>& ndrange);
-                //void append_to_ndrange(const ObjectList<Nodecl::NodeclBase>& ndrange);
-                ObjectList<Nodecl::NodeclBase> get_ndrange() const;
+                void set_file(std::string file)
+                {
+                    _file = file;
+                }
 
-                void set_shmem(const ObjectList<Nodecl::NodeclBase>& shmem);
-                //void append_to_shmem(const ObjectList<Nodecl::NodeclBase>& shmem);
-                ObjectList<Nodecl::NodeclBase> get_shmem() const;
+                std::string get_file() const
+                {
+                    return _file;
+                }
 
-                void set_onto(const ObjectList<Nodecl::NodeclBase>& onto);
-                //void append_to_onto(const ObjectList<Nodecl::NodeclBase>& onto);
-                ObjectList<Nodecl::NodeclBase> get_onto() const;
+                void set_name(std::string name)
+                {
+                    _name = name;
+                }
 
-                Nodecl::Utils::SimpleSymbolMap& get_param_arg_map();
-                void set_param_arg_map(Nodecl::Utils::SimpleSymbolMap param_arg_map);
+                std::string get_name() const
+                {
+                    return _name;
+                }
+
+                void set_ndrange(const ObjectList<Nodecl::NodeclBase>& ndrange)
+                {
+                    _ndrange_exprs = ndrange;
+                }
+
+                ObjectList<Nodecl::NodeclBase> get_ndrange() const
+                {
+                    return _ndrange_exprs;
+                }
+
+                void set_shmem(const ObjectList<Nodecl::NodeclBase>& shmem)
+                {
+                    _shmem_exprs = shmem;
+                }
+
+                ObjectList<Nodecl::NodeclBase> get_shmem() const
+                {
+                    return _shmem_exprs;
+                }
+
+                void set_onto(const ObjectList<Nodecl::NodeclBase>& onto)
+                {
+                    _onto_exprs.append(onto);
+                }
+
+                ObjectList<Nodecl::NodeclBase> get_onto() const
+                {
+                    return _onto_exprs;
+                }
+
+                Nodecl::Utils::SimpleSymbolMap& get_param_arg_map()
+                {
+                    return _param_to_args;
+                }
+
+                void set_param_arg_map(Nodecl::Utils::SimpleSymbolMap param_arg_map)
+                {
+                    _param_to_args = param_arg_map;
+                }
         };
-
     }
 }
 
