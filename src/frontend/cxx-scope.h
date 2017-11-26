60761a787ec05aefcb1613e61d9e13fe48f1fa74
Roger Ferrer Ibanez <rofirrim@gmail.com>
2017-11-26 11:46:51 +0000

    Print template parameter lists in print_scope

--- src/frontend/cxx-scope.h
+++ src/frontend/cxx-scope.h
@@ -240,7 +240,7 @@ LIBMCXX_EXTERN const char* unmangle_symbol_name(scope_entry_t* entry);
 LIBMCXX_EXTERN const char* symbol_kind_to_str(enum cxx_symbol_kind symbol_kind);
 LIBMCXX_EXTERN enum cxx_symbol_kind symbol_str_to_kind(const char* str);
 
-// Debug
+// Debug. Defined in cxx-printscope.c
 LIBMCXX_EXTERN void print_template_parameter_list(template_parameter_list_t* template_parameters);
 
 // Dependent names
