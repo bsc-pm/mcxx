c4f30ddd4c0696a8ccb30f7e9b586d80c1b36b83
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2018-08-16 15:29:09 +0200

    Allow incomplete independent type when checking if a func. type can override another func. type
    It fixes #2856

--- tests/02_typecalc_cxx.dg/success_644.cpp
+++ tests/02_typecalc_cxx.dg/success_644.cpp
@@ -4,14 +4,35 @@ test_generator="config/mercurium"
 test_nolink=yes
 </testinfo>
 */
-template < typename T>
-struct A;
 
-
-void foo(A<int>& var);
-void foo(A<double>& var);
-
-void bar(A<double> & v)
+namespace test1
 {
-    foo(v);
+    template < typename T>
+        struct A;
+
+
+    void foo(A<int>& var);
+    void foo(A<double>& var);
+
+    void bar(A<double> & v)
+    {
+        foo(v);
+    }
+}
+
+namespace test2
+{
+    struct A
+    {
+        virtual A* foo() = 0;
+    };
+
+    template < typename>
+    struct B {};
+
+    template <>
+    struct B<int> : public A
+    {
+        B<int>* foo()  {}
+    };
 }
