8b74de4fab2ec7d9a3625bcac71568657d4d9405
Josep M. Perez <josep.m.perez@bsc.es>
2018-09-26 16:01:42 +0200

    Moved functions to create static variables in Nanos6 to a place where they can be reused.

--- src/tl/ompss/nanos6/tl-nanos6-support.cpp
+++ src/tl/ompss/nanos6/tl-nanos6-support.cpp
@@ -71,6 +71,201 @@ void add_extra_mapping_for_dimension(Nodecl::NodeclBase vla_var,
         new_vlas.append(new_vla_var);
     }
 }
+
+void create_static_variable_regular_function(
+    const std::string &var_name,
+    TL::Type var_type,
+    Nodecl::NodeclBase context,
+    TL::Nanos6::LoweringPhase* phase,
+    /* out */
+    TL::Symbol &new_var)
+{
+    // task info goes to the global scope
+    new_var = TL::Scope::get_global_scope().new_symbol(var_name);
+    new_var.get_internal_symbol()->kind = SK_VARIABLE;
+    symbol_entity_specs_set_is_user_declared(new_var.get_internal_symbol(), 1);
+    new_var.set_type(var_type);
+    symbol_entity_specs_set_is_static(new_var.get_internal_symbol(), 1);
+
+    // Add required declarations to the tree
+    if (IS_C_LANGUAGE || IS_CXX_LANGUAGE)
+    {
+        if (IS_CXX_LANGUAGE)
+        {
+            Nodecl::Utils::prepend_to_enclosing_top_level_location(
+                context, Nodecl::CxxDef::make(Nodecl::NodeclBase::null(), new_var));
+        }
+
+        Nodecl::Utils::prepend_to_enclosing_top_level_location(
+            context, Nodecl::ObjectInit::make(new_var));
+    }
+    else if (IS_FORTRAN_LANGUAGE)
+    {
+        phase->get_extra_c_code().append(
+            Nodecl::ObjectInit::make(new_var));
+    }
+    else
+    {
+        internal_error("Code unreachable", 0);
+    }
+}
+
+void create_static_variable_nondependent_function(
+    const std::string &var_name,
+    TL::Type var_type,
+    Nodecl::NodeclBase context,
+    TL::Nanos6::LoweringPhase* phase,
+    /* out */
+    TL::Symbol &new_var)
+{
+    ERROR_CONDITION(!IS_CXX_LANGUAGE, "This is only for C++", 0);
+
+    TL::Symbol related_function = Nodecl::Utils::get_enclosing_function(context);
+
+    if (!related_function.is_member())
+        return create_static_variable_regular_function(
+                var_name, var_type, context, phase, new_var);
+
+    // Member
+
+    // new_var is a static member of the class
+    TL::Type class_type = related_function.get_class_type();
+    TL::Scope class_scope = ::class_type_get_inner_context (class_type.get_internal_type());
+
+    new_var = class_scope.new_symbol(var_name);
+
+    new_var.get_internal_symbol()->kind = SK_VARIABLE;
+    symbol_entity_specs_set_is_user_declared(new_var.get_internal_symbol(), 1);
+    new_var.set_type(var_type);
+    symbol_entity_specs_set_is_member(new_var.get_internal_symbol(), 1);
+    symbol_entity_specs_set_class_type(new_var.get_internal_symbol(), class_type.get_internal_type());
+    symbol_entity_specs_set_is_static(new_var.get_internal_symbol(), 1);
+    symbol_entity_specs_set_access(new_var.get_internal_symbol(), AS_PUBLIC);
+
+    class_type_add_member(class_type.get_internal_type(),
+                            new_var.get_internal_symbol(),
+                            new_var.get_internal_symbol()->decl_context,
+                            /* is_definition */ 0);
+
+    set_is_dependent_type(class_type.get_internal_type(),
+                            related_function.get_class_type().is_dependent());
+
+    Nodecl::Utils::append_to_top_level_nodecl(
+                Nodecl::List::make(
+                    Nodecl::CxxDef::make(Nodecl::Context::make(nodecl_null(), TL::Scope::get_global_scope()), new_var),
+                    Nodecl::ObjectInit::make(new_var)));
+}
+
+void create_static_variable_dependent_function(
+    const std::string &var_name,
+    TL::Type var_type,
+    Nodecl::NodeclBase context,
+    TL::Symbol related_function,
+    TL::Nanos6::LoweringPhase* phase,
+    /* out */
+    TL::Symbol &new_var)
+{
+    ERROR_CONDITION(!IS_CXX_LANGUAGE, "This is only for C++", 0);
+
+    TL::Scope scope_of_template_class;
+    if (!related_function.is_member())
+    {
+
+        // We want new_var symbol be in the anonymous namespace of the
+        // global
+        // scope, so make sure it has been created
+        TL::Source src;
+        src << "namespace { }";
+        src.parse_global(TL::Scope::get_global_scope());
+        //
+
+        TL::Symbol anonymous_namespace = TL::Scope::get_global_scope().get_symbol_from_name( "(unnamed)");
+        ERROR_CONDITION(!anonymous_namespace.is_valid(), "Missing unnamed namespace", 0);
+        scope_of_template_class = anonymous_namespace.get_internal_symbol() ->related_decl_context;
+    }
+    else
+    {
+        scope_of_template_class = ::class_type_get_inner_context(
+            related_function.get_class_type().get_internal_type());
+    }
+
+    std::string tpl_name = var_name + std::string("_templated_type");
+
+    template_parameter_list_t *tpl
+        = template_specialized_type_get_template_parameters(
+            related_function.get_type().get_internal_type());
+
+    TL::Symbol new_class_symbol =
+        SymbolUtils::new_class_template(tpl_name,
+                tpl,
+                scope_of_template_class,
+                context.get_locus());
+
+    if (related_function.is_member())
+    {
+        type_t *current_class = related_function.get_class_type().get_internal_type();
+        symbol_entity_specs_set_is_member(new_class_symbol.get_internal_symbol(), 1);
+        symbol_entity_specs_set_class_type(new_class_symbol.get_internal_symbol(), current_class);
+        symbol_entity_specs_set_is_defined_inside_class_specifier(new_class_symbol.get_internal_symbol(), 1);
+        symbol_entity_specs_set_access(new_class_symbol.get_internal_symbol(), AS_PUBLIC);
+        class_type_add_member(
+            current_class,
+            new_class_symbol.get_internal_symbol(),
+            new_class_symbol.get_internal_symbol()->decl_context,
+            /* is_definition */ 1);
+
+        class_type_set_enclosing_class_type(
+            new_class_symbol.get_type().get_internal_type(), current_class);
+    }
+
+    TL::Scope class_scope(class_type_get_inner_context(
+        new_class_symbol.get_type().get_internal_type()));
+
+
+    // Now add the field
+    new_var = class_scope.new_symbol(var_name);
+    new_var.get_internal_symbol()->kind = SK_VARIABLE;
+    symbol_entity_specs_set_is_user_declared(new_var.get_internal_symbol(), 1);
+    new_var.set_type(var_type);
+    symbol_entity_specs_set_is_member(new_var.get_internal_symbol(), 1);
+    symbol_entity_specs_set_class_type(
+        new_var.get_internal_symbol(),
+        new_class_symbol.get_user_defined_type().get_internal_type());
+
+    symbol_entity_specs_set_is_static(new_var.get_internal_symbol(), 1);
+    symbol_entity_specs_set_access(new_var.get_internal_symbol(), AS_PUBLIC);
+
+    class_type_add_member(
+            new_class_symbol.get_type().get_internal_type(),
+            new_var.get_internal_symbol(),
+            new_var.get_internal_symbol()->decl_context,
+            /* is_definition */ 0);
+
+    // Finish the template class
+    nodecl_t nodecl_output = nodecl_null();
+    finish_class_type(
+        new_class_symbol.get_type().get_internal_type(),
+        ::get_user_defined_type(new_class_symbol.get_internal_symbol()),
+        new_class_symbol.get_scope().get_decl_context(),
+        context.get_locus(),
+        &nodecl_output);
+    set_is_complete_type(new_class_symbol.get_type().get_internal_type(), /* is_complete */ 1);
+    set_is_complete_type(
+        get_actual_class_type(
+            new_class_symbol.get_type().get_internal_type()),
+        /* is_complete */ 1);
+
+    // Add required declarations to the tree
+    Nodecl::Utils::prepend_to_enclosing_top_level_location(
+        context,
+        Nodecl::CxxDef::make(Nodecl::NodeclBase::null(), new_class_symbol));
+
+    Nodecl::Utils::append_to_top_level_nodecl(
+                Nodecl::List::make(
+                    Nodecl::CxxDef::make(Nodecl::Context::make(nodecl_null(), TL::Scope::get_global_scope()), new_var),
+                    Nodecl::ObjectInit::make(new_var)));
+}
+
 }
 
 namespace TL { namespace Nanos6 {
@@ -182,4 +377,50 @@ namespace TL { namespace Nanos6 {
 
         return call_to_nanos6_bzero;
     }
+
+    void create_static_variable_depending_on_function_context(
+        const std::string &var_name,
+        TL::Type var_type,
+        Nodecl::NodeclBase context,
+        LoweringPhase* phase,
+        /* out */
+        TL::Symbol &new_var)
+    {
+        if (IS_C_LANGUAGE || IS_FORTRAN_LANGUAGE)
+        {
+            create_static_variable_regular_function(
+                    var_name,
+                    var_type,
+                    context,
+                    phase,
+                    new_var);
+        }
+        else // IS_CXX_LANGUAGE
+        {
+            TL::Symbol related_function = Nodecl::Utils::get_enclosing_function(context);
+            if (!related_function.get_type().is_template_specialized_type()
+                    || (!related_function.get_type().is_dependent()
+                        && (!related_function.is_member()
+                            || !related_function.get_class_type().is_dependent())))
+            {
+                create_static_variable_nondependent_function(
+                        var_name,
+                        var_type,
+                        context,
+                        phase,
+                        new_var);
+            }
+            else
+            {
+                create_static_variable_dependent_function(
+                        var_name,
+                        var_type,
+                        context,
+                        related_function,
+                        phase,
+                        new_var);
+            }
+        }
+    }
+
 }}
