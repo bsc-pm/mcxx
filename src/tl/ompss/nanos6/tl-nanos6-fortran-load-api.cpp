bsc-pm/mcxx

commit fbd85322678f0774bf9b5f3888c5a7abcf67cd55

Added support for weakcommutative dependencies in OmpSs-2.

src/tl/ompss/nanos6/tl-nanos6-fortran-load-api.cpp
@@ -57,6 +57,7 @@ const char *multidimensional_entry_points[] =
     "nanos6_register_region_weak_write_depinfo",
     "nanos6_register_region_weak_readwrite_depinfo",
     "nanos6_register_region_commutative_depinfo",
+    "nanos6_register_region_weakcommutative_depinfo",
     "nanos6_register_region_concurrent_depinfo",
     "nanos6_register_region_reduction_depinfo",
     "nanos6_register_region_weakreduction_depinfo",

src/frontend/cxx-nodecl.def
@@ -745,6 +745,7 @@ omp-deps-info: NODECL_OMP_SS*DEP_CONCURRENT([exprs]expression-seq)
                 | NODECL_OMP_SS*DEP_WEAK_IN([exprs]expression-seq)
                 | NODECL_OMP_SS*DEP_WEAK_OUT([exprs]expression-seq)
                 | NODECL_OMP_SS*DEP_WEAK_INOUT([exprs]expression-seq)
+                | NODECL_OMP_SS*DEP_WEAK_COMMUTATIVE([exprs]expression-seq)
 
 ompss-device-info : NODECL_OMP_SS*TARGET([devices]text-seq, [items]ompss-device-info-clauses-seq-opt)
 

src/tl/omp/base/tl-omp-base.cpp
@@ -3554,6 +3554,12 @@ namespace TL { namespace OpenMP {
                 locus,
                 result_list);
 
+        make_item_list<Nodecl::OmpSs::DepWeakCommutative>(
+                dependences,
+                OpenMP::DEP_OMPSS_WEAK_COMMUTATIVE,
+                locus,
+                result_list);
+
         make_item_list<Nodecl::OmpSs::DepReduction>(
                 dependences, OpenMP::DEP_OMPSS_REDUCTION,
                 locus,

src/tl/omp/base/tl-ompss-base-task.cpp
@@ -469,6 +469,12 @@ namespace TL { namespace OmpSs {
                 locus,
                 result_list);
 
+        _base->make_item_list<Nodecl::OmpSs::DepWeakCommutative>(
+                task_dependences,
+                OpenMP::DEP_OMPSS_WEAK_COMMUTATIVE,
+                locus,
+                result_list);
+
         // Make sure the remaining symbols are firstprivate
         std::vector<bool> has_dep(function_sym.get_type().parameters().size(), false);
 
@@ -773,6 +779,11 @@ namespace TL { namespace OmpSs {
             there_are_dependences = true;
         }
 
+        void visit(const Nodecl::OmpSs::DepWeakCommutative& dep_commutative)
+        {
+            there_are_dependences = true;
+        }
+
         void visit(const Nodecl::OmpSs::CopyIn& copy_in)
         {
             there_are_copies = true;
@@ -872,6 +883,11 @@ namespace TL { namespace OmpSs {
         void visit(const Nodecl::OmpSs::DepCommutative& dep_inout)
         {
             report_dep(dep_inout.get_exprs(), OpenMP::DEP_OMPSS_COMMUTATIVE);
+        }
+
+        void visit(const Nodecl::OmpSs::DepWeakCommutative& dep_commutative)
+        {
+            report_dep(dep_commutative.get_exprs(), OpenMP::DEP_OMPSS_WEAK_COMMUTATIVE);
         }
     };
 

src/tl/omp/core/tl-omp-deps.cpp
@@ -442,6 +442,13 @@ namespace TL {
                 expr_list, default_data_attr, this->in_ompss_mode(),
                 "commutative", data_sharing_environment, extra_symbols);
 
+        expr_list = parse_dependences_ompss_clause(
+                pragma_line.get_clause("weakcommutative"),
+                parsing_context);
+        get_info_from_dependences<DEP_OMPSS_WEAK_COMMUTATIVE>(
+                expr_list, default_data_attr, this->in_ompss_mode(),
+                "weakcommutative", data_sharing_environment, extra_symbols);
+
         // OpenMP standard clauses
         PragmaCustomClause depends = pragma_line.get_clause("depend");
         get_dependences_openmp(depends, parsing_context, data_sharing_environment,
@@ -788,6 +795,7 @@ namespace TL {
             case DEP_OMPSS_WEAK_IN:
             case DEP_OMPSS_WEAK_OUT:
             case DEP_OMPSS_WEAK_INOUT:
+            case DEP_OMPSS_WEAK_COMMUTATIVE:
             case DEP_OMPSS_WEAK_REDUCTION:
                 return true;
             default:
@@ -817,6 +825,8 @@ namespace TL {
                 return "weakout";
             case DEP_OMPSS_WEAK_INOUT:
                 return "weakinout";
+            case DEP_OMPSS_WEAK_COMMUTATIVE:
+                return "weakcommutative";
             case DEP_OMPSS_REDUCTION:
                 return "reduction";
             case DEP_OMPSS_WEAK_REDUCTION:

src/tl/omp/core/tl-omp-deps.hpp
@@ -63,7 +63,9 @@ namespace TL { namespace OpenMP {
         //   Reduction dependence type
         DEP_OMPSS_REDUCTION = BITMAP(8),
         //   Weakreduction dependence type
-        DEP_OMPSS_WEAK_REDUCTION = BITMAP(9)
+        DEP_OMPSS_WEAK_REDUCTION = BITMAP(9),
+        //   Weakcommutative dependence type
+        DEP_OMPSS_WEAK_COMMUTATIVE = BITMAP(10)
     };
 
     bool is_strict_dependency(DependencyDirection dir);

src/tl/omp/core/tl-ompss-core.cpp
@@ -319,7 +319,8 @@ namespace TL { namespace OpenMP {
             inout_arguments,
             weakinout_arguments,
             concurrent_arguments,
-            commutative_arguments;
+            commutative_arguments,
+				weakcommutative_arguments;
 
         {
             struct DependencesClauses
@@ -337,6 +338,7 @@ namespace TL { namespace OpenMP {
                 { weakinout_arguments, "weakinout", NULL },
                 { concurrent_arguments, "concurrent", NULL },
                 { commutative_arguments, "commutative", NULL },
+                { weakcommutative_arguments, "weakcommutative", NULL },
             };
 
             for (DependencesClauses* it = deps_clauses;
@@ -443,15 +445,16 @@ namespace TL { namespace OpenMP {
                 TL::ObjectList<Nodecl::NodeclBase>& deps_nodes;
                 DependencyItem::ItemDirection direction;
             } deps_info[] = {
-                { input_arguments,         DEP_DIR_IN               },
-                { weakinput_arguments,     DEP_OMPSS_WEAK_IN        },
-                { input_private_arguments, DEP_OMPSS_DIR_IN_PRIVATE },
-                { output_arguments,        DEP_DIR_OUT              },
-                { weakoutput_arguments,    DEP_OMPSS_WEAK_OUT       },
-                { inout_arguments,         DEP_DIR_INOUT            },
-                { weakinout_arguments,     DEP_OMPSS_WEAK_INOUT     },
-                { concurrent_arguments,    DEP_OMPSS_CONCURRENT     },
-                { commutative_arguments,   DEP_OMPSS_COMMUTATIVE    },
+                { input_arguments,           DEP_DIR_IN               },
+                { weakinput_arguments,       DEP_OMPSS_WEAK_IN        },
+                { input_private_arguments,   DEP_OMPSS_DIR_IN_PRIVATE },
+                { output_arguments,          DEP_DIR_OUT              },
+                { weakoutput_arguments,      DEP_OMPSS_WEAK_OUT       },
+                { inout_arguments,           DEP_DIR_INOUT            },
+                { weakinout_arguments,       DEP_OMPSS_WEAK_INOUT     },
+                { concurrent_arguments,      DEP_OMPSS_CONCURRENT     },
+                { commutative_arguments,     DEP_OMPSS_COMMUTATIVE    },
+                { weakcommutative_arguments, DEP_OMPSS_WEAK_COMMUTATIVE    },
             };
 
             for (DependencesInformation* it = deps_info;

src/tl/omp/core/tl-ompss-target.cpp
@@ -625,6 +625,7 @@ namespace TL
                                 break;
                             }
                         case DEP_OMPSS_WEAK_INOUT:
+                        case DEP_OMPSS_WEAK_COMMUTATIVE:
                             {
                                 p = &dep_list_weakinout;
                                 break;

commit a9d6f12c4b18743adcb861a44de034f4b0e76704

Fix Nanos6 multidimensional dependences entry points for Fortran
- Typos introduced in commits fbd853226 and 25830b79d.

src/tl/ompss/nanos6/tl-nanos6-fortran-load-api.cpp
@@ -57,10 +57,10 @@ const char *multidimensional_entry_points[] =
     "nanos6_register_region_weak_write_depinfo",
     "nanos6_register_region_weak_readwrite_depinfo",
     "nanos6_register_region_commutative_depinfo",
-    "nanos6_register_region_weakcommutative_depinfo",
+    "nanos6_register_region_weak_commutative_depinfo",
     "nanos6_register_region_concurrent_depinfo",
     "nanos6_register_region_reduction_depinfo",
-    "nanos6_register_region_weakreduction_depinfo",
+    "nanos6_register_region_weak_reduction_depinfo",
     "nanos6_release_read_",
     "nanos6_release_write_",
     "nanos6_release_readwrite_",