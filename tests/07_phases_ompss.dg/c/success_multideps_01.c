8f83dd1899489155d8ad79cd36ab76428db94530
Sergi Mateo Bellido <sergi.mateo@bsc.es>
2019-03-28 14:31:57 +0100

    Disabling some OmpSs-2 tests
    They will be reenabled once a reported race condition is solved.

--- tests/07_phases_ompss.dg/c/success_multideps_01.c
+++ tests/07_phases_ompss.dg/c/success_multideps_01.c
@@ -27,7 +27,7 @@
 
 /*
 <testinfo>
-test_generator=(config/mercurium-ompss "config/mercurium-ompss-2 openmp-compatibility")
+test_generator=(config/mercurium-ompss)
 test_ENV="NX_THROTTLE=dummy"
 test_CFLAGS="--no-copy-deps"
 </testinfo>
--- tests/07_phases_ompss.dg/cxx/success_multideps_02.cpp
+++ tests/07_phases_ompss.dg/cxx/success_multideps_02.cpp
@@ -1,6 +1,6 @@
 /*
 <testinfo>
-test_generator=(config/mercurium-ompss "config/mercurium-ompss-2 openmp-compatibility")
+test_generator=(config/mercurium-ompss)
 test_CXXFLAGS="--no-copy-deps"
 </testinfo>
 */
