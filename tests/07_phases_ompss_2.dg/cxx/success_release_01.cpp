70a299cfeb1f96735e6b9835aee946451f1913b2
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2020-06-19 22:58:09 +0000

    Disable tests for a Nanos6 unsupported feature
    The 'release' construct does not currently support the type downgrade of
    dependencies; the dependency type specified at that construct must be
    its complete type

--- tests/07_phases_ompss_2.dg/cxx/success_release_01.cpp
+++ tests/07_phases_ompss_2.dg/cxx/success_release_01.cpp
@@ -1,8 +1,8 @@
 /*
 <testinfo>
 test_generator="config/mercurium-ompss-2"
-test_exec_fail=yes
-test_exec_faulty=yes
+test_ignore=yes
+test_ignore_reason="feature not supported by nanos6"
 </testinfo>
 */
 #include<assert.h>
