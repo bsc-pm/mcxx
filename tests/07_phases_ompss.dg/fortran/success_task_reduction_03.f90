112f57f1d1ded5d522e148ce6ea42f1488ad8c05
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2020-11-17 16:11:46 +0100

    More fortran reduction tests work

--- tests/07_phases_ompss.dg/fortran/success_task_reduction_03.f90
+++ tests/07_phases_ompss.dg/fortran/success_task_reduction_03.f90
@@ -1,7 +1,5 @@
 ! <testinfo>
 ! test_generator=(config/mercurium-ompss "config/mercurium-ompss-2 openmp-compatibility")
-! test_compile_fail_nanos6_mercurium=yes
-! test_compile_fail_nanos6_imfc=yes
 ! </testinfo>
 
 
